a0527339335f00108c1b32d962521bf13b99f542
Nicolas Luck <nicolas@lucksus.org>
2018-06-15 23:56:06 +0200

    WIP action loop in its own thread

--- hc_core/src/instance.rs
+++ hc_core/src/instance.rs
@@ -1,9 +1,14 @@
 use state::*;
 use std::collections::VecDeque;
+use std::sync::mpsc::*;
+use std::thread;
+use std::time::Duration;
+use std::sync::Arc;
 
 pub struct Instance {
     state: State,
     pending_actions: VecDeque<Action>,
+    action_channel: Sender<Action>
 }
 
 impl Instance {
@@ -22,10 +27,26 @@ impl Instance {
         }
     }
 
+    pub fn start_action_loop(&mut self) {
+        let (tx_action, rx_action) = channel();
+        self.action_channel = tx_action.clone();
+
+        thread::spawn(move || {
+            let mut state = State::create();
+            //self.state = &state;
+            match rx_action.recv_timeout(Duration::from_millis(400)) {
+                Ok(action) => state = state.clone().reduce(&action),
+                Err(ref recv_error) => {}
+            }
+        });
+    }
+
     pub fn create() -> Self {
+        let (sender, receiver) = channel();
         Instance {
             state: State::create(),
             pending_actions: VecDeque::new(),
+            action_channel: sender
         }
     }
 

1bcfb2680ea4ab6c96da084a3def5ab756ac5037
David Meister <thedavidmeister@gmail.com>
2018-06-18 22:52:47 +1000

    tweaking hashes vs. content in pairs

--- hc_core/src/common/entry.rs
+++ hc_core/src/common/entry.rs
@@ -4,20 +4,30 @@ use std::hash::{Hash as _Hash, Hasher};
 #[derive(Clone, Debug, PartialEq, Hash)]
 pub struct Entry {
     content: String,
+    hash: u64,
 }
 
 impl Entry {
     pub fn new (content: &String) -> Entry {
-        Entry {
+        let mut e = Entry {
             content: content.clone(),
-        }
+            hash: 0,
+        };
+        let mut hasher = DefaultHasher::new();
+        _Hash::hash(&e, &mut hasher);
+        e.hash = hasher.finish();
+        e
+    }
+
+    pub fn hash(&self) -> u64 {
+        self.hash
     }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Header {
     previous: Option<u64>,
-    entry: Entry,
+    entry: u64,
     hash: u64,
 }
 
@@ -32,7 +42,7 @@ impl Header {
     pub fn new(previous: Option<u64>, entry: &Entry) -> Header {
         let mut h = Header {
             previous: previous,
-            entry: entry.clone(),
+            entry: entry.hash(),
             hash: 0,
         };
         let mut hasher = DefaultHasher::new();

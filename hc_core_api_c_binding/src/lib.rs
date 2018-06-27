577c7ca0f342684c3b781d2a322a89e70fe0b33e
Nicolas Luck <nicolas@lucksus.org>
2018-06-27 15:37:00 +0200

    clippy

--- hc_core_api_c_binding/src/lib.rs
+++ hc_core_api_c_binding/src/lib.rs
@@ -12,10 +12,9 @@ use std::sync::Arc;
 use hc_agent::Agent;
 use hc_core::logger::Logger;
 use hc_core::persister::SimplePersister;
-use std::sync::Mutex;
-use std::os::raw::c_char;
 use std::ffi::{CStr, CString};
-
+use std::os::raw::c_char;
+use std::sync::Mutex;
 
 #[derive(Clone, Debug)]
 struct NullLogger {}
@@ -25,7 +24,7 @@ impl Logger for NullLogger {
 }
 
 #[no_mangle]
-pub extern "C" fn hc_new(ptr: *mut Dna) -> *mut Holochain {
+pub unsafe extern "C" fn hc_new(ptr: *mut Dna) -> *mut Holochain {
     let agent = Agent::from_string("c_bob");
 
     let context = Arc::new(Context {
@@ -34,10 +33,8 @@ pub extern "C" fn hc_new(ptr: *mut Dna) -> *mut Holochain {
         persister: Arc::new(Mutex::new(SimplePersister::new())),
     });
 
-    let dna = unsafe {
-        assert!(!ptr.is_null());
-        Box::from_raw(ptr)
-    };
+    assert!(!ptr.is_null());
+    let dna = Box::from_raw(ptr);
 
     match Holochain::new(*dna, context) {
         Ok(hc) => Box::into_raw(Box::new(hc)),
@@ -46,57 +43,70 @@ pub extern "C" fn hc_new(ptr: *mut Dna) -> *mut Holochain {
 }
 
 #[no_mangle]
-pub extern "C" fn hc_start(ptr: *mut Holochain) -> bool {
-    let holochain = unsafe {
+pub unsafe extern "C" fn hc_start(ptr: *mut Holochain) -> bool {
+    let holochain = {
         if ptr.is_null() {
             return false;
         }
-        &mut*ptr
+        &mut *ptr
     };
 
-    match holochain.start() {
-        Ok(_) => true,
-        Err(_) => false
-    }
+    holochain.start().is_ok()
 }
 
 #[no_mangle]
-pub extern "C" fn hc_stop(ptr: *mut Holochain) -> bool {
-    let holochain = unsafe {
+pub unsafe extern "C" fn hc_stop(ptr: *mut Holochain) -> bool {
+    let holochain = {
         if ptr.is_null() {
             return false;
         }
-        &mut*ptr
+        &mut *ptr
     };
 
-    match holochain.stop() {
-        Ok(_) => true,
-        Err(_) => false
-    }
+    holochain.stop().is_ok()
 }
 
 type CStrPtr = *mut c_char;
 
 #[no_mangle]
-pub extern "C" fn hc_call(ptr: *mut Holochain, zome: CStrPtr, capability: CStrPtr, function: CStrPtr, parameters: CStrPtr) -> CStrPtr {
-    if ptr.is_null() || zome.is_null() || capability.is_null() || function.is_null() || parameters.is_null() {
-        return std::ptr::null_mut()
+pub unsafe extern "C" fn hc_call(
+    ptr: *mut Holochain,
+    zome: CStrPtr,
+    capability: CStrPtr,
+    function: CStrPtr,
+    parameters: CStrPtr,
+) -> CStrPtr {
+    if ptr.is_null()
+        || zome.is_null()
+        || capability.is_null()
+        || function.is_null()
+        || parameters.is_null()
+    {
+        return std::ptr::null_mut();
     }
 
-    let holochain = unsafe { &mut*ptr };
-    let zome = unsafe { CStr::from_ptr(zome).to_string_lossy().into_owned() };
-    let capability = unsafe { CStr::from_ptr(capability).to_string_lossy().into_owned() };
-    let function = unsafe { CStr::from_ptr(function).to_string_lossy().into_owned() };
-    let parameters = unsafe { CStr::from_ptr(parameters).to_string_lossy().into_owned() };
+    let holochain = &mut *ptr;
+    let zome = CStr::from_ptr(zome).to_string_lossy().into_owned();
+    let capability = CStr::from_ptr(capability).to_string_lossy().into_owned();
+    let function = CStr::from_ptr(function).to_string_lossy().into_owned();
+    let parameters = CStr::from_ptr(parameters).to_string_lossy().into_owned();
 
-    match holochain.call(zome.as_str(), capability.as_str(), function.as_str(), parameters.as_str()) {
+    match holochain.call(
+        zome.as_str(),
+        capability.as_str(),
+        function.as_str(),
+        parameters.as_str(),
+    ) {
         Ok(string_result) => match CString::new(string_result) {
-                                Ok(s) => s.into_raw(),
-                                Err(_) => std::ptr::null_mut(),
-                            },
-        Err(holochain_error) => match CString::new(format!("Error calling zome function: {:?}", holochain_error)) {
             Ok(s) => s.into_raw(),
-            Err(_) => std::ptr::null_mut()
-        }
+            Err(_) => std::ptr::null_mut(),
+        },
+        Err(holochain_error) => match CString::new(format!(
+            "Error calling zome function: {:?}",
+            holochain_error
+        )) {
+            Ok(s) => s.into_raw(),
+            Err(_) => std::ptr::null_mut(),
+        },
     }
 }

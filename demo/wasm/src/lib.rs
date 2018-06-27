4e4d4e51d5ad7a55be43ed096b2d8aeca3d39ada
Julian Laubstein <contact@julianlaubstein.de>
2018-06-27 07:59:33 +0200

    [WIP] cleanup

--- demo/wasm/src/lib.rs
+++ demo/wasm/src/lib.rs
@@ -6,27 +6,7 @@ extern crate serde_derive;
 extern crate serde_json;
 
 use serde::{Deserialize, Serialize};
-
-extern "C" {
-    fn print(i: i32);
-}
-
-#[no_mangle]
-pub extern "C" fn _call(input_data: *mut u8, input_len: usize) -> i32 {
-    unsafe {
-        print(input_len as i32);
-        for i in 0..input_len {
-            //print(i as i32);
-            //print(888);
-            print(*input_data.offset(i as isize) as i32);
-        }
-    }
-
-    return 0; //a * a;
-}
-
-use std::ffi::CStr;
-use std::os::raw::c_char;
+use std::{ffi::CStr, os::raw::c_char, slice};
 
 fn make_internal<'s, T: Deserialize<'s>>(data: *mut c_char) -> T {
     let c_str = unsafe { CStr::from_ptr(data) };
@@ -36,16 +16,17 @@ fn make_internal<'s, T: Deserialize<'s>>(data: *mut c_char) -> T {
 
 fn make_external<T: Serialize>(data: *mut c_char, params_len: usize, internal: T) -> i32 {
     let json = serde_json::to_string(&internal).unwrap(); //same!
-                                                          //    let json = "fish".to_string();
+
     let bytes = json.as_bytes();
     let len = bytes.len();
-    for i in 0..len {
-        unsafe {
-            *data.offset(i as isize) = bytes[i] as i8;
-        }
+
+    let mem = unsafe { slice::from_raw_parts_mut(data, len) };
+
+    for (i, byte) in bytes.iter().enumerate() {
+        mem[params_len + i] = *byte as i8;
     }
+
     len as i32
-    //unimplemented!()
 }
 
 #[no_mangle]
@@ -72,16 +53,4 @@ fn test(input: InputStruct) -> OutputStruct {
         input_int_val_plus2: input.input_int_val + 2,
         input_str_val_plus_dog: format!("{}.puppy", input.input_str_val),
     }
-}
-
-pub extern "C" fn hello_dispatch(data: *mut c_char, params_len: usize) -> i32 {
-    let hello = "{\"holo\":\"world\"}";
-    let bytes = hello.as_bytes();
-    let len = bytes.len();
-    for i in 0..len {
-        unsafe {
-            *data.offset(i as isize) = bytes[i] as i8;
-        }
-    }
-    len as i32
 }

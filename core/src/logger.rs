1168ad674a0cf07eba0be5041e5600cd94984e87
Julian Laubstein <contact@julianlaubstein.de>
2018-06-27 09:08:55 +0200

    Huge refactor

---
+++ core/src/logger.rs
@@ -0,0 +1,39 @@
+//use std::sync::{Arc, Mutex};
+
+/*!
+This logger is the logger that's attached to each Holochain application
+which is separate from standard logging via the log crate warn! info! debug! logging that
+gets emitted globaly from the container.
+*/
+
+use std::fmt;
+
+/// trait that defines the logging functionality that holochain_core requires
+pub trait Logger: fmt::Debug {
+    fn log(&mut self, msg: String);
+}
+
+#[derive(Clone)]
+pub struct SimpleLogger {
+    //    log: Vec<String>,
+}
+
+extern crate chrono;
+
+use self::chrono::Local;
+
+impl Logger for SimpleLogger {
+    fn log(&mut self, msg: String) {
+        let date = Local::now();
+        println!("{}:{}", date.format("%Y-%m-%d %H:%M:%S"), msg);
+    }
+    /*    fn new() -> SimpleLogger {
+        SimpleLogger {}
+    }*/
+}
+
+impl fmt::Debug for SimpleLogger {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<empty>")
+    }
+}

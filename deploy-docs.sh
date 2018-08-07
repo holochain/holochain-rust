09f3ed4c6f685c17e85a4f5316a62766717ed34c
Travis CI <>
2018-08-07 15:48:34 -0400

    set up for Travis

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -9,33 +9,34 @@ function doCompile {
 }
 
 # Pull requests and commits to other branches shouldn't build
-#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#    exit 0
-#fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    exit 0
+fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into doc/holochain_101/book
-git clone $REPO doc/holochain_101/book
-cd doc/holochain_101/book
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ../../..
+# Clone the existing gh-pages for this repo into doc/holochain_101/working
+git clone -b $TARGET_BRANCH $REPO doc/holochain_101/working
 
 # Clean out existing contents
-rm -rf doc/holochain_101/book/**/* || exit 0
+cd doc/holochain_101/working
+git ls-files | xargs rm -rf
+cd ../../..
 
 # Run our compile script
 doCompile
 
+# Move all our built files into the working directory
+mv doc/holochain_101/book/* doc/holochain_101/working
+
 # Move a copy of our Github Pages config file back into the directory
-cp _config.yml doc/holochain_101/book/_config.yml
+cp _config.yml doc/holochain_101/working/_config.yml
 
 # Set things up with git for committing new changes to the mdbook
-cd doc/holochain_101/book
+cd doc/holochain_101/working
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -51,14 +52,14 @@ git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-#ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-#ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-#ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-#ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-#chmod 600 ../deploy_key
-#eval `ssh-agent -s`
-#ssh-add deploy_key
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+chmod 600 ../deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
--- doc/holochain_101/src/building_apps.md
+++ doc/holochain_101/src/building_apps.md
@@ -1,3 +1,1 @@
 # Building Apps
-
-Test

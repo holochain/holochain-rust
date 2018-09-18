7f615cb8faca5b201b605cd8edd82f3560d09c0f
damien <doute.d@gmail.com>
2018-09-17 23:42:48 -0400

    rustfmt

--- core/src/nucleus/ribosome/callback/mod.rs
+++ core/src/nucleus/ribosome/callback/mod.rs
@@ -22,7 +22,6 @@ use nucleus::{
 use num_traits::FromPrimitive;
 use std::{str::FromStr, sync::mpsc::Sender};
 
-
 /// Enumeration of all Zome Callbacks known and used by Holochain
 /// Enumeration can convert to str
 // @TODO should each one be an action, e.g. Action::Genesis(Zome)?

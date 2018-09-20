8cc5368fd0a32fccd42f0fb8bd433b84e0622fce
David Meister <thedavidmeister@gmail.com>
2018-09-20 23:28:50 +1000

    CAS as ContentAddressableStorage

--- core/src/cas/mod.rs
+++ core/src/cas/mod.rs
@@ -20,13 +20,13 @@ pub trait AddressableContent {
 /// content addressable store
 /// implements storage in memory or persistently
 /// anything implementing AddressableContent can be stored and retrieved
-pub trait ContentAddressableStore {
+pub trait ContentAddressableStorage {
     /// stores AddressableContent in the Store by its Address as Content
-    fn store(&self, content: &AddressableContent) -> Result<(), HolochainError>;
+    fn add(&self, content: &AddressableContent) -> Result<(), HolochainError>;
     /// true if the Address is in the Store, false otherwise.
     /// may be more efficient than retrieve depending on the implementation.
     fn contains(&self, address: &Address) -> Result<bool, HolochainError>;
     /// returns some Content String if it is in the Store
     /// note: the original struct/type is NOT restored/deserialized
-    fn retrieve(&self, address: &Address) -> Result<Option<Content>, HolochainError>;
+    fn fetch(&self, address: &Address) -> Result<Option<Content>, HolochainError>;
 }

ad170d5b9fe46da0205f99d4434327f142e716e1
David Meister <thedavidmeister@gmail.com>
2018-09-22 21:57:44 +1000

    tests for two different AddressableContent implementations in a single CAS

--- core/src/cas.rs
+++ core/src/cas.rs
@@ -13,8 +13,11 @@ type Content = String;
 /// the content is the address, there is no "location" like a file system or URL
 /// @see https://en.wikipedia.org/wiki/Content-addressable_storage
 pub trait AddressableContent {
+    /// the Address the Content would be available at once stored in a ContentAddressableStorage
     fn address(&self) -> Address;
+    /// the Content that would be stored in a ContentAddressableStorage
     fn content(&self) -> Content;
+    /// restore/deserialize the original struct/type from serialized Content
     fn from_content(&Content) -> Self where Self: Sized;
 }
 
@@ -28,8 +31,9 @@ pub trait ContentAddressableStorage {
     /// true if the Address is in the Store, false otherwise.
     /// may be more efficient than retrieve depending on the implementation.
     fn contains(&self, address: &Address) -> Result<bool, HolochainError>;
-    /// returns Some Content String if it is in the Store, else None
-    /// note: the original struct/type is NOT restored/deserialized
+    /// returns Some AddressableContent if it is in the Store, else None
+    /// AddressableContent::from_content() can be used to allow the compiler to infer the type
+    /// @see the fetch implementation for ExampleCas in the tests below
     fn fetch<C: AddressableContent> (&self, address: &Address) -> Result<Option<C>, HolochainError>;
 }
 
@@ -45,6 +49,8 @@ mod tests {
     use multihash::Hash;
 
     #[derive(Debug, PartialEq, Clone)]
+    /// some struct that can be content addressed
+    /// imagine an Entry, Header, MetaValue, etc.
     struct ExampleAddressableContent {
         content: Content,
     }
@@ -59,18 +65,38 @@ mod tests {
         }
 
         fn from_content (content: &Content) -> Self {
-            ExampleAddressableContent::new(content)
-        }
-    }
-
-    impl ExampleAddressableContent {
-        fn new(s: &str) -> ExampleAddressableContent {
             ExampleAddressableContent {
-                content: s.to_string(),
+                content: content.clone(),
             }
         }
     }
 
+    #[derive(Debug, PartialEq, Clone)]
+    /// another struct that can be content addressed
+    /// used to show ExampleCas storing multiple types
+    struct OtherExampleAddressableContent {
+        content: Content,
+    }
+
+    /// identical implementation to ExampleAddressableContent for simplicity
+    impl AddressableContent for OtherExampleAddressableContent {
+        fn address (&self) -> Address {
+            HashString::encode_from_str(&self.content(), Hash::SHA2256)
+        }
+
+        fn content (&self) -> Content {
+            self.content.clone()
+        }
+
+        fn from_content (content: &Content) -> Self {
+            OtherExampleAddressableContent {
+                content: content.clone(),
+            }
+        }
+    }
+
+    /// some struct to show an example ContentAddressableStorage implementation
+    /// there is no persistence or concurrency in this example so use a raw HashMap
     struct ExampleCas {
         storage: HashMap<Address, Content>,
     }
@@ -100,13 +126,25 @@ mod tests {
 
     #[test]
     fn example_round_trip() {
-        let content = ExampleAddressableContent::new("foo");
+        let content = ExampleAddressableContent::from_content(&"foo".to_string());
+        let other_content = OtherExampleAddressableContent::from_content(&"bar".to_string());
         let mut cas = ExampleCas::new();
 
         assert_eq!(Ok(false), cas.contains(&content.address()));
+        assert_eq!(Ok(false), cas.contains(&other_content.address()));
 
+        // round trip some AddressableContent through the ContentAddressableStorage
         assert_eq!(Ok(()), cas.add(&content));
         assert_eq!(Ok(true), cas.contains(&content.address()));
+        assert_eq!(Ok(false), cas.contains(&other_content.address()));
         assert_eq!(Ok(Some(content.clone())), cas.fetch(&content.address()));
+
+        // multiple types of AddressableContent can sit in a single ContentAddressableStorage
+        // the safety of this is only as good as the hashing algorithm(s) used
+        assert_eq!(Ok(()), cas.add(&other_content));
+        assert_eq!(Ok(true), cas.contains(&content.address()));
+        assert_eq!(Ok(true), cas.contains(&other_content.address()));
+        assert_eq!(Ok(Some(content.clone())), cas.fetch(&content.address()));
+        assert_eq!(Ok(Some(other_content.clone())), cas.fetch(&other_content.address()));
     }
 }

06f3b02849ffe6849504a8d86a570eef4dced3fd
David Meister <thedavidmeister@gmail.com>
2018-09-26 22:31:35 +1000

    tests for file cas

--- core/src/cas/file/mod.rs
+++ core/src/cas/file/mod.rs
@@ -13,6 +13,12 @@ pub struct FileContentAddressableStorage {
 }
 
 impl FileContentAddressableStorage {
+    pub fn new(path: &str) -> FileContentAddressableStorage {
+        FileContentAddressableStorage {
+            path: path.to_string(),
+        }
+    }
+
     fn address_to_path(&self, address: &Address) -> String {
         format!("{}{}{}.json", self.path, MAIN_SEPARATOR, address)
     }
@@ -40,4 +46,55 @@ impl ContentAddressableStorage for FileContentAddressableStorage {
             Ok(None)
         }
     }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use cas::{
+        content::{
+            tests::{ExampleAddressableContent, OtherExampleAddressableContent},
+            AddressableContent,
+        },
+        file::FileContentAddressableStorage,
+        storage::ContentAddressableStorage,
+    };
+    use tempfile::{tempdir, TempDir};
+
+    pub fn test_file_cas() -> (FileContentAddressableStorage, TempDir) {
+        let dir = tempdir().unwrap();
+        (
+            FileContentAddressableStorage::new(dir.path().to_str().unwrap()),
+            dir,
+        )
+    }
+
+    #[test]
+    /// show that content of different types can round trip through the same storage
+    /// this is copied straight from the example with a file CAS
+    fn file_content_round_trip_test() {
+        let content = ExampleAddressableContent::from_content(&"foo".to_string());
+        let other_content = OtherExampleAddressableContent::from_content(&"bar".to_string());
+        let (mut cas, _dir) = test_file_cas();
+
+        assert_eq!(Ok(false), cas.contains(&content.address()));
+        assert_eq!(Ok(false), cas.contains(&other_content.address()));
+
+        // round trip some AddressableContent through the FileContentAddressableStorage
+        assert_eq!(Ok(()), cas.add(&content));
+        assert_eq!(Ok(true), cas.contains(&content.address()));
+        assert_eq!(Ok(false), cas.contains(&other_content.address()));
+        assert_eq!(Ok(Some(content.clone())), cas.fetch(&content.address()));
+
+        // multiple types of AddressableContent can sit in a single FileContentAddressableStorage
+        // the safety of this is only as good as the hashing algorithm(s) used
+        assert_eq!(Ok(()), cas.add(&other_content));
+        assert_eq!(Ok(true), cas.contains(&content.address()));
+        assert_eq!(Ok(true), cas.contains(&other_content.address()));
+        assert_eq!(Ok(Some(content.clone())), cas.fetch(&content.address()));
+        assert_eq!(
+            Ok(Some(other_content.clone())),
+            cas.fetch(&other_content.address())
+        );
+    }
+
 }

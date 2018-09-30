0926899e8333d792193612e458413e758db849ac
David Meister <thedavidmeister@gmail.com>
2018-09-30 09:40:49 -0600

    more eav tests

--- core/src/cas/eav.rs
+++ core/src/cas/eav.rs
@@ -214,22 +214,99 @@ pub mod tests {
         let mut expected = HashSet::new();
         for many in vec![many_one.clone(), many_two.clone(), many_three.clone()] {
             let eav = EntityAttributeValue::new(&one.address(), &attribute, &many.address());
-            eav_storage.add_eav(&eav)
-            .expect("could not add eav");
+            eav_storage.add_eav(&eav).expect("could not add eav");
             expected.insert(eav);
         }
 
         // throw an extra thing referencing many to show fetch ignores it
         let two = ExampleAddressableContent::from_content(&"foo".to_string());
         for many in vec![many_one.clone(), many_three.clone()] {
-            eav_storage.add_eav(&EntityAttributeValue::new(&two.address(), &attribute, &many.address()))
-            .expect("could not add eav");
+            eav_storage
+                .add_eav(&EntityAttributeValue::new(
+                    &two.address(),
+                    &attribute,
+                    &many.address(),
+                ))
+                .expect("could not add eav");
         }
 
+        // show the many results for one
         assert_eq!(
             expected,
-            eav_storage.fetch_eav(Some(one.address()), Some(attribute), None).expect("could not fetch eav"),
+            eav_storage
+                .fetch_eav(Some(one.address()), Some(attribute.clone()), None)
+                .expect("could not fetch eav"),
         );
+
+        // show one for the many results
+        for many in vec![many_one.clone(), many_two.clone(), many_three.clone()] {
+            let mut expected_one = HashSet::new();
+            expected_one.insert(EntityAttributeValue::new(
+                &one.address(),
+                &attribute.clone(),
+                &many.address(),
+            ));
+            assert_eq!(
+                expected_one,
+                eav_storage
+                    .fetch_eav(None, Some(attribute.clone()), Some(many.address()))
+                    .expect("could not fetch eav"),
+            );
+        }
+    }
+
+    #[test]
+    fn example_eav_many_to_one() {
+        let one = ExampleAddressableContent::from_content(&"foo".to_string());
+        // it can reference itself, why not?
+        let many_one = ExampleAddressableContent::from_content(&"foo".to_string());
+        let many_two = ExampleAddressableContent::from_content(&"bar".to_string());
+        let many_three = ExampleAddressableContent::from_content(&"baz".to_string());
+        let attribute = "many_to_one".to_string();
+
+        let mut eav_storage = ExampleEntityAttributeValueStorage::new();
+        let mut expected = HashSet::new();
+        for many in vec![many_one.clone(), many_two.clone(), many_three.clone()] {
+            let eav = EntityAttributeValue::new(&many.address(), &attribute, &one.address());
+            eav_storage.add_eav(&eav).expect("could not add eav");
+            expected.insert(eav);
+        }
+
+        // throw an extra thing referenced by many to show fetch ignores it
+        let two = ExampleAddressableContent::from_content(&"foo".to_string());
+        for many in vec![many_one.clone(), many_three.clone()] {
+            eav_storage
+                .add_eav(&EntityAttributeValue::new(
+                    &many.address(),
+                    &attribute,
+                    &two.address(),
+                ))
+                .expect("could not add eav");
+        }
+
+        // show the many referencing one
+        assert_eq!(
+            expected,
+            eav_storage
+                .fetch_eav(None, Some(attribute.clone()), Some(one.address()))
+                .expect("could not fetch eav"),
+        );
+
+        // show one for the many results
+        for many in vec![many_one.clone(), many_two.clone(), many_three.clone()] {
+            let mut expected_one = HashSet::new();
+            expected_one.insert(EntityAttributeValue::new(
+                &many.address(),
+                &attribute.clone(),
+                &one.address(),
+            ));
+            assert_eq!(
+                expected_one,
+                eav_storage
+                    .fetch_eav(Some(many.address()), Some(attribute.clone()), None)
+                    .expect("could not fetch eav"),
+            );
+        }
     }
 
 }

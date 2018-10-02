e95995e29c770332704e7a2b7e574c680a13a4da
damien <doute.d@gmail.com>
2018-10-02 01:10:38 -0400

    Moved SinglePageManager from Nucleus to Ribosome.

--- core/src/nucleus/ribosome/api/mod.rs
+++ core/src/nucleus/ribosome/api/mod.rs
@@ -11,13 +11,13 @@ use context::Context;
 use holochain_dna::zome::capabilities::ReservedCapabilityNames;
 use holochain_wasm_utils::{error::HcApiReturnCode, memory_allocation::SinglePageAllocation};
 use nucleus::{
-    memory::SinglePageManager,
     ribosome::{
         api::{
             call::invoke_call, commit::invoke_commit_app_entry, debug::invoke_debug,
             get_entry::invoke_get_entry, init_globals::invoke_init_globals,
         },
         Defn,
+        memory::SinglePageManager,
     },
     ZomeFnCall,
 };

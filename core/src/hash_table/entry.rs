d9b7fa3ccc1cf82a7b3bcbe4f09d80d7c4141512
David Meister <thedavidmeister@gmail.com>
2018-10-02 08:09:31 -0600

    remove entry type from Entry

--- core/src/hash_table/entry.rs
+++ core/src/hash_table/entry.rs
@@ -15,10 +15,6 @@ use std::{
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Entry {
     content: String,
-
-    // @TODO do NOT serialize entry_type in Entry as it should only be in Header
-    // @see https://github.com/holochain/holochain-rust/issues/80
-    entry_type: String,
 }
 
 impl PartialEq for Entry {

0f57af3412fbf732aa4cfdb619bd27cb6d721e16
Nicolas Luck <nicolas@lucksus.eu>
2018-10-07 13:57:38 -0400

    Trait descriptions

--- cas_implementations/src/lib.rs
+++ cas_implementations/src/lib.rs
@@ -1,3 +1,10 @@
+/// CAS Implementations
+///
+/// (CAS == Content Addressable Storage)
+///
+/// This crate contains implementations for the CAS and EAV traits
+/// which are defined but not implemented in the core_types crate.
+
 extern crate futures;
 extern crate holochain_core_types;
 extern crate riker;

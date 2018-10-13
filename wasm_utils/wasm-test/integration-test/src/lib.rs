8796da04ac5738d14d2bacfcfb63f6466a0b36ca
damien <doute.d@gmail.com>
2018-10-13 16:11:14 -0400

    some cleanup

--- wasm_utils/wasm-test/integration-test/src/lib.rs
+++ wasm_utils/wasm-test/integration-test/src/lib.rs
@@ -26,28 +26,7 @@ pub extern "C" fn test_serialize(encoded_allocation: u32) -> u32 {
     let mut stack = SinglePageStack::default();
     let obj = InputTestStruct { value: "fish".to_string() };
     assert_eq!(0, stack.top());
-    dna_assert!(stack, 1 == stack.top());
     let res = {serialize(&mut stack, obj.clone())};
     assert_eq!(json!(obj).to_string().len(), stack.top() as usize);
     res.unwrap().encode()
 }
-
-
-//#[cfg(test)]
-//pub mod tests {
-//
-//    use super::*;
-//    use holochain_wasm_utils::{memory_allocation::*, memory_serialization::*};
-//
-//    #[test]
-//    pub extern "C" fn can_serialize() {
-//        println!("COUCOU");
-//        let mut stack = SinglePageStack::default();
-//        println!("stack: {:?}", stack);
-//        let obj = InputTestStruct { value: "fish".to_string() };
-//        assert_eq!(0, stack.top());
-//        let res = serialize(&mut stack, obj);
-//        println!("stack: {:?}", stack);
-//        println!("res: {:?}", res);
-//    }
-//}

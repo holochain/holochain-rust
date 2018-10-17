777e4d41509f20240b888dd087811c6424277e36
Connor Turland <connorturland@gmail.com>
2018-10-16 22:34:04 -0400

    Update wasm_utils/src/memory_serialization.rs

--- wasm_utils/src/memory_serialization.rs
+++ wasm_utils/src/memory_serialization.rs
@@ -61,7 +61,7 @@ pub fn store_json_into_encoded_allocation<T: Serialize>(
 // Helper
 //-------------------------------------------------------------------------------------------------
 
-// Convert a json string stored in wasm memory into a specified struct
+/// Convert a json string stored in wasm memory into a specified struct
 // If json deserialization of custom struct failed, tries to deserialize a RibosomeErrorReport struct.
 // If that also failed, tries to load a string directly, since we are expecting an error string at this stage.
 #[allow(unknown_lints)]

384cbe9f2dc75890cb0d4e41a2b8fae52d2404e0
Nicolas Luck <nicolas@lucksus.eu>
2018-10-23 00:15:08 +0200

    More lints

--- core_api_c_binding/src/lib.rs
+++ core_api_c_binding/src/lib.rs
@@ -7,8 +7,7 @@ extern crate holochain_core_types;
 extern crate holochain_dna;
 
 use holochain_cas_implementations::{
-    cas::file::FilesystemStorage, eav::file::EavFileStorage,
-    path::{create_path_if_not_exists},
+    cas::file::FilesystemStorage, eav::file::EavFileStorage, path::create_path_if_not_exists,
 };
 use holochain_core::context::Context;
 use holochain_core_api::Holochain;

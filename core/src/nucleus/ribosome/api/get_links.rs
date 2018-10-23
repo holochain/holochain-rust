93e5735ccc7157ed29adc4756502b79014aa9473
Nicolas Luck <nicolas@lucksus.eu>
2018-10-23 20:30:28 +0200

    tests for invoke_get_links

--- core/src/nucleus/ribosome/api/get_links.rs
+++ core/src/nucleus/ribosome/api/get_links.rs
@@ -38,3 +38,100 @@ pub fn invoke_get_links(
 
     runtime.store_utf8(&json)
 }
+
+
+#[cfg(test)]
+pub mod tests {
+    extern crate test_utils;
+    extern crate wabt;
+
+    use agent::actions::commit::commit_entry;
+    use dht::actions::add_link::add_link;
+    use futures::executor::block_on;
+    use holochain_core_types::{
+        cas::content::Address, entry::Entry, entry_type::test_entry_type,
+        links_entry::Link,
+    };
+    use holochain_wasm_utils::api_serialization::{get_links::GetLinksArgs};
+    use instance::tests::{test_context_and_logger, test_instance};
+    use nucleus::ribosome::{
+        api::{tests::*, ZomeApiFunction},
+        Defn,
+    };
+    use serde_json;
+
+    /// dummy link_entries args from standard test entry
+    pub fn test_get_links_args_bytes(base: &Address, tag: &str) -> Vec<u8> {
+        let args = GetLinksArgs {
+            entry_address: base.clone(),
+            tag: String::from(tag),
+        };
+        serde_json::to_string(&args)
+            .expect("args should serialize")
+            .into_bytes()
+    }
+
+
+
+    #[test]
+    fn returns_list_of_links() {
+        let wasm = test_zome_api_function_wasm(ZomeApiFunction::GetLinks.as_str());
+        let dna = test_utils::create_test_dna_with_wasm(
+            &test_zome_name(),
+            &test_capability(),
+            wasm.clone(),
+        );
+
+        let dna_name = &dna.name.to_string().clone();
+        let instance = test_instance(dna).expect("Could not create test instance");
+
+        let (context, logger) = test_context_and_logger("joan");
+        let initialized_context = instance.initialize_context(context);
+
+        let mut entry_hashes: Vec<Address> = Vec::new();
+        for i in 0..3 {
+            let entry = Entry::new(&test_entry_type(), &format!("entry{} value", i));
+            let hash = block_on(commit_entry(
+                entry,
+                &initialized_context.action_channel.clone(),
+                &initialized_context,
+            )).expect("Could not commit entry for testing");
+            entry_hashes.push(hash);
+        }
+
+        let link1 = Link::new(&entry_hashes[0], &entry_hashes[1], "test-tag");
+        let link2 = Link::new(&entry_hashes[0], &entry_hashes[2], "test-tag");
+
+        assert!(block_on(add_link(&link1, &initialized_context)).is_ok());
+        assert!(block_on(add_link(&link2, &initialized_context)).is_ok());
+
+        let (call_result, _) = test_zome_api_function_call(
+            &dna_name,
+            initialized_context.clone(),
+            logger.clone(),
+            &instance,
+            &wasm,
+            test_get_links_args_bytes(&entry_hashes[0], "test-tag"),
+        );
+
+        let ordering1: bool = call_result == format!(r#"{{"ok":true,"links":["{}","{}"],"error":""}}"#, entry_hashes[1], entry_hashes[2]) + "\u{0}";
+        let ordering2: bool = call_result == format!(r#"{{"ok":true,"links":["{}","{}"],"error":""}}"#, entry_hashes[2], entry_hashes[1]) + "\u{0}";
+
+        assert!(ordering1 || ordering2);
+
+        let (call_result, _) = test_zome_api_function_call(
+            &dna_name,
+            initialized_context.clone(),
+            logger.clone(),
+            &instance,
+            &wasm,
+            test_get_links_args_bytes(&entry_hashes[0], "other-tag"),
+        );
+
+        assert_eq!(
+            call_result,
+            r#"{"ok":true,"links":[],"error":""}"#.to_string() + "\u{0}",
+        );
+    }
+
+}

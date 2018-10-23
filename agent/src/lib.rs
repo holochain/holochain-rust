5ebd6de2fbdaa8268624b6a0c5051945ad1f0b5a
damien <doute.d@gmail.com>
2018-10-23 15:31:29 -0400

    Cleanup

--- agent/src/lib.rs
+++ agent/src/lib.rs
@@ -9,7 +9,6 @@ use holochain_core_types::{
     cas::content::{AddressableContent, Content},
     entry::{Entry, ToEntry},
     entry_type::EntryType,
-    //json::{FromJson, ToJson},
 };
 
 /// Object holding an Agent's identity.
@@ -69,7 +68,6 @@ impl ToEntry for Agent {
 
 impl AddressableContent for Agent {
     fn content(&self) -> Content {
-        // self.to_entry().to_json().unwrap()/*.to_string()*/
         self.to_entry().content()
     }
 

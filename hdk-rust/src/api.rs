f28c6be85b5e93f7a27dd6b4105bcca156815268
Connor Turland <connorturland@gmail.com>
2018-10-23 13:38:28 -0400

    fmt

--- hdk-rust/src/api.rs
+++ hdk-rust/src/api.rs
@@ -289,7 +289,7 @@ pub fn link_entries<S: Into<String>>(
 
 /// Not Yet Available
 // Returns a DNA property, which are defined by the DNA developer.
-// They are custom values that are defined in the DNA file 
+// They are custom values that are defined in the DNA file
 // that can be used in the zome code for defining configurable behaviors.
 // (e.g. Name, Language, Description, Author, etc.).
 pub fn property<S: Into<String>>(_name: S) -> ZomeApiResult<String> {

f7f5b09fc7da1c1df2eeef925b8f0e6435de2732
Nicolas Luck <nicolas@lucksus.org>
2018-10-24 16:51:20 -0400

    Update core/src/nucleus/ribosome/api/hash_entry.rs
    Co-Authored-By: ddd-mtl <doute.d@gmail.com>

--- core/src/nucleus/ribosome/api/hash_entry.rs
+++ core/src/nucleus/ribosome/api/hash_entry.rs
@@ -15,7 +15,8 @@ pub fn get_entry_type(dna: &Dna, entry_type_name: &str) -> Result<EntryType, Opt
     if maybe_entry_type.is_err() {
         return Err(Some(RuntimeValue::I32(holochain_core_types::error::RibosomeErrorCode::UnknownEntryType as i32)));
     }
-    let entry_type = maybe_entry_type.unwrap();
+    let entry_type = EntryType::from_str(&entry_type_name)
+  .map_err(|_| Err(Some(RuntimeValue::I32(holochain_core_types::error::RibosomeErrorCode::UnknownEntryType as i32))))?
     // Check if AppEntry is a valid AppEntryType
     if entry_type.is_app() {
         let result = dna.get_entry_type_def(entry_type_name);

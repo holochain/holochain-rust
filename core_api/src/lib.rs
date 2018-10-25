e0df8c30b5a357df67b1528c7a58290317d8a353
Nicolas Luck <nicolas@lucksus.eu>
2018-10-25 19:11:41 +0200

    Fix example code

--- core_api/src/lib.rs
+++ core_api/src/lib.rs
@@ -33,7 +33,7 @@
 //! let context = Context::new(
 //!     agent,
 //!     Arc::new(Mutex::new(SimpleLogger {})),
-//!     Arc::new(Mutex::new(SimplePersister::new())),
+//!     Arc::new(Mutex::new(SimplePersister::new(String::from("Agent Name")))),
 //!     FilesystemStorage::new(tempdir().unwrap().path().to_str().unwrap()).unwrap(),
 //!     EavFileStorage::new(tempdir().unwrap().path().to_str().unwrap().to_string()).unwrap(),
 //!  ).unwrap();

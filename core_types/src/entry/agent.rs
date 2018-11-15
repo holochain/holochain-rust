e2e56365e0165b03b56151bc1c6bfff94792387a
neonphog <neonphog@gmail.com>
2018-11-15 10:26:28 -0700

    comments

--- core_types/src/entry/agent.rs
+++ core_types/src/entry/agent.rs
@@ -1,3 +1,5 @@
+//! Represents an agent entry in the cas
+
 use cas::content::{Address, AddressableContent, Content};
 use entry::{Entry, ToEntry};
 use entry_type::EntryType;
@@ -10,10 +12,18 @@ use reed_solomon::{Decoder, Encoder};
 
 const PARITY_LEN: usize = 2;
 
+/// A raw public key buffer
+/// Can extract the signature and encryption portions
+/// Can parse a base64url encoded user representation
+/// Can render a base64url encoded user representation
 #[derive(Clone)]
 pub struct KeyBuffer([u8; 64]);
 
 impl KeyBuffer {
+    /// take a potentially user-entered base64url encoded user representation
+    /// of an public key identity
+    /// apply reed-solomon parity correction
+    /// returns a raw byte buffer
     pub fn with_corrected(s: &str) -> Result<KeyBuffer, HolochainError> {
         let s = s.replace("-", "+").replace("_", "/");
         let s = base64::decode(&s)?;
@@ -22,32 +32,45 @@ impl KeyBuffer {
         Ok(KeyBuffer::with_raw(array_ref![dec, 0, 64]))
     }
 
+    /// generate a key buffer from raw bytes (no correction)
     pub fn with_raw(b: &[u8; 64]) -> KeyBuffer {
         KeyBuffer(b.clone())
     }
 
+    /// render a base64url encoded user identity with reed-solomon parity bytes
     pub fn render(&self) -> String {
         let enc = Encoder::new(PARITY_LEN);
         let enc = *enc.encode(&self.0);
         base64::encode(&enc[..]).replace("+", "-").replace("/", "_")
     }
 
+    /// get the signature public key portion of this buffer
     pub fn get_sig(&self) -> &[u8; 32] {
         array_ref![self.0, 0, 32]
     }
 
+    /// get the encryption public key portion of this buffer
     pub fn get_enc(&self) -> &[u8; 32] {
         array_ref![self.0, 32, 32]
     }
 }
 
+/// agent data that can be stored in the cas
+/// note thate the "address" of an agent entry is the base64url encoded
+/// public key identity string
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, DefaultJson)]
 pub struct Agent {
+    /// a nickname for referencing this agent
     pub nick: String,
+    /// the base64url encoded public identity string for this agent
     pub key: String,
 }
 
 impl Agent {
+    /// generate a fake testing agent
+    /// `s` will be used for the `nick` and included in the key string as well
+    /// this agent is not cryptographically generated...
+    /// it will not be able to sign / encrypt anything
     pub fn generate_fake(s: &str) -> Self {
         let mut buf = s.to_string();
         while buf.len() < 84 {
@@ -60,6 +83,7 @@ impl Agent {
         Agent::new(s, &buf)
     }
 
+    /// initialize an Agent struct with `nick` and `key`
     pub fn new(nick: &str, key: &KeyBuffer) -> Self {
         Agent {
             nick: nick.to_string(),
@@ -67,6 +91,7 @@ impl Agent {
         }
     }
 
+    /// get a key buffer based on this agent's key (no correction)
     pub fn to_buffer(&self) -> KeyBuffer {
         let key = base64::decode(&self.key).expect("corrupt identity key");
         KeyBuffer::with_raw(array_ref![key, 0, 64])
@@ -74,10 +99,12 @@ impl Agent {
 }
 
 impl ToEntry for Agent {
+    /// convert Agent to an entry
     fn to_entry(&self) -> Entry {
         Entry::new(EntryType::AgentId, JsonString::from(self))
     }
 
+    /// build an Agent from an entry
     fn from_entry(entry: &Entry) -> Self {
         assert_eq!(&EntryType::AgentId, entry.entry_type());
         match Agent::try_from(entry.value().to_owned()) {
@@ -88,14 +115,17 @@ impl ToEntry for Agent {
 }
 
 impl AddressableContent for Agent {
+    /// for an Agent, the address is their public base64url encoded itentity string
     fn address(&self) -> Address {
         self.key.clone().into()
     }
 
+    /// get the entry content
     fn content(&self) -> Content {
         self.to_entry().content()
     }
 
+    /// build from entry content
     fn from_content(content: &Content) -> Self {
         Agent::from_entry(&Entry::from_content(content))
     }

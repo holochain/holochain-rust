b12e1ecdd45363795444fc4ce5d3a6edb3ea7b40
David Meister <thedavidmeister@gmail.com>
2018-11-15 23:00:09 +1100

    derivation for fmt

--- shell.nix
+++ shell.nix
@@ -2,22 +2,23 @@
 # so we can access the `pkgs` and `stdenv` variables
 with import <nixpkgs> {};
 
-stdenv.mkDerivation {
+let
+  rustVersionTools = "nightly-2018-07-17";
+  hc-fmt = pkgs.writeShellScriptBin "hc-fmt" "rustup run ${rustVersionTools} cargo fmt";
+in
+stdenv.mkDerivation rec {
   name = "holochain-rust-environment";
 
   buildInputs = [
-  pkgs.zeromq3
-  pkgs.cargo
-  pkgs.rustup
-  pkgs.rustfmt
-  pkgs.rustc
-  pkgs.gnumake
-  pkgs.gcc
-  pkgs.binutils-unwrapped
+    pkgs.zeromq3
+    pkgs.cargo
+    pkgs.rustup
+    pkgs.rustfmt
+    pkgs.rustc
+    pkgs.gnumake
+    pkgs.gcc
+    pkgs.binutils-unwrapped
+    hc-fmt
   ];
 
-  # The '' quotes are 2 single quote characters
-  # They are used for multi-line strings
-  shellHook = ''
-  '';
 }

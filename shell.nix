00f1ef06a6c2e1a8c50a3f6c5e39fe3f00438ef9
David Meister <thedavidmeister@gmail.com>
2018-11-16 00:10:32 +1100

    fix zmq bug

--- shell.nix
+++ shell.nix
@@ -3,22 +3,27 @@
 with import <nixpkgs> {};
 
 let
+  rustVersionCore = "nightly-2018-10-12";
   rustVersionTools = "nightly-2018-07-17";
   hc-fmt = pkgs.writeShellScriptBin "hc-fmt" "rustup run ${rustVersionTools} cargo fmt";
+  hc-test = pkgs.writeShellScriptBin "hc-test" "rustup run ${rustVersionCore} cargo test";
 in
 stdenv.mkDerivation rec {
   name = "holochain-rust-environment";
 
   buildInputs = [
-    pkgs.zeromq3
-    pkgs.cargo
+    pkgs.zeromq
     pkgs.rustup
+    pkgs.cargo
     pkgs.rustfmt
     pkgs.rustc
     pkgs.gnumake
     pkgs.gcc
     pkgs.binutils-unwrapped
+    pkgs.pkgconfig
+    pkgs.python
     hc-fmt
+    hc-test
   ];
 
 }

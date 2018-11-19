44ac32c25048c4fc555f72b4fef96d846f2b9562
Nicolas Luck <nicolas@lucksus.eu>
2018-11-19 18:25:51 +0100

    LinkValidationArgs

--- wasm_utils/src/api_serialization/validation.rs
+++ wasm_utils/src/api_serialization/validation.rs
@@ -1,4 +1,7 @@
-use holochain_core_types::{error::HolochainError, json::*, validation::ValidationData};
+use holochain_core_types::{
+    link::Link,
+    error::HolochainError, json::*, validation::ValidationData
+};
 
 #[derive(Deserialize, Debug, Serialize, DefaultJson)]
 pub struct EntryValidationArgs {
@@ -18,4 +21,12 @@ pub struct LinkValidationPackageArgs {
     pub entry_type: String,
     pub tag: String,
     pub direction: LinkDirection
+}
+
+#[derive(Deserialize, Debug, Serialize, DefaultJson, Clone)]
+pub struct LinkValidationArgs {
+    pub entry_type: String,
+    pub link: Link,
+    pub direction: LinkDirection,
+    pub validation_data: ValidationData,
 }

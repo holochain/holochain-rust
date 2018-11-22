d5e182c0c9c23633748a4a3993061e1cb4c703d5
Connor Turland <connorturland@gmail.com>
2018-11-21 20:42:23 -0500

    path stuff in script file wasn't working

--- scripts/configure_windows.sh
+++
@@ -1,9 +0,0 @@
-choco install python2
-choco install nodist
-export NODE_PATH="/c/Program Files (x86)\Nodist\bin\node_modules;$NODE_PATH"
-export NODIST_PREFIX="/c/Program Files (x86)\Nodist"
-export NODIST_X64=1
-export PATH="/c/Program Files (x86)/Nodist/bin:$PATH"
-export PATH="/c/Python27:/c/Python27/Scripts:$PATH"
-nodist add $TRAVIS_NODE_VERSION
-nodist $TRAVIS_NODE_VERSION

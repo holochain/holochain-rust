c1ab8df8ec179be4632dd0f3bbc9b6cd3d47845a
damien <doute.d@gmail.com>
2018-11-22 11:31:42 -0500

    minor cleanup of printfs

--- hdk-rust/wasm-test/src/handle_crud.rs
+++ hdk-rust/wasm-test/src/handle_crud.rs
@@ -15,54 +15,54 @@ use TestEntryType;
 
 pub(crate) fn handle_update_entry_ok() -> JsonString {
     // Commit v1 entry
-    hdk::debug("\n **** Commit v1 entry:").ok();
+    hdk::debug("**** Commit v1 entry:").ok();
     let res = hdk::commit_entry(&hdk_test_entry());
     let addr_v1 = res.unwrap();
     // get it
-    hdk::debug("\n **** Get it:").ok();
+    hdk::debug("**** Get it:").ok();
     let res = hdk::get_entry(addr_v1.clone());
     let entry_v1 = res.unwrap().unwrap();
 
     // update it to v2
-    hdk::debug("\n **** update it to v2:").ok();
+    hdk::debug("**** update it to v2:").ok();
     let entry_v2 =
         Entry::new(hdk_test_entry_type(), TestEntryType { stuff: "v2".into() });
     let res = hdk::update_entry(entry_v2.clone(), addr_v1.clone());
     let addr_v2 = res.unwrap();
     // get latest from latest
-    hdk::debug("\n **** get latest from latest").ok();
+    hdk::debug("**** get latest from latest").ok();
     let res = hdk::get_entry(addr_v2.clone());
     let entry_res = res.unwrap().unwrap();
     assert_eq!(entry_res, entry_v2.clone());
     // get latest from initial
-    hdk::debug("\n **** get latest from initial").ok();
+    hdk::debug("**** get latest from initial").ok();
     let res = hdk::get_entry(addr_v1.clone());
     let entry_res = res.unwrap().unwrap();
     assert_eq!(entry_res, entry_v2.clone());
     // get initial from latest
-    hdk::debug("\n **** get initial from latest").ok();
+    hdk::debug("**** get initial from latest").ok();
     let res = hdk::get_entry_initial(addr_v2.clone());
     let entry_res = res.unwrap().unwrap();
     assert_eq!(entry_res, entry_v2.clone());
     // get initial from initial
-    hdk::debug("\n **** get initial from initial").ok();
+    hdk::debug("**** get initial from initial").ok();
     let res = hdk::get_entry_initial(addr_v1.clone());
     let entry_res = res.unwrap().unwrap();
     assert_eq!(entry_res, entry_v1.clone());
 
     // update it again from v1
-    hdk::debug("\n **** update it again from v1").ok();
+    hdk::debug("**** update it again from v1").ok();
     let entry_v3 =
         Entry::new(hdk_test_entry_type(), TestEntryType { stuff: "v3".into() });
     let res = hdk::update_entry(entry_v3.clone(), addr_v1.clone());
     let addr_v3 = res.unwrap();
     // get latest from v1
-    hdk::debug("\n **** get latest from v1").ok();
+    hdk::debug("**** get latest from v1").ok();
     let res = hdk::get_entry(addr_v1.clone());
     let entry_res = res.unwrap().unwrap();
     assert_eq!(entry_res, entry_v3.clone());
     // get latest from v2
-    hdk::debug("\n **** get latest from v2").ok();
+    hdk::debug("**** get latest from v2").ok();
     let res = hdk::get_entry(addr_v2.clone());
     let entry_res = res.unwrap().unwrap();
     assert_eq!(entry_res, entry_v3.clone());
@@ -101,13 +101,13 @@ pub(crate) fn handle_update_entry_ok() -> JsonString {
     let entry_res = res.unwrap().unwrap();
     assert_eq!(entry_res, entry_v3.clone());
     // get initial from v4
-    hdk::debug("\n **** get initial from v4").ok();
+    hdk::debug("**** get initial from v4").ok();
     let res = hdk::get_entry_initial(addr_v4.clone());
     let entry_res = res.unwrap().unwrap();
     assert_eq!(entry_res, entry_v4.clone());
 
     // get history from latest
-    hdk::debug("\n **** get history from latest").ok();
+    hdk::debug("**** get history from latest").ok();
     let res = hdk::get_entry_history(addr_v4.clone());
     let latest = res.unwrap().unwrap();
     assert_eq!(latest.entries.len(), 1);
@@ -117,7 +117,7 @@ pub(crate) fn handle_update_entry_ok() -> JsonString {
     assert_eq!(latest.crud_links.len(), 0);
 
     // get history from initial
-    hdk::debug("\n **** get history from initial").ok();
+    hdk::debug("**** get history from initial").ok();
     let res = hdk::get_entry_history(addr_v1.clone());
     let history = res.unwrap().unwrap();
 
@@ -148,25 +148,25 @@ pub(crate) fn handle_update_entry_ok() -> JsonString {
 //
 pub fn handle_remove_entry_ok() -> JsonString {
     // Commit v1 entry
-    hdk::debug("\n **** Commit v1 entry").ok();
+    hdk::debug("**** Commit v1 entry").ok();
     let entry_test = hdk_test_entry();
     let res = hdk::commit_entry(&entry_test);
     let addr_v1 = res.unwrap();
     // Get it
-    hdk::debug("\n **** Get it").ok();
+    hdk::debug("**** Get it").ok();
     let res = hdk::get_entry(addr_v1.clone());
     let entry_v1 = res.unwrap().unwrap();
     assert_eq!(entry_test, entry_v1);
     // Delete it
-    hdk::debug("\n **** Delete it:").ok();
+    hdk::debug("**** Delete it").ok();
     let res = hdk::remove_entry(addr_v1.clone());
     assert!(res.is_ok());
     // Get it should fail
-    hdk::debug("\n **** Get it should fail:").ok();
+    hdk::debug("**** Get it should fail").ok();
     let res = hdk::get_entry(addr_v1.clone());
     assert_eq!(res.unwrap(), None);
     // Delete it again should fail
-    hdk::debug("\n **** Delete it again should fail:").ok();
+    hdk::debug("**** Delete it again should fail").ok();
     let res = hdk::remove_entry(addr_v1.clone());
     assert!(res.is_err());
     // get entry_result

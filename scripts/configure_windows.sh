8a9c54f5fbc49a11167888af3b65f4d1515ecce6
Connor Turland <connor.turland@holo.host>
2018-11-21 19:13:42 -0500

    Update configure_windows.sh

--- scripts/configure_windows.sh
+++ scripts/configure_windows.sh
@@ -3,7 +3,7 @@ choco install nodist
 export NODE_PATH="/c/Program Files (x86)\Nodist\bin\node_modules;$NODE_PATH"
 export NODIST_PREFIX="/c/Program Files (x86)\Nodist"
 export NODIST_X64=1
-nodist add $TRAVIS_NODE_VERSION
-nodist $TRAVIS_NODE_VERSION
 export PATH="/c/Program Files (x86)/Nodist/bin:$PATH"
 export PATH="/c/Python27:/c/Python27/Scripts:$PATH"
+nodist add $TRAVIS_NODE_VERSION
+nodist $TRAVIS_NODE_VERSION

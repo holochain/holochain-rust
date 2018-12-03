9f8d411917aa4dc192b5e601fc6dfa5c391cedd6
Julian Laubstein <contact@julianlaubstein.de>
2018-12-03 10:17:55 +0100

    Small refactorin

--- core_types/src/dna/zome/entry_types.rs
+++ core_types/src/dna/zome/entry_types.rs
@@ -109,17 +109,17 @@ where
     map.end()
 }
 
-pub fn deserialize_entry_types<'de, D>(deserializer: D) -> Result<(ZomeEntryTypes), D::Error>
+pub fn deserialize_entry_types<'de, D>(deserializer: D) -> Result<ZomeEntryTypes, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let serialized_entry_types = BTreeMap::<String, EntryTypeDef>::deserialize(deserializer)?;
+    let serialized_entry_types: BTreeMap<String, EntryTypeDef> =
+        BTreeMap::deserialize(deserializer)?;
 
-    let mut map = BTreeMap::new();
-    for (k, v) in serialized_entry_types {
-        map.insert(EntryType::from(k), v);
-    }
-    Ok(map)
+    Ok(serialized_entry_types
+        .into_iter()
+        .map(|(k, v)| (EntryType::from(k), v))
+        .collect())
 }
 
 /// Represents an individual object in the "zome" "entry_types" array.

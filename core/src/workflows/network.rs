2ab320fc98944d422767ec0867139b6852c94674
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2018-12-06 13:31:53 -0500

    Renamed variable

--- core/src/workflows/network.rs
+++ core/src/workflows/network.rs
@@ -12,8 +12,8 @@ use holochain_core_types::error::HolochainError;
 
 pub async fn initialize(context:Arc<Context>) -> Result<(), HolochainError>
 {
-    let agent_dna = await!(initialize_network::get_dna_and_agent(&context))?;
-    if agent_dna.0.is_empty() && agent_dna.1.is_empty()
+    let agent_dna_pair = await!(initialize_network::get_dna_and_agent(&context))?;
+    if agent_dna_pair.0.is_empty() && agent_dna_pair.1.is_empty()
     {
         Ok(await!(initialize_network::initialize_network(&context))?)
     }

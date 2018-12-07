f49e72b1dd587c71e0784b05c1940a12233541bf
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2018-12-06 19:33:49 -0500

    Update from tuple to clean up and make look cleaner

--- core/src/workflows/network.rs
+++ core/src/workflows/network.rs
@@ -12,8 +12,8 @@ use holochain_core_types::error::HcResult;
 
 pub async fn initialize(context:Arc<Context>) -> HcResult<()>
 {
-    let agent_dna_pair = await!(get_dna_and_agent(&context))?;
-    if agent_dna_pair.0.is_empty() && agent_dna_pair.1.is_empty()
+    let (agent_id,dna_id) = await!(get_dna_and_agent(&context))?;
+    if agent_id.is_empty() && dna_id.is_empty()
     {
         Ok(await!(initialize_network::initialize_network(&context))?)
     }

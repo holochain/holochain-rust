7d621a877bffed339b79ab28fafc1b96aa41c56e
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2018-12-07 10:12:04 -0500

    Added more checks in workflow

--- core/src/workflows/network.rs
+++ core/src/workflows/network.rs
@@ -7,19 +7,17 @@ use std::{
     sync::Arc,
 };
 
-use holochain_core_types::error::HcResult;
+use holochain_core_types::error::{HcResult,HolochainError};
 
 
 pub async fn initialize(context:Arc<Context>) -> HcResult<()>
 {
     let (agent_id,dna_id) = await!(get_dna_and_agent(&context))?;
-    if agent_id.is_empty() && dna_id.is_empty()
-    {
-        Ok(await!(initialize_network::initialize_network(&context))?)
-    }
-    else
-    {
-        Ok(())
-    }
 
+    match (agent_id.is_empty(),dna_id.is_empty())
+    {
+        (true,true) =>{Ok(await!(initialize_network::initialize_network(&context))?)},
+        (true,false) =>{Err(HolochainError::DnaMissing)},
+        (_,_) =>{Ok(())}
+    }
 }

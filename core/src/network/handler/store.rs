35088fdc87b0f2f2b34e969f012370012f4663a4
Nicolas Luck <nicolas@lucksus.eu>
2018-12-10 19:27:07 +0100

    Log errors occuring during hold_entry_workflow()

--- core/src/network/handler/store.rs
+++ core/src/network/handler/store.rs
@@ -16,7 +16,10 @@ pub fn handle_store_dht(dht_data: DhtData, context: Arc<Context>) {
     let entry_with_header: EntryWithHeader =
         serde_json::from_str(&serde_json::to_string(&dht_data.content).unwrap()).unwrap();
     thread::spawn(move || {
-        let result = block_on(hold_entry_workflow(&entry_with_header, &context.clone()));
+        match block_on(hold_entry_workflow(&entry_with_header, &context.clone())) {
+            Err(error) => context.log(error),
+            _ => (),
+        }
     });
 }
 

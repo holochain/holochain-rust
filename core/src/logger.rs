1467ac57e3cb3fc863807e29dc824c74608449ac
JhonatanHern <jhonatanhernandez998@gmail.com>
2018-12-11 17:17:47 -0400

    added test for logger

--- core/src/logger.rs
+++ core/src/logger.rs
@@ -1,26 +1,21 @@
 //! This logger is the logger that's attached to each Holochain application
 //! which is separate from standard logging via the log crate warn! info! debug! logging that
 //! gets emitted globaly from the container.
-
 use chrono::Local;
-
-/// trait that defines the logging functionality that holochain_core requires
+ /// trait that defines the logging functionality that holochain_core requires
 pub trait Logger: Send {
     // Add log message to logger
     fn log(&mut self, msg: String);
-
-    // Dump all held logs
+     // Dump all held logs
     fn dump(&self) -> String {
         String::new()
     }
 }
-
 #[derive(Clone)]
 pub struct SimpleLogger {
     // log: Vec<String>,
 }
-
-// ignore this in test coverage as it is only side effects
+ // ignore this in test coverage as it is only side effects
 #[cfg_attr(tarpaulin, skip)]
 impl Logger for SimpleLogger {
     fn log(&mut self, msg: String) {
@@ -30,4 +25,16 @@ impl Logger for SimpleLogger {
     // fn new() -> SimpleLogger {
     //      SimpleLogger {}
     // }
+}
+#[cfg(test)]
+pub mod tests {
+    use crate::{
+        logger::SimpleLogger,
+        logger::Logger
+    };
+    #[test]
+    fn test_logger() {
+        let mut logger_test = SimpleLogger{};
+        logger_test.log("Example Log".to_string());
+    }
 }

7a067dfd3e364b4a2f5c2068ed20f0faf1093859
Connor Turland <connorturland@gmail.com>
2018-12-20 12:32:50 -0500

    and error and entry mod comments

--- core_types/src/error/mod.rs
+++ core_types/src/error/mod.rs
@@ -1,4 +1,5 @@
-//! ?
+//! This module contains Error type definitions that are used throughout Holochain, and the Ribosome in particular,
+//! which is responsible for mounting and running instances of DNA, and executing WASM code.
 
 mod dna_error;
 pub mod error;

2f24db1ed58d912ff490dfe2d4fa35b127a9fa5c
David Meister <thedavidmeister@gmail.com>
2018-12-29 21:29:34 +1100

    debug paths

--- container_api/src/container.rs
+++ container_api/src/container.rs
@@ -681,7 +681,7 @@ pub mod tests {
 
     fn caller_dna() -> Dna {
         let wasm = create_wasm_from_file(
-            "/tmp/holochain/target/wasm32-unknown-unknown/release/test_bridge_caller.wasm",
+            "/tmp/holochain/container_api/test-bridge-caller/target/wasm32-unknown-unknown/release/test_bridge_caller.wasm",
         );
         let capabability = create_test_cap_with_fn_name("call_bridge");
         let mut dna = create_test_dna_with_cap("main", "main", &capabability, &wasm);

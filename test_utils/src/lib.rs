bcdc5b014e2042d7f6c65e70763581ac84a82c35
David Meister <thedavidmeister@gmail.com>
2018-12-30 20:40:14 +1100

    debug

--- test_utils/src/lib.rs
+++ test_utils/src/lib.rs
@@ -42,7 +42,10 @@ use wabt::Wat2Wasm;
 
 /// Load WASM from filesystem
 pub fn create_wasm_from_file(fname: &str) -> Vec<u8> {
-    let mut file = File::open(fname).unwrap();
+    let mut file = match File::open(fname){
+        Ok(file) = file,
+        Err(e) => panic!("Failed to open {}, error: {}", fname, e),
+    };
     let mut buf = Vec::new();
     file.read_to_end(&mut buf).unwrap();
     buf

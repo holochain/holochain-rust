0fa7ec5de0df38ee82f8d5da5d299707d04a6565
Michael Dougherty <maackle.d@gmail.com>
2019-01-03 10:02:30 -0800

    Rename app -> alice, app2 -> tash

--- app_spec/test/test.js
+++ app_spec/test/test.js
@@ -8,12 +8,12 @@ const dnaPath = "./dist/app_spec.hcpkg"
 // IIFE to keep config-only stuff out of test scope
 const container = (() => {
   const agentAlice = Config.agent("alice")
-  const agentBob = Config.agent("bob")
+  const agentTash = Config.agent("tash")
 
   const dna = Config.dna(dnaPath)
 
   const instanceAlice = Config.instance(agentAlice, dna)
-  const instanceBob = Config.instance(agentBob, dna)
+  const instanceBob = Config.instance(agentTash, dna)
 
   const containerConfig = Config.container(instanceAlice, instanceBob)
   return new Container(containerConfig)
@@ -22,13 +22,13 @@ const container = (() => {
 // Initialize the Container
 container.start()
 
-const app = container.makeCaller('alice', dnaPath)
-const app2 = container.makeCaller('bob', dnaPath)
+const alice = container.makeCaller('alice', dnaPath)
+const tash = container.makeCaller('tash', dnaPath)
 
 test('agentId', (t) => {
   t.plan(2)
-  t.ok(app.agentId)
-  t.notEqual(app.agentId, app2.agentId)
+  t.ok(alice.agentId)
+  t.notEqual(alice.agentId, tash.agentId)
 })
 
 test('call', (t) => {
@@ -37,7 +37,7 @@ test('call', (t) => {
   const num1 = 2
   const num2 = 2
   const params = { num1, num2 }
-  const result = app.call("blog", "main", "check_sum", params)
+  const result = alice.call("blog", "main", "check_sum", params)
 
   t.deepEqual(result.Ok, { "sum": "4" })
 })
@@ -46,7 +46,7 @@ test('hash_post', (t) => {
   t.plan(1)
 
   const params = { content: "Holo world" }
-  const result = app.call("blog", "main", "post_address", params)
+  const result = alice.call("blog", "main", "post_address", params)
 
   t.equal(result.Ok, "QmY6MfiuhHnQ1kg7RwNZJNUQhwDxTFL45AAPnpJMNPEoxk")
 })
@@ -57,7 +57,7 @@ test('create_post', (t) => {
   const content = "Holo world"
   const in_reply_to = null
   const params = { content, in_reply_to }
-  const result = app.call("blog", "main", "create_post", params)
+  const result = alice.call("blog", "main", "create_post", params)
 
   t.ok(result.Ok)
   t.notOk(result.Err)
@@ -70,7 +70,7 @@ test('create_post with bad reply to', (t) => {
   const content = "Holo world"
   const in_reply_to = "bad"
   const params = { content, in_reply_to }
-  const result = app.call("blog", "main", "create_post", params)
+  const result = alice.call("blog", "main", "create_post", params)
 
   // bad in_reply_to is an error condition
   t.ok(result.Err)
@@ -87,7 +87,7 @@ test('post max content size 280 characters', (t) => {
   const content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   const in_reply_to = null
   const params = { content, in_reply_to }
-  const result = app.call("blog", "main", "create_post", params)
+  const result = alice.call("blog", "main", "create_post", params)
 
   // result should be an error
   t.ok(result.Err);
@@ -106,7 +106,7 @@ test('posts_by_agent', (t) => {
   const agent = "Bob"
   const params = { agent }
 
-  const result = app.call("blog", "main", "posts_by_agent", params)
+  const result = alice.call("blog", "main", "posts_by_agent", params)
 
   t.deepEqual(result.Ok, { "addresses": [] })
 })
@@ -114,16 +114,16 @@ test('posts_by_agent', (t) => {
 test('my_posts', async (t) => {
   t.plan(1)
 
-  app.call("blog", "main", "create_post",
+  alice.call("blog", "main", "create_post",
     { "content": "Holo world", "in_reply_to": "" }
   )
 
-  app.call("blog", "main", "create_post",
+  alice.call("blog", "main", "create_post",
     { "content": "Another post", "in_reply_to": "" }
   )
 
   const result = await pollFor(
-    () => app.call("blog", "main", "my_posts", {}),
+    () => alice.call("blog", "main", "my_posts", {}),
     (result) => {
       return result &&
         result.Ok &&
@@ -141,11 +141,11 @@ test('create/get_post roundtrip', (t) => {
   const content = "Holo world"
   const in_reply_to = null
   const params = { content, in_reply_to }
-  const create_post_result = app.call("blog", "main", "create_post", params)
+  const create_post_result = alice.call("blog", "main", "create_post", params)
   const post_address = create_post_result.Ok
 
   const params_get = { post_address }
-  const result = app.call("blog", "main", "get_post", params_get)
+  const result = alice.call("blog", "main", "get_post", params_get)
 
   const entry_value = JSON.parse(result.Ok.App[1])
   t.comment("get_post() entry_value = " + entry_value + "")
@@ -159,7 +159,7 @@ test('get_post with non-existant address returns null', (t) => {
 
   const post_address = "RANDOM"
   const params_get = { post_address }
-  const result = app.call("blog", "main", "get_post", params_get)
+  const result = alice.call("blog", "main", "get_post", params_get)
 
   // should be Ok value but null
   // lookup did not error
@@ -174,12 +174,12 @@ test('scenario test create & publish post -> get from other instance', async (t)
   const content = "Holo world"
   const in_reply_to = null
   const params = { content, in_reply_to }
-  const create_result = app.call("blog", "main", "create_post", params)
+  const create_result = alice.call("blog", "main", "create_post", params)
   t.comment("create_result = " + create_result.address + "")
 
   const content2 = "post 2"
   const params2 = { content2, in_reply_to }
-  const create_result2 = app2.call("blog", "main", "create_post", params2)
+  const create_result2 = tash.call("blog", "main", "create_post", params2)
   t.comment("create_result2 = " + create_result2.address + "")
 
   t.equal(create_result.Ok.length, 46)
@@ -189,7 +189,7 @@ test('scenario test create & publish post -> get from other instance', async (t)
   const params_get = { post_address }
 
   const result = await pollFor(
-    () => app2.call("blog", "main", "get_post", params_get)
+    () => tash.call("blog", "main", "get_post", params_get)
   ).catch(t.fail)
   const value = JSON.parse(result.Ok.App[1])
   t.equal(value.content, content)

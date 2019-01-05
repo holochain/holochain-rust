58cb23f1da81edef297e9c6ac2d9ebf4c5c771c1
Michael Dougherty <maackle.d@gmail.com>
2019-01-04 18:20:58 -0800

    Missed one

--- nodejs_container/native/src/config.rs
+++ nodejs_container/native/src/config.rs
@@ -58,7 +58,7 @@ fn make_config(instance_data: Vec<InstanceData>) -> Configuration {
             logger_type: String::from("DONTCARE"),
             file: None,
         };
-        let network_mock = Some(P2pConfig::DEFAULT_MOCK_CONFIG.to_string());
+        let network_mock = Some(P2pConfig::default_mock_config("TODO make unique name"));
         let agent_id = agent_config.id.clone();
         let dna_id = dna_config.id.clone();
         let instance = InstanceConfiguration {

4ee449a808b2fc31ff78dbb8dd69ca0db60c44d4
Eric Harris-Braun <eric@harris-braun.com>
2019-01-07 13:11:02 -0500

    cleanup dups of TestLogger and added a test

--- core/src/logger.rs
+++ core/src/logger.rs
@@ -2,6 +2,8 @@
 //! which is separate from standard logging via the log crate warn! info! debug! logging that
 //! gets emitted globaly from the container.
 use chrono::Local;
+use std::sync::{Arc, Mutex};
+
 /// trait that defines the logging functionality that holochain_core requires
 pub trait Logger: Send {
     // Add log message to logger
@@ -22,16 +24,42 @@ impl Logger for SimpleLogger {
         let date = Local::now();
         println!("{}:{}", date.format("%Y-%m-%d %H:%M:%S"), msg);
     }
-    // fn new() -> SimpleLogger {
-    //      SimpleLogger {}
-    // }
 }
+
+/// create a test logger
+pub fn test_logger() -> Arc<Mutex<TestLogger>> {
+    Arc::new(Mutex::new(TestLogger { log: Vec::new() }))
+}
+
+#[derive(Clone, Debug)]
+pub struct TestLogger {
+    pub log: Vec<String>,
+}
+
+impl Logger for TestLogger {
+    fn log(&mut self, msg: String) {
+        self.log.push(msg);
+    }
+    fn dump(&self) -> String {
+        format!("{:?}", self.log)
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
-    use crate::logger::{Logger, SimpleLogger};
+    use super::*;
+
     #[test]
-    fn test_logger() {
+    fn simple_logger_smoke_test() {
         let mut logger_test = SimpleLogger {};
         logger_test.log("Example Log".to_string());
     }
+
+    #[test]
+    fn test_test_logger() {
+        let mut logger = TestLogger { log: Vec::new() };
+        logger.log("test".to_string());
+        assert_eq!(logger.dump(), "[\"test\"]".to_string());
+    }
+
 }

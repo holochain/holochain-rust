c509f3db519353b81f87131720e3807f607cbb7d
David Meister <thedavidmeister@gmail.com>
2019-01-08 12:35:10 +1100

    try dropping LIBZMQ_PREFIX from travis

--- zmq-sys/build.rs
+++ zmq-sys/build.rs
@@ -3,34 +3,39 @@ extern crate metadeps;
 use std::env;
 use std::path::Path;
 
-fn prefix_dir(env_name: &str, dir: &str) -> Option<String> {
-    env::var(env_name).ok().or_else(|| {
-        env::var("LIBZMQ_PREFIX").ok()
-            .map(|prefix| Path::new(&prefix).join(dir))
-            .and_then(|path| path.to_str().map(|p| p.to_owned()))
-    })
-}
+// fn prefix_dir(env_name: &str, dir: &str) -> Option<String> {
+//     env::var(env_name).ok().or_else(|| {
+//         env::var("LIBZMQ_PREFIX").ok()
+//             .map(|prefix| Path::new(&prefix).join(dir))
+//             .and_then(|path| path.to_str().map(|p| p.to_owned()))
+//     })
+// }
 
 fn main() {
     println!("fooo!");
-    let lib_path = prefix_dir("LIBZMQ_LIB_DIR", "lib");
-    let include = prefix_dir("LIBZMQ_INCLUDE_DIR", "include");
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let prefix_dir = format!("{}/zmq-sys/vendor/zmq", dir);
+    println!("cargo:rustc-link-search=native={}/lib", &prefix_dir);
+    println!("cargo:include={}/include", &prefix_dir);
 
-    match (lib_path, include) {
-        (Some(lib_path), Some(include)) => {
-            println!("cargo:rustc-link-search=native={}", lib_path);
-            println!("cargo:include={}", include);
-        }
-        (Some(_), None) => {
-            panic!("Unable to locate libzmq include directory.")
-        }
-        (None, Some(_)) => {
-            panic!("Unable to locate libzmq library directory.")
-        }
-        (None, None) => {
-            if let Err(e) = metadeps::probe() {
-                panic!("Unable to locate libzmq:\n{}", e);
-            }
-        }
-    }
+    // let lib_path = prefix_dir("LIBZMQ_LIB_DIR", "lib");
+    // let include = prefix_dir("LIBZMQ_INCLUDE_DIR", "include");
+    //
+    // match (lib_path, include) {
+    //     (Some(lib_path), Some(include)) => {
+    //         println!("cargo:rustc-link-search=native={}", lib_path);
+    //         println!("cargo:include={}", include);
+    //     }
+    //     (Some(_), None) => {
+    //         panic!("Unable to locate libzmq include directory.")
+    //     }
+    //     (None, Some(_)) => {
+    //         panic!("Unable to locate libzmq library directory.")
+    //     }
+    //     (None, None) => {
+    //         if let Err(e) = metadeps::probe() {
+    //             panic!("Unable to locate libzmq:\n{}", e);
+    //         }
+    //     }
+    // }
 }

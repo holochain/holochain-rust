39f1081e7d8f952a4593d58ff78a25ea90d7206f
David Meister <thedavidmeister@gmail.com>
2019-01-09 14:24:32 +1100

    testing 0.0.3 install instructions

--- scripts/install/ubuntu.sh
+++ scripts/install/ubuntu.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y \
-  cmake \
-  curl \
-  sudo \
-  pkg-config \
-  libssl1.0-dev \
-  libzmq3-dev \
-  python2.7 \
-  qt5-default \
+# apt-get update && apt-get install -y \
+#   cmake \
+#   curl \
+#   sudo \
+#   pkg-config \
+#   libssl1.0-dev \
+#   libzmq3-dev \
+#   python2.7 \
+#   qt5-default \
+
+apt-get update && \
+apt-get install -y \
+  git \
+  build-essential \
+  libssl-dev \
+  curl

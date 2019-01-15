496e63193a1a8fd11b7588667c4f015495ad2070
zo-el <joelulahanna@gmail.com>
2019-01-15 12:12:01 -0600

    added comments to the bundls.rs

--- hc_dpki/src/bundle.rs
+++ hc_dpki/src/bundle.rs
@@ -1,9 +1,15 @@
+/// This struct is the bundle for the Key pairs. i.e. signing and encryption keys
+/// 
+/// The bundle_type tells if the bundle is a RootSeed bundle | DeviceSeed bundle | DevicePINSeed Bundle | ApplicationKeys Bundle
+/// 
+/// the data includes a base64 encoded string of the Keys Struct
 pub struct KeyBundle {
     pub bundle_type: String,
     pub hint: String,
     pub data: String,
 }
 
+/// This struct is gives the sign and encryption key pairs
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct Keys {
     pub pw_sign_pub: ReturnBundleData,
@@ -12,6 +18,7 @@ pub struct Keys {
     pub pw_enc_priv: ReturnBundleData,
 }
 
+/// This struct type is for the return type for  util::pw_enc
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct ReturnBundleData {
     pub salt: Vec<u8>,

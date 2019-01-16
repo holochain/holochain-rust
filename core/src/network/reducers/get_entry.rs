14819aa12463f5fa8ca989d2ac82f8d2021ea6ef
Nicolas Luck <nicolas@lucksus.org>
2019-01-16 23:56:11 +0100

    rustfmt

--- core/src/network/reducers/get_entry.rs
+++ core/src/network/reducers/get_entry.rs
@@ -72,10 +72,7 @@ mod tests {
         instance::tests::test_context,
         state::test_store,
     };
-    use holochain_core_types::{
-        //agent::AgentId, crud_status::CrudStatus, entry::EntryWithMeta,
-        error::HolochainError,
-    };
+    use holochain_core_types::error::HolochainError;
     //use holochain_net_connection::protocol_wrapper::DhtData;
     //use std::sync::{Arc, RwLock};
 

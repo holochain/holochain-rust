fb40b58103183c395646a80cc874c1dd0967d1c9
Nicolas Luck <nicolas@lucksus.org>
2019-01-17 00:29:33 +0100

    Deactivate flaky test

--- core/src/network/reducers/get_links.rs
+++ core/src/network/reducers/get_links.rs
@@ -130,6 +130,10 @@ mod tests {
     }
 
     #[test]
+    // This test needs to be refactored.
+    // It is non-deterministically failing with "sending on a closed channel" originating form
+    // within the mock network.
+    #[cfg(feature = "broken-tests")]
     pub fn reduce_get_links_timeout_test() {
         let mut context = test_context("alice");
         let store = test_store(context.clone());

cd27af0e3ae6c01ad8bc291f8768bdc6b629ddd1
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-01-18 17:42:00 -0500

    Revert "Revert "updated with uuid""
    This reverts commit eec0d4b84f26f415295abbc5879bfe253e0240a6.

--- cas_implementations/src/eav/file.rs
+++ cas_implementations/src/eav/file.rs
@@ -259,7 +259,7 @@ impl EntityAttributeValueStorage for EavFileStorage {
             .into_iter()
             .map(|(hash, content)| {
                 (
-                    from_key(hash).unwrap_or(Key(0, Action::None)),
+                    from_key(hash).unwrap_or(Key(0, Action::None, Uuid::new_v4())),
                     EntityAttributeValue::try_from_content(&JsonString::from(content)),
                 )
             })

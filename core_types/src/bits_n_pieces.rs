ee2f49d2a15de73f481e6daff07f9d711360169a
David Meister <thedavidmeister@gmail.com>
2019-01-22 03:19:52 +1100

    WIP on bits_n_pieces tests

--- core_types/src/bits_n_pieces.rs
+++ core_types/src/bits_n_pieces.rs
@@ -19,3 +19,44 @@ pub fn u32_split_bits(i: u32) -> (u16, u16) {
 pub fn u32_merge_bits(high: u16, low: u16) -> u32 {
     (u32::from(high) << 16) | u32::from(low)
 }
+
+#[cfg(test)]
+pub mod tests {
+
+    #[test]
+    /// tests that we can extract the high bits from a u32 into the correct u16
+    fn u32_high_bits_test() {
+        assert_eq!(
+            0b1010101010101010,
+            super::u32_high_bits(0b1010101010101010_0101010101010101),
+        );
+    }
+
+    #[test]
+    /// tests that we can extract the high bits from a u32 into the correct u16
+    fn u32_low_bits_test() {
+        assert_eq!(
+            0b0101010101010101,
+            super::u32_low_bits(0b1010101010101010_0101010101010101),
+        );
+    }
+
+    #[test]
+    /// tests that we can split a u32 into a tuple of high/low bits
+    fn u32_split_bits_test() {
+        assert_eq!(
+            (0b1010101010101010, 0b0101010101010101),
+            super::u32_split_bits(0b1010101010101010_0101010101010101),
+        );
+    }
+
+    #[test]
+    /// tests that we can merge a u16 tuple into a u32
+    fn u32_merge_bits_test() {
+        assert_eq!(
+            0b1010101010101010_0101010101010101,
+            super::u32_merge_bits(0b1010101010101010, 0b0101010101010101),
+        );
+    }
+
+}

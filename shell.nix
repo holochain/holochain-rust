a8d158fbbb620e161a2246876296538a5c55f366
Michael Dougherty <maackle.d@gmail.com>
2019-01-22 01:31:54 -0800

    Update date in nix too

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ let
     overlays = [ moz_overlay ];
   };
 
-  date = "2018-12-26";
+  date = "2019-01-21";
   wasmTarget = "wasm32-unknown-unknown";
 
   rust-build = (nixpkgs.rustChannelOfTargets "nightly" date [ wasmTarget ]);

65670d7983b945d48b3aea2b1e1781fd760581da
Michael Dougherty <maackle.d@gmail.com>
2019-01-22 15:48:51 -0200

    Update app_spec/zomes/blog/code/src/blog.rs
    Co-Authored-By: juliolrmonteiro <juliolrmonteiro@gmail.com>

--- app_spec/zomes/blog/code/src/blog.rs
+++ app_spec/zomes/blog/code/src/blog.rs
@@ -32,7 +32,7 @@ pub fn handle_check_sum(num1: u32, num2: u32) -> ZomeApiResult<JsonString> {
     )
 }
 
-pub fn handle_send(to_agent: Address, message: String) -> ZomeApiResult<String> {
+pub fn handle_check_send(to_agent: Address, message: String) -> ZomeApiResult<String> {
     hdk::send(to_agent, message)
 }
 

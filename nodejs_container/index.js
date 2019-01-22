8c1c4a54b07de35c4eb508d870b197984b602064
Michael Dougherty <maackle.d@gmail.com>
2019-01-22 13:14:23 -0800

    Consistent rejection between callWithPromise and callSync

--- nodejs_container/index.js
+++ nodejs_container/index.js
@@ -77,15 +77,16 @@ Container.prototype.callWithPromise = function (...args) {
         })
         return [result, promise]
     } catch (e) {
-        return [undefined, Promise.reject(e)]
+        return [
+            undefined, 
+            Promise.reject(e).catch(err => console.error("Error with scenario test system: ", err))
+        ]
     }
 }
 
 Container.prototype.callSync = function (...args) {
     const [result, promise] = this.callWithPromise(...args)
-    return promise
-        .catch(err => console.error("Error with scenario test system: ", err))
-        .then(() => { return result })
+    return promise.then(() => { return result })
 }
 
 // Convenience function for making an object that can call into the container

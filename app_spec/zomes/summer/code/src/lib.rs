4057467725dc6a952f930f8ec4d166d3a4b065d8
David Meister <thedavidmeister@gmail.com>
2019-01-23 05:53:06 +1100

    summer tests

--- app_spec/zomes/summer/code/src/lib.rs
+++ app_spec/zomes/summer/code/src/lib.rs
@@ -4,15 +4,13 @@ extern crate hdk;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
 extern crate serde_json;
 extern crate boolinator;
 use hdk::holochain_core_types::json::JsonString;
 use hdk::holochain_core_types::error::HolochainError;
 
-fn handle_sum(num1: u32, num2: u32) -> JsonString {
-    let sum = num1 + num2;
-    return json!({"sum": format!("{}",sum)}).into();
+fn handle_sum(num1: u32, num2: u32) -> u32 {
+    num1 + num2
 }
 
 define_zome! {
@@ -26,9 +24,24 @@ define_zome! {
         main (Public) {
             sum: {
                 inputs: |num1: u32, num2: u32|,
-                outputs: |sum: JsonString|,
+                outputs: |sum: u32|,
                 handler: handle_sum
             }
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use handle_sum;
+
+    #[test]
+    pub fn handle_sum_test() {
+        assert_eq!(
+            handle_sum(1, 1),
+            2,
+        );
+    }
+
 }

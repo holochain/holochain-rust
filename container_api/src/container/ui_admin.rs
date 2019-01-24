0b7c884318d9fb05c02c5416ead8440ff392b594
Willem Olding <willemolding@gmail.com>
2019-01-24 11:26:59 +0100

    starts implementing install_ui_bundle

--- container_api/src/container/ui_admin.rs
+++ container_api/src/container/ui_admin.rs
@@ -1,14 +1,15 @@
 use crate::{
     config::{
         UiInterfaceConfiguration,
+        UiBundleConfiguration,
     },
-    container::{Container},
+    container::{Container, base::notify},
 };
 use holochain_core_types::{error::HolochainError};
 use std::{path::PathBuf};
 
 pub trait ContainerUiAdmin {
-    fn install_ui_bundle_from_file(&mut self, path: PathBuf, id: String) -> Result<(), HolochainError>;
+    fn install_ui_bundle_from_file(&mut self, path: PathBuf, id: &String) -> Result<(), HolochainError>;
     fn uninstall_ui_bundle(&mut self, id: &String) -> Result<(), HolochainError>;
 
     fn add_ui_interface(&mut self, new_instance: UiInterfaceConfiguration)
@@ -20,7 +21,24 @@ pub trait ContainerUiAdmin {
 }
 
 impl ContainerUiAdmin for Container {
-    fn install_ui_bundle_from_file(&mut self, path: PathBuf, id: String) -> Result<(), HolochainError> {
+    fn install_ui_bundle_from_file(&mut self, path: PathBuf, id: &String) -> Result<(), HolochainError> {
+        let path_string = path
+            .to_str()
+            .ok_or(HolochainError::ConfigError("invalid path".into()))?;
+        
+
+        let new_bundle = UiBundleConfiguration {
+            id: id.to_string(),
+            root_dir: path_string.into(),
+            hash: "1234".to_string(),
+        };
+
+        let mut new_config = self.config.clone();
+        new_config.ui_bundles.push(new_bundle.clone());
+        new_config.check_consistency()?;
+        self.config = new_config;
+        self.save_config()?;
+        notify(format!("Installed UI bundle from {} as \"{}\"", path_string, id));
         Ok(())
     }
 

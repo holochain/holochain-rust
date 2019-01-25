6093294891fb8fd66e3f5ac5ee046a8a3d6e1e9d
Perry Kundert <perry@hardconsulting.com>
2019-01-25 16:42:32 -0700

    Fix timezone offset handling, provide fmt traits, Ord, Eq for sorting o Timezones offsets must be full +/-HH:MM to be accepted by RFC 3339 o Provide a fmt::Debug that displays ISO 8601 conversions, and an   error for invalid timestamp. o To support sortability, the Cmp and Eq total-order traits are provided   which deem all invalid Iso8601 timestamps to be equivalent, and less   than any valid Iso8601 for sorting purposes.

--- core_types/src/time.rs
+++ core_types/src/time.rs
@@ -6,7 +6,7 @@ use chrono::{offset::FixedOffset, DateTime, Utc};
 use error::HolochainError;
 use json::JsonString;
 use regex::Regex;
-use std::{cmp::Ordering, convert::TryFrom, time::Duration};
+use std::{cmp::Ordering, convert::TryFrom, time::Duration, fmt};
 
 /// Represents a timeout for an HDK function
 #[derive(Clone, Deserialize, Debug, Eq, PartialEq, Hash, Serialize, DefaultJson)]
@@ -46,7 +46,7 @@ impl From<usize> for Timeout {
 /// restrictive) format.
 ///
 /// More info on the relevant [wikipedia article](https://en.wikipedia.org/wiki/ISO_8601).
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Iso8601(String);
 
 /// Iso8601::now() and default() return the current Utc time.
@@ -59,6 +59,28 @@ impl Iso8601 {
 impl Default for Iso8601 {
     fn default() -> Iso8601 {
         Iso8601::now()
+    }
+}
+
+impl fmt::Display for Iso8601 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "\"{}\"", self.0)
+    }
+}
+
+impl fmt::Debug for Iso8601 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match DateTime::<FixedOffset>::try_from(self) {
+            Ok(dt) => {
+                let ts = dt.to_rfc3339();
+                if self.0 != ts {
+                    write!(f, "Iso8601 {{ \"{}\" <- \"{}\" }}", ts, &self.0 )
+                } else {
+                    write!(f, "Iso8601 {{ \"{}\" }}", &self.0 )
+                }
+            }
+            Err(e) => write!(f, "Iso8601 {{ \"{}\" -> {} }}", &self.0, e ),
+        }
     }
 }
 
@@ -137,10 +159,8 @@ impl TryFrom<&Iso8601> for DateTime<FixedOffset> {
                   (?:[.]\d+)?
                 )
                 \s*             # Optional whitespace, no timezone specifier implies Z
-                (?P<Z>
-                   (?:[+-]\d{2}
-                     (?:[:]?\d{2})?
-                   )
+                (?P<Z>          #   but require complete HH:MM offset specification
+                   [+-]\d{2}:\d{2}
                  | [Zz]?
                 )
                 \s*
@@ -178,7 +198,8 @@ impl TryFrom<&Iso8601> for DateTime<FixedOffset> {
 /// that two timestamps that differ in time specification may be equal, because they are the same
 /// time specified in two different timezones.  Therefore, a String-based Partial{Cmp,Eq} are not
 /// correct.  If conversion of any Iso8601 String fails, returns false for every test; similarly to
-/// how float NaN != NaN.
+/// how float NaN != NaN.  However, to ease sorting, we'll also provide an Ord implementation that
+/// orders any invalid Iso8601s as equal, before all valid Iso8601s.
 impl PartialEq for Iso8601 {
     fn eq(&self, rhs: &Iso8601) -> bool {
         match DateTime::<FixedOffset>::try_from(self) {
@@ -191,6 +212,10 @@ impl PartialEq for Iso8601 {
     }
 }
 
+/// The PartialEq implements a total order, where all invalid Iso8601 are considered equal to
+/// each-other; equally invalid.  Needed to implement Ord.
+impl Eq for Iso8601 { }
+
 impl PartialOrd for Iso8601 {
     fn partial_cmp(&self, rhs: &Iso8601) -> Option<Ordering> {
         match DateTime::<FixedOffset>::try_from(self) {
@@ -199,6 +224,23 @@ impl PartialOrd for Iso8601 {
                 Err(_e) => None,
             },
             Err(_e) => None,
+        }
+    }
+}
+
+// Invalid timestamps are "greater-than" any valid timestamp.  This puts them last in an in-order
+// sort, first in a reverse sort.
+impl Ord for Iso8601 {
+    fn cmp(&self, rhs: &Iso8601) -> Ordering {
+        match DateTime::<FixedOffset>::try_from(self) {
+            Ok(ts_lhs) => match DateTime::<FixedOffset>::try_from(rhs) {
+                Ok(ts_rhs) => ts_lhs.cmp(&ts_rhs),
+                Err(_) => Ordering::Greater,     // lhs is good, rhs is invalid; lhs is always > rhs (invalid)
+            }
+            Err(_) => match DateTime::<FixedOffset>::try_from(rhs) {
+                Ok(_) => Ordering::Less,        // lhs is invalid, rhs is valid; lhs (invalid) is always < rhs
+                Err(_) => Ordering::Equal,      // lhs and rhs both invalid; always equal-to each-other
+            }
         }
     }
 }
@@ -325,5 +367,64 @@ pub mod tests {
             }
             Err(e) => panic!("Unexpected failure of Iso8601::default {:?}", &e),
         }
+    }
+
+    #[test]
+    fn test_iso_8601_sorting() {
+        // Different ways of specifying UTC "Zulu".  A bare timestamp will be defaulted to "Zulu".
+        let mut v: Vec<Iso8601> = vec![
+            "2018-10-11T03:23:39-08:00".into(),
+            "2018-10-11T03:23:39-07:00".into(),
+            "2018-10-11 03:23:39+03:00".into(),
+            "baz".into(),
+            "2018-10-11T03:23:39-06:00".into(),
+            "20181011 032339 +04:00".into(),
+            "2018-10-11T03:23:39-09:00".into(),
+            "2018-10-11T03:23:39+11:00".into(),
+            "2018-10-11T03:23:39Z".into(),
+            "boo".into(),
+            "bar".into(),
+        ];
+        v.sort_by(|a,b| {
+            let cmp = a.cmp(b);
+            //println!( "{} {:?} {}", a, cmp, b );
+            cmp
+        });
+        assert_eq!(
+            v.iter()
+                .map(|ts| format!("{:?}", &ts).to_string())
+                .collect::<Vec<String>>()
+                .join(", "), concat!(
+                    "Iso8601 { \"baz\" -> Failed to find ISO 3339 or RFC 8601 timestamp in \"baz\" }, ",
+                    "Iso8601 { \"boo\" -> Failed to find ISO 3339 or RFC 8601 timestamp in \"boo\" }, ",
+                    "Iso8601 { \"bar\" -> Failed to find ISO 3339 or RFC 8601 timestamp in \"bar\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39+11:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39+04:00\" <- \"20181011 032339 +04:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39+03:00\" <- \"2018-10-11 03:23:39+03:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39+00:00\" <- \"2018-10-11T03:23:39Z\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39-06:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39-07:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39-08:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39-09:00\" }"
+                ));
+
+        v.sort_by(|a,b| b.cmp(a)); // reverse
+        assert_eq!(
+            v.iter()
+                .map(|ts| format!("{:?}", &ts).to_string())
+                .collect::<Vec<String>>()
+                .join(", "), concat!(
+                    "Iso8601 { \"2018-10-11T03:23:39-09:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39-08:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39-07:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39-06:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39+00:00\" <- \"2018-10-11T03:23:39Z\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39+03:00\" <- \"2018-10-11 03:23:39+03:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39+04:00\" <- \"20181011 032339 +04:00\" }, ",
+                    "Iso8601 { \"2018-10-11T03:23:39+11:00\" }, ",
+                    "Iso8601 { \"baz\" -> Failed to find ISO 3339 or RFC 8601 timestamp in \"baz\" }, ",
+                    "Iso8601 { \"boo\" -> Failed to find ISO 3339 or RFC 8601 timestamp in \"boo\" }, ",
+                    "Iso8601 { \"bar\" -> Failed to find ISO 3339 or RFC 8601 timestamp in \"bar\" }"
+                ));
     }
 }

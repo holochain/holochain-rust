942a11dd91da78849f5cf338a6061eb91ba63183
Perry Kundert <perry@hardconsulting.com>
2019-01-31 11:30:17 -0700

    Fix test filename typo in run.sh

--- app_spec/test/run.sh
+++ app_spec/test/run.sh
@@ -3,7 +3,7 @@
 # Execute the desired tests, retaining tests output and dumping on failure
 # 
 if [ -z $1 ]; then
-    tape test.js regression.js | tee test.out~ | faucet || ( cat test.out~; false )
+    tape test.js regressions.js | tee test.out~ | faucet || ( cat test.out~; false )
 else
-    tape $1                    | tee test.out~ | faucet || ( cat test.out~; false )
+    tape $1                     | tee test.out~ | faucet || ( cat test.out~; false )
 fi 

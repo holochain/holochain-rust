12379689f6e0a5e47da972fd12c8c5211baddc48
zo-el <joelulahanna@gmail.com>
2019-02-01 04:32:08 -0600

    update error

--- sodium/src/error.rs
+++ sodium/src/error.rs
@@ -1,4 +1,6 @@
 /// Error for Sodium lib to use in your code.
+use holochain_core_types::{error::HolochainError};
+
 #[derive(Debug)]
 pub enum SodiumError {
     Generic(String),
@@ -12,5 +14,15 @@ impl SodiumError {
 
     pub fn with_output_length(msg: &str) -> SodiumError {
         SodiumError::OutputLength(msg.to_string())
+    }
+}
+
+
+impl From<SodiumError> for HolochainError {
+    fn from(error: SodiumError) -> Self {
+        match error {
+            SodiumError::Generic(s) => HolochainError::new(&s),
+            SodiumError::OutputLength(s) => HolochainError::new(&s),
+        }
     }
 }

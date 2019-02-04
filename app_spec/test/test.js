d0130d346f49f6c7ff2bff1b354a4abdb98b2088
Nicolas Luck <nicolas@lucksus.org>
2019-02-04 10:21:21 -0500

    Update app_spec/test/test.js
    Co-Authored-By: AshantiMutinta <AshantiMutinta@gmail.com>

--- app_spec/test/test.js
+++ app_spec/test/test.js
@@ -63,7 +63,7 @@ scenario1.runTape('delete_post', async (t, { alice }) => {
   const content = "Holo world"
   const in_reply_to = null
   const params = { content, in_reply_to }
-  const result = alice.call("blog", "create_post", params)
+  const result = await alice.callSync("blog", "create_post", params)
   const result_delete = alice.call("blog", "delete_post", params)
   t.ok(result_delete.Ok)
   const result_list = alice.call("blog", "posts_by_agent", params)

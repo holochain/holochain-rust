721df5dc5e1fe7be9bcf5db1aaf966a2069d79ad
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-02-05 14:21:16 -0500

    Update blog.rs

--- app_spec/zomes/blog/code/src/blog.rs
+++ app_spec/zomes/blog/code/src/blog.rs
@@ -83,7 +83,7 @@ pub fn handle_create_post(content: String, in_reply_to: Option<Address>) -> Zome
 pub fn handle_delete_post(content:String) -> ZomeApiResult<Address>
 {
     let address = hdk::entry_address(&post_entry(content))?;
-    hdk::remove_link(&AGENT_ADDRESS,&address.clone(),"authored_posts")
+    hdk::remove_link(&AGENT_ADDRESS,&address.clone(),"authored_posts")?;
     Ok(address)
 }
 

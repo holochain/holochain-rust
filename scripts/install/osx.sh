6c8272d1c98e6730d81d6933662baa5b1265f429
David Meister <thedavidmeister@gmail.com>
2019-02-07 23:45:57 +1100

    fix install script on mac os

--- scripts/install/osx.sh
+++ scripts/install/osx.sh
@@ -2,10 +2,11 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install cmake
 brew install zmq
 brew upgrade zeromq
 
-export INSTALL_NODE_VERSION=8.4
+export INSTALL_NODE_VERSION=8.14
 
 rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install $INSTALL_NODE_VERSION
 curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.10.1

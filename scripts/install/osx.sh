29a604acff30952181264991c53173b1165939fd
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-02-09 13:50:08 +0100

    Make sure to install neon-dependency in install script

--- scripts/install/osx.sh
+++ scripts/install/osx.sh
@@ -10,3 +10,4 @@ export INSTALL_NODE_VERSION=8.4
 rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install $INSTALL_NODE_VERSION
 curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.10.1
 export PATH=$HOME/.yarn/bin:$PATH
+npm install -g neon-cli

d84ad41109e6caafbdb1f84377de10f5e061abc1
Nicolas Luck <nicolas@lucksus.org>
2019-02-11 14:40:30 +0100

    XDG compliant paths

--- common/src/paths.rs
+++ common/src/paths.rs
@@ -1,20 +1,23 @@
 use std::path::PathBuf;
 
-pub const CONFIG_DIRECTORY: &'static str = "holochain";
+pub const QUALIFIER: &'static str = "org";
+pub const ORGANIZATION: &'static str = "holochain";
+pub const APPLICATION: &'static str = "holochain";
 pub const KEYS_DIRECTORY: &'static str = "keys";
 
 /// Returns the path to the root config directory for all of Holochain.
-/// If we can get a user directory it will be a dot-directory in ~, like "/home/peter/.holochain".
-/// If it can't get a user directory it will default to /etc, i.e. "/etc/holochain".
+/// If we can get a user directory it will be an XDG compliant path
+/// like "/home/peter/.config/holochain".
+/// If it can't get a user directory it will default to "/etc/holochain".
 pub fn config_root() -> PathBuf {
-    directories::UserDirs::new()
-        .and_then(|user_dirs| Some(user_dirs.home_dir().join(format!(".{}", CONFIG_DIRECTORY))))
-        .or(Some(PathBuf::new().join("/etc").join(CONFIG_DIRECTORY)))
+    directories::ProjectDirs::from(QUALIFIER, ORGANIZATION, APPLICATION)
+        .and_then(|dirs| Some(dirs.config_dir().to_owned()))
+        .or(Some(PathBuf::new().join("/etc").join(APPLICATION)))
         .unwrap()
 }
 
 /// Returns the path to where agent keys are stored and looked for by default.
-/// Something like "~/.holochain/keys".
+/// Something like "~/.config/holochain/keys".
 pub fn keys_directory() -> PathBuf {
     config_root().join(KEYS_DIRECTORY)
 }

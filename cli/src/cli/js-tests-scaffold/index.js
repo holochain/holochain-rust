7edee57ced588e3efebb1def3cf44c105cdc4808
Connor Turland <connor.turland@holo.host>
2019-02-15 16:50:40 -0500

    destructuring again

--- cli/src/cli/js-tests-scaffold/index.js
+++ cli/src/cli/js-tests-scaffold/index.js
@@ -9,11 +9,11 @@ const dna = Config.dna(dnaPath)
 const instanceAlice = Config.instance(agentAlice, dna)
 const scenario = new Scenario([instanceAlice])
 
-scenario.runTape("description of example test", (t, instances) => {
+scenario.runTape("description of example test", (t, { alice }) => {
   // Make a call to a Zome function
   // indicating the function, and passing it an input
-  const addr = instances.alice.call("my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
-  const result = instances.alice.call("my_zome", "get_my_entry", {"address": addr.Ok})
+  const addr = alice.call("my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
+  const result = alice.call("my_zome", "get_my_entry", {"address": addr.Ok})
   
   // check for equality of the actual and expected results
   t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })

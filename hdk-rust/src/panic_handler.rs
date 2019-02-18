7c11b2abc8a1cfbf7bcb8112ff62ba16e952e57a
Nicolas Luck <nicolas@lucksus.org>
2019-02-18 23:13:04 +0100

    rustfmt

--- hdk-rust/src/panic_handler.rs
+++ hdk-rust/src/panic_handler.rs
@@ -5,13 +5,20 @@ use std::panic;
 #[no_mangle]
 pub extern "C" fn __panic_handler() -> () {
     panic::set_hook(Box::new(move |info| {
-        let _ = debug(RawString::from(info.payload().downcast_ref::<String>().unwrap().clone()));
+        let _ = debug(RawString::from(
+            info.payload().downcast_ref::<String>().unwrap().clone(),
+        ));
         //let _ = debug(RawString::from(format!("{}", info.message().unwrap().clone())));
         let _ = if let Some(location) = info.location() {
-            debug(RawString::from(format!("panic occurred in file '{}' at line {}", location.file(),
-                     location.line())))
+            debug(RawString::from(format!(
+                "panic occurred in file '{}' at line {}",
+                location.file(),
+                location.line()
+            )))
         } else {
-            debug(RawString::from(format!("panic occurred but can't get location information...")))
+            debug(RawString::from(format!(
+                "panic occurred but can't get location information..."
+            )))
         };
     }));
 }

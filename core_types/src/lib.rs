92645ca486fcc8747b8ed36437f45ade4c4b1883
Julian Laubstein <contact@julianlaubstein.de>
2019-02-19 16:07:29 +0100

    Added test

--- core_types/src/lib.rs
+++ core_types/src/lib.rs
@@ -58,3 +58,17 @@ pub const GIT_HASH: &str = env!(
     "GIT_HASH",
     "failed to obtain git hash from build environment. Check build.rs"
 );
+
+#[cfg(test)]
+mod test_hash {
+    use super::*;
+
+    #[test]
+    fn test_hash() {
+        assert_eq!(GIT_HASH.chars().count(), 40);
+        assert!(
+            GIT_HASH.is_ascii(),
+            "GIT HASH contains non-ascii characters"
+        );
+    }
+}

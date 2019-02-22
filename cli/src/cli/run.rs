2198c9cd55a1116c8d21c0695ffc4529f1fac0e8
damien <doute.d@gmail.com>
2019-02-22 16:03:38 -0500

    [ci skip] [skip travis]

--- cli/src/cli/run.rs
+++ cli/src/cli/run.rs
@@ -32,11 +32,6 @@ pub fn run(
         .load_config()
         .map_err(|err| format_err!("{}", err))?;
 
-    println!(
-        "Holochain development conductor started. Network running on {:?}",
-        conductor.p2p_bindings()
-    );
-
     conductor.start_all_interfaces();
     conductor.start_all_instances()?;
 

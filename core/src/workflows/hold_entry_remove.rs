a75c3fa40d5bd814c44e1b14e73529f995b029e3
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-02-26 15:20:45 -0500

    Hold methiods for update and entry

---
+++ core/src/workflows/hold_entry_remove.rs
@@ -0,0 +1,42 @@
+use crate::{
+    context::Context,
+    dht::actions::remove_entry::remove_entry,
+    network::{
+        actions::get_validation_package::get_validation_package, entry_with_header::EntryWithHeader,
+    },
+    nucleus::actions::validate::validate_entry,
+};
+
+use holochain_core_types::{
+    cas::content::{Address,AddressableContent},
+    error::HolochainError,
+    validation::{EntryAction, EntryLifecycle, ValidationData},
+};
+use std::sync::Arc;
+
+pub async fn hold_remove_workflow<'a>(
+    entry_with_header: EntryWithHeader,
+    context: Arc<Context>,
+) -> Result<(), HolochainError> {
+    let EntryWithHeader { entry, header } = &entry_with_header;
+
+    // 1. Get validation package from source
+    let maybe_validation_package = await!(get_validation_package(header.clone(), &context))?;
+    let validation_package = maybe_validation_package
+        .ok_or("Could not get validation package from source".to_string())?;
+
+    // 2. Create validation data struct
+    let validation_data = ValidationData {
+        package: validation_package,
+        lifecycle: EntryLifecycle::Dht,
+        action: EntryAction::Create,
+    };
+
+    // 3. Validate the entry
+    await!(validate_entry(entry.clone(), validation_data, &context))?;
+
+    // 3. If valid store the entry in the local DHT shard
+    await!(remove_entry(&context.clone(),entry.address())?)
+}
+
+

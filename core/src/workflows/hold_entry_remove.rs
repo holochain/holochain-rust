9c983ec67f274ba1d2008bef8d1290872810867f
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-03-01 09:16:45 -0500

    removed println

--- core/src/workflows/hold_entry_remove.rs
+++ core/src/workflows/hold_entry_remove.rs
@@ -20,26 +20,26 @@ pub async fn hold_remove_workflow<'a>(
     context: Arc<Context>,
 ) -> Result<(), HolochainError> {
     let EntryWithHeader { entry, header } = &entry_with_header;
-    println!("get validation from source");
+   
     // 1. Get validation package from source
     let maybe_validation_package = await!(get_validation_package(header.clone(), &context))?;
     let validation_package = maybe_validation_package
         .ok_or("Could not get validation package from source".to_string())?;
-    println!("create validation data");
+
     // 2. Create validation data struct
     let validation_data = ValidationData {
         package: validation_package,
         lifecycle: EntryLifecycle::Dht,
-        action: EntryAction::Create,
+        action: EntryAction::Delete,
     };
 
-    println!("validate entry");
+
     // 3. Validate the entry
     await!(validate_entry(entry.clone(), validation_data, &context))?;
 
 
     let deletion_entry = unwrap_to!(entry => Entry::Deletion);
-    println!("remove from link");
+
     let deleted_entry_address = deletion_entry.clone().deleted_entry_address();
     // 3. If valid store the entry in the local DHT shard
     await!(remove_entry(&context.clone(),deletion_entry.clone().deleted_entry_address(),entry.address().clone())?)
--- core/src/workflows/hold_entry_update.rs
+++ core/src/workflows/hold_entry_update.rs
@@ -28,20 +28,20 @@ pub async fn hold_update_workflow<'a>(
     let validation_package = maybe_validation_package
         .ok_or("Could not get validation package from source".to_string())?;
 
-    println!("create validation package");
+
     // 2. Create validation data struct
     let validation_data = ValidationData {
         package: validation_package,
         lifecycle: EntryLifecycle::Dht,
-        action: EntryAction::Create,
+        action: EntryAction::Modify,
     };
      
-     println!("validate package");
+
     let link = header.link_update_delete().ok_or("Could not get link update from header".to_string())?;
-    println!("old address {:?}",link.clone());
+ 
     // 3. Validate the entry
     await!(validate_entry(entry.clone(), validation_data, &context))?;
-    println!("update entry");
+ 
     // 3. If valid store the entry in the local DHT shard
     await!(update_entry(&context.clone(),link,entry.address().clone())?)
 }

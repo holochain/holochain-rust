1b627935e08d816951aabce1ecb69392d7219de9
Julian Laubstein <contact@julianlaubstein.de>
2019-03-04 16:32:22 +0100

    Added test for signing

--- core/src/nucleus/ribosome/api/sign.rs
+++ core/src/nucleus/ribosome/api/sign.rs
@@ -13,3 +13,22 @@ pub fn invoke_sign(runtime: &mut Runtime, args: &RuntimeArgs) -> ZomeApiResult {
 
     runtime.store_result(signature)
 }
+
+#[cfg(test)]
+mod test_super {
+    use crate::nucleus::ribosome::{
+        api::{tests::test_zome_api_function, ZomeApiFunction},
+        Defn,
+    };
+    use holochain_core_types::json::JsonString;
+
+    /// test that bytes passed to debug end up in the log
+    #[test]
+    fn test_zome_api_function_sign() {
+        let (call_result, _) = test_zome_api_function(
+            ZomeApiFunction::Sign.as_str(),
+            "test".to_string().into_bytes(),
+        );
+        assert_eq!(JsonString::from("{\"ok\":true,\"value\":\"+StjDIBItBYSefv3sezv8A+n7eBhKimq8KSmLSXmqH3Lwu+TLsUUdbXiwtC+Hzlb1Yi1smbqE7wg7q2xIC6XAw==\",\"error\":\"null\"}"), call_result,);
+    }
+}

8aba025f67ce1f5567712ff46f54752358a0b52b
Eric Harris-Braun <eric@harris-braun.com>
2019-03-08 14:05:54 -0500

    fixed broken test

--- conductor_api/src/interface.rs
+++ conductor_api/src/interface.rs
@@ -1109,7 +1109,7 @@ pub mod tests {
             .expect("Invalid call to handler");
         assert_eq!(
             response_str,
-            r#"{"jsonrpc":"2.0","error":{"code":-32602,"message":"invalid provenance param: missing field `source` at line 1 column 26"},"id":"0"}"#
+            r#"{"jsonrpc":"2.0","error":{"code":-32602,"message":"invalid provenance: missing field `source` at line 1 column 26"},"id":"0"}"#
         );
 
         let response_str = handler

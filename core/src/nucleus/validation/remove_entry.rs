99ca71fdbb5e817ff908a09a11f8ad6031283e08
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-03-11 12:30:12 -0400

    Remove entry checks if entry exists

--- core/src/nucleus/validation/remove_entry.rs
+++ core/src/nucleus/validation/remove_entry.rs
@@ -33,22 +33,25 @@ pub async fn validate_remove_entry(entry: Entry,
 
         if entity_to_delete_header.provenances().iter().find(|prov| deletion_entry_header.provenances().iter().find(|prov2|prov.0==prov2.0).is_some()).is_some()
         {
-            println!("entry here");
             let app_entry_type = match entry_to_delete.clone()
             {
             Entry::App(app_entry_type,_) => Ok(app_entry_type),
             _ => Err(ValidationError::Fail("Entry type should be App Type".to_string()))
             }?;
 
-            println!("app entry type{:?}",app_entry_type.clone());
             let zome_name = dna
             .get_zome_name_for_app_entry_type(&app_entry_type)
             .ok_or(ValidationError::NotImplemented)?;
-            println!("get zome name {:?}",zome_name.clone());
+            let entry_args = &GetEntryArgs {
+                 address: deletion_entry.clone().deleted_entry_address(),
+                      options: Default::default()};
+            let result = await!(get_entry_result_workflow(&context,entry_args).map_err(|_|{
+                ValidationError::Fail("Could not get entry for link_update_delete".to_string())
+            }))?;
+            let latest = result.latest().ok_or(ValidationError::Fail("Could not find entry for deletion entry".to_string()))?;
             let params = EntryValidationArgs {
             validation_data: validation_data.clone().entry_validation,
             };
-            println!("validation call back params{:?}",params.clone());
             let call = CallbackFnCall::new(&zome_name, "__hdk_validate_app_entry", params);
             await!(run_validation_callback(entry.address(), call, context))
         }

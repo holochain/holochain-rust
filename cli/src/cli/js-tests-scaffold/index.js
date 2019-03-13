1153bd3e9b49e9b55b441c2d1bcfd435e6ea06cc
Connor Turland <connorturland@gmail.com>
2019-03-13 12:57:52 -0400

    fix js scaffold

--- cli/src/cli/js-tests-scaffold/index.js
+++ cli/src/cli/js-tests-scaffold/index.js
@@ -9,7 +9,7 @@ const dna = Config.dna(dnaPath)
 const instanceAlice = Config.instance(agentAlice, dna)
 const scenario = new Scenario([instanceAlice])
 
-scenario.runTape("description of example test", (t, { alice }) => {
+scenario.runTape("description of example test", async (t, { alice }) => {
   // Make a call to a Zome function
   // indicating the function, and passing it an input
   const addr = alice.call("my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})

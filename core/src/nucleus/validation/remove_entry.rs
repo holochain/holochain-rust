fee648316527d2d1ca1ef0f8e23c1e8d0c0105fe
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-03-13 13:24:38 -0400

    Update remove_entry.rs

--- core/src/nucleus/validation/remove_entry.rs
+++ core/src/nucleus/validation/remove_entry.rs
@@ -18,15 +18,19 @@ use std::sync::Arc;
 use futures_util::try_future::TryFutureExt;
 
 pub async fn validate_remove_entry(entry: Entry,
-    _validation_data: ValidationData,
+    validation_data: ValidationData,
     context: &Arc<Context>) -> ValidationResult
     {
         let dna = context.get_dna().expect("Callback called without DNA set");
         let deletion_entry = unwrap_to!(entry=>Entry::Deletion);
-        let EntryWithHeader{entry : entry_to_delete,header: _entity_to_delete_header} = fetch_entry_with_header(&deletion_entry.clone().deleted_entry_address(),&context).map_err(|_|{
-            ValidationError::Fail("Author mismatch ".to_string())
+        let EntryWithHeader{entry : entry_to_delete,header: entity_to_delete_header} = fetch_entry_with_header(&deletion_entry.clone().deleted_entry_address(),&context).map_err(|_|{
+            ValidationError::Fail("Author ".to_string())
         })?;
+        let headers = &validation_data.package.chain_header;
 
+        if headers.provenances().iter().find(|prov| entity_to_delete_header.provenances().iter().find(|prov2|prov.source()==prov2.source()).is_some()).is_some()
+        {
+            println!("Provenances match");
             let app_entry_type = match entry_to_delete.clone()
             {
             Entry::App(app_entry_type,_) => Ok(app_entry_type),
@@ -36,13 +40,7 @@ pub async fn validate_remove_entry(entry: Entry,
             let zome_name = dna
             .get_zome_name_for_app_entry_type(&app_entry_type)
             .ok_or(ValidationError::NotImplemented)?;
-            let entry_args = &GetEntryArgs {
-                 address: deletion_entry.clone().deleted_entry_address(),
-                      options: Default::default()};
-            let result = await!(get_entry_result_workflow(&context,entry_args).map_err(|_|{
-                ValidationError::Fail("Could not get entry for link_update_delete".to_string())
-            }))?;
-            result.latest().ok_or(ValidationError::Fail("Could not find entry for deletion entry".to_string()))?;
+          
             let params = EntryValidationArgs {
             validation_data: entry_to_validation_data(context.clone(),&entry,None).map_err(|_|{
             ValidationError::Fail("Could not get entry validation".to_string())
@@ -50,6 +48,12 @@ pub async fn validate_remove_entry(entry: Entry,
             };
             let call = CallbackFnCall::new(&zome_name, "__hdk_validate_app_entry", params);
             await!(run_validation_callback(entry.address(), call, context))
+        }
+        else
+        {
+            Err(ValidationError::Fail("Tried to Delete Entry From Different Author".to_string()))
+        }
+
         
     }
 

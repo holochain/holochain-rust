e9e00308f740c9a5e4d8fe22609163c7900a2e14
Julian Laubstein <contact@julianlaubstein.de>
2019-03-14 12:58:54 +0100

    Added stub methods for signing and verification

--- app_spec/zomes/converse/code/src/lib.rs
+++ app_spec/zomes/converse/code/src/lib.rs
@@ -1,15 +1,24 @@
 #![feature(try_from)]
 
+#[macro_use]
+extern crate hdk;
+#[macro_use]
+extern crate serde_derive;
+
 use hdk::{
-    entry_definition::ValidatingEntryType,
     error::ZomeApiResult,
     holochain_core_types::{
-        cas::content::Address, dna::entry_types::Sharing, entry::Entry, error::HolochainError,
-        json::JsonString,
+        cas::content::Address, entry::Entry, error::HolochainError, json::JsonString,
     },
 };
 
-pub fn handle_sign_me_message(entry: MyEntry) -> ZomeApiResult<Address> {
+pub fn handle_sign_me_message(message: String) -> ZomeApiResult<Address> {
+    let entry = Entry::App("my_entry".into(), entry.into());
+    let address = hdk::commit_entry(&entry)?;
+    Ok(address)
+}
+
+pub fn handle_verify_me_message(entry: String) -> ZomeApiResult<Address> {
     let entry = Entry::App("my_entry".into(), entry.into());
     let address = hdk::commit_entry(&entry)?;
     Ok(address)
@@ -22,13 +31,19 @@ define_zome! {
 
     functions: [
         sign_me_message: {
-            inputs: |entry: String|,
+            inputs: |message: String|,
             outputs: |result: ZomeApiResult<Address>|,
             handler: handle_sign_me_message
+        }
+
+        verify_me_message: {
+            inputs: |entry: String|,
+            outputs: |result: ZomeApiResult<Address>|,
+            handler: handle_verify_me_message
         }
     ]
 
     traits: {
-        hc_public [create_my_entry]
+        hc_public [sign_me_message, verify_me_message]
     }
 }

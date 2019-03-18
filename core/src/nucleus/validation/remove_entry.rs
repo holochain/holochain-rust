7391b7be92d41eb78e0b3f9f7bbda0c796f9601d
Nicolas Luck <nicolas@lucksus.org>
2019-03-18 10:43:22 -0400

    Update core/src/nucleus/validation/remove_entry.rs
    Co-Authored-By: AshantiMutinta <AshantiMutinta@gmail.com>

--- core/src/nucleus/validation/remove_entry.rs
+++ core/src/nucleus/validation/remove_entry.rs
@@ -24,7 +24,7 @@ pub async fn validate_remove_entry(
     let deletion_address = deletion_entry.clone().deleted_entry_address();
     let entry_to_delete = get_entry_from_dht(&context.clone(), &deletion_address)
         .map_err(|_| {
-            ValidationError::Fail("Could not find entry for link_update_delete".to_string())
+            ValidationError::UnresolvedDependencies([deletion_address.clone()])
         })?
         .ok_or(ValidationError::Fail(
             "Could not obtain entry for link_update_delte".to_string(),

55a50cada21568a0adc318b5c4e8f129dbc4b1f7
Michael Dougherty <maackle.d@gmail.com>
2019-03-19 11:57:20 -0700

    Use AgentState as address directly

--- cli/src/cli/chain_log.rs
+++ cli/src/cli/chain_log.rs
@@ -5,7 +5,7 @@ use holochain_core::agent::{
     chain_store::ChainStore,
     state::{AgentState, AgentStateSnapshot},
 };
-use holochain_core_types::{chain_header::ChainHeader, entry::Entry};
+use holochain_core_types::{cas::content::Address, chain_header::ChainHeader, entry::Entry};
 use std::{convert::TryFrom, fs, path::PathBuf};
 
 // TODO: use system-agnostic default path
@@ -14,38 +14,48 @@ const DEFAULT_CHAIN_PATH: &str = "/home/michael/.holochain/holo/storage/";
 pub fn chain_log(storage_path: Option<PathBuf>, instance_id: String) -> DefaultResult<()> {
     let storage_path = storage_path.unwrap_or_else(|| PathBuf::new().join(DEFAULT_CHAIN_PATH));
     let cas_path = storage_path.join(instance_id).join("cas");
-    let snapshot_path = cas_path.join("AgentState.txt");
-    let snapshot_json = fs::read_to_string(snapshot_path)?;
-    let snapshot = AgentStateSnapshot::from_json_str(&snapshot_json)?;
     let chain_store = ChainStore::new(std::sync::Arc::new(std::sync::RwLock::new(
-        FilesystemStorage::new(cas_path.clone()).expect("could not create chain store"),
+        FilesystemStorage::new(cas_path.clone()).expect("Could not create chain store".into()),
     )));
     let cas_lock = chain_store.content_storage();
     let cas = cas_lock.read().unwrap();
-    let agent =
-        AgentState::new_with_top_chain_header(chain_store, snapshot.top_chain_header().cloned());
 
-    if agent.top_chain_header().is_none() {
-        println!("Chain is empty.")
-    } else {
-        println!(
-            "\nChain entries for '{}' (latest on top):\n",
-            cas_path.to_string_lossy()
-        );
-        for ref header in agent.iter_chain() {
-            let content = cas
-                .fetch(header.entry_address())
-                .expect("Panic while fetching from CAS!")
-                .ok_or_else(|| {
-                    println!(
-                        "{:?} referenced in header but not found in CAS!",
-                        header.entry_address(),
-                    )
-                })
-                .unwrap();
-            let entry = Entry::try_from(content).expect("Invalid content");
-            display_header(&header, &entry);
-        }
+    let agent = cas
+        .fetch(&Address::from("AgentState"))?
+        .ok_or("Chain does not exist or has not been initialized")
+        .and_then(|snapshot_json| {
+            AgentStateSnapshot::from_json_str(&snapshot_json.to_string())
+                .map_err(|_| "AgentState is malformed")
+        })
+        .map(|snapshot| {
+            let top_header = snapshot.top_chain_header().to_owned().clone();
+            AgentState::new_with_top_chain_header(chain_store, top_header.cloned())
+        })
+        .map_err(|err| {
+            format_err!(
+                "Could not display chain for '{}': {}",
+                cas_path.to_string_lossy(),
+                err.to_string()
+            )
+        })?;
+
+    println!(
+        "\nChain entries for '{}' (latest on top):\n",
+        cas_path.to_string_lossy()
+    );
+    for ref header in agent.iter_chain() {
+        let content = cas
+            .fetch(header.entry_address())
+            .expect("Panic while fetching from CAS!")
+            .ok_or_else(|| {
+                println!(
+                    "{:?} referenced in header but not found in CAS!",
+                    header.entry_address(),
+                )
+            })
+            .unwrap();
+        let entry = Entry::try_from(content).expect("Invalid content");
+        display_header(&header, &entry);
     }
 
     Ok(())
@@ -54,7 +64,7 @@ pub fn chain_log(storage_path: Option<PathBuf>, instance_id: String) -> DefaultR
 pub fn chain_list(path: Option<PathBuf>) {
     let path = path.unwrap_or_else(|| PathBuf::new().join(DEFAULT_CHAIN_PATH));
     println!("Please specify an instance ID to view its chain.");
-    println!("Available instances for '{}':", path.to_string_lossy());
+    println!("Available instances for '{}':\n", path.to_string_lossy());
     for entry in fs::read_dir(path).unwrap() {
         let name = entry.unwrap().file_name();
         println!("- {}", name.to_string_lossy());

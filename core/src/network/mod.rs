09d8c6cc6a08e67915f08b8164ad1053b7b9cff3
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-03-22 16:27:58 -0400

    Update mod.rs

--- core/src/network/mod.rs
+++ core/src/network/mod.rs
@@ -177,7 +177,10 @@ pub mod tests {
         assert!(maybe_entry_with_meta.is_some());
         let entry_with_meta = maybe_entry_with_meta.unwrap();
         assert_eq!(entry_with_meta.entry_with_meta.entry, entry);
-        assert_eq!(entry_with_meta.entry_with_meta.crud_status, CrudStatus::Live);
+        assert_eq!(
+            entry_with_meta.entry_with_meta.crud_status,
+            CrudStatus::Live
+        );
     }
     #[test]
     fn get_validation_package_roundtrip() {

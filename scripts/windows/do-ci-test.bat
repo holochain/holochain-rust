212694f90697b4b0f42f284c96112ddc6a696ac5
David Meister <thedavidmeister@gmail.com>
2019-03-26 12:18:53 +1100

    fix equality checks in do-ci-test.bat

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -1,8 +1,8 @@
-IF NOT [%wasm_path] = [] (
+IF NOT [%wasm_path] == [] (
  cargo build --manifest-path "%test_path%%wasm_path%Cargo.toml" --release --target wasm32-unknown-unknown --target-dir "%hc_target_prefix%%test_path%%wasm_path%target"
 )
 
-IF NOT [%wasm_path_2] = [] (
+IF NOT [%wasm_path_2] == [] (
  cargo build --manifest-path "%test_path%%wasm_path_2%Cargo.toml" --release --target wasm32-unknown-unknown --target-dir "%hc_target_prefix%%test_path%%wasm_path_2%target"
 )
 

1e9a236447bd623fddfe9fb24a18c4cad0aac0e7
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-03-27 12:08:53 -0400

    cargo fmt

--- benchmarks/benches/my_benchmark.rs
+++ benchmarks/benches/my_benchmark.rs
@@ -1,14 +1,11 @@
-
 #[macro_use]
 extern crate bencher;
-extern crate tempfile;
-extern crate holochain_core_types;
 extern crate holochain_cas_implementations;
+extern crate holochain_core_types;
+extern crate tempfile;
 
-
-
-use bencher::Bencher;
 use self::tempfile::tempdir;
+use bencher::Bencher;
 use holochain_cas_implementations::eav::file::EavFileStorage;
 use holochain_core_types::{
     cas::{
@@ -19,27 +16,30 @@ use holochain_core_types::{
     json::RawString,
 };
 fn bench_file_eav_one_to_many(b: &mut Bencher) {
-        b.iter(||{
-           
-                let temp = tempdir().expect("test was supposed to create temp dir");
-                let temp_path = String::from(temp.path().to_str().expect("temp dir could not be string"));
-                let eav_storage = EavFileStorage::new(temp_path).unwrap();
-                EavTestSuite::test_one_to_many::<ExampleAddressableContent, EavFileStorage>(eav_storage.clone())
-        
-        })
-    }
-
-
-fn bench_file_eav_many_to_one(b: &mut Bencher) {
-     b.iter(||{
-          
-                let temp = tempdir().expect("test was supposed to create temp dir");
-                let temp_path = String::from(temp.path().to_str().expect("temp dir could not be string"));
-                let eav_storage = EavFileStorage::new(temp_path).unwrap();
-                EavTestSuite::test_many_to_one::<ExampleAddressableContent, EavFileStorage>(eav_storage.clone())
-       
-        })
+    b.iter(|| {
+        let temp = tempdir().expect("test was supposed to create temp dir");
+        let temp_path = String::from(temp.path().to_str().expect("temp dir could not be string"));
+        let eav_storage = EavFileStorage::new(temp_path).unwrap();
+        EavTestSuite::test_one_to_many::<ExampleAddressableContent, EavFileStorage>(
+            eav_storage.clone(),
+        )
+    })
 }
 
-benchmark_group!(benches, bench_file_eav_one_to_many,bench_file_eav_many_to_one);
+fn bench_file_eav_many_to_one(b: &mut Bencher) {
+    b.iter(|| {
+        let temp = tempdir().expect("test was supposed to create temp dir");
+        let temp_path = String::from(temp.path().to_str().expect("temp dir could not be string"));
+        let eav_storage = EavFileStorage::new(temp_path).unwrap();
+        EavTestSuite::test_many_to_one::<ExampleAddressableContent, EavFileStorage>(
+            eav_storage.clone(),
+        )
+    })
+}
+
+benchmark_group!(
+    benches,
+    bench_file_eav_one_to_many,
+    bench_file_eav_many_to_one
+);
 benchmark_main!(benches);

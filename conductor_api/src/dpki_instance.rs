b27b7b8effc3f024f07cee8e6a791a129b4f1593
Eric Harris-Braun <eric@harris-braun.com>
2019-03-31 19:22:41 -0400

    added implementations of init and is_initialized to dpki_trait

--- conductor_api/src/dpki_instance.rs
+++ conductor_api/src/dpki_instance.rs
@@ -2,9 +2,8 @@
 /// for the conductor to use in the various dpki related flows, e.g. bootstrap and agent addition
 use crate::holochain::Holochain;
 use holochain_core_types::{
-    cas::content::Address,
+    cas::content::{Address, AddressableContent},
     error::HolochainError,
-    signature::{Provenance, Signature},
 };
 
 use holochain_core::nucleus::{
@@ -14,41 +13,71 @@ use holochain_core::nucleus::{
 
 pub const DPKI_ZOME_NAME: &str = "dpki";
 pub const DPKI_TRAIT_FN_ADD_AGENT: &str = "create_agent_key";
+pub const DPKI_TRAIT_FN_INIT: &str = "init";
+pub const DPKI_TRAIT_FN_IS_INITIALIZED: &str = "is_initialized";
+
+use std::convert::TryInto;
 
 pub trait DpkiInstance {
     fn dpki_cap_request(
         &mut self,
-        method: &str,
-        params: &str,
+        function: &str,
+        parameters: &str,
     ) -> Result<CapabilityRequest, HolochainError>;
     fn dpki_create_agent_key(&mut self, agent_name: String) -> Result<(), HolochainError>;
+    fn dpki_init(&mut self, params: String) -> Result<(), HolochainError>;
+    fn dpki_is_initialized(&mut self) -> Result<bool, HolochainError>;
 }
 
 impl DpkiInstance for Holochain {
     /// create a capability request for a given dpki call
     fn dpki_cap_request(
         &mut self,
-        method: &str,
-        params: &str,
+        function: &str,
+        parameters: &str,
     ) -> Result<CapabilityRequest, HolochainError> {
-        let token = Address::from("");
-        let provenance = Provenance::new(Address::from(""), Signature::fake());
-        Ok(CapabilityRequest::new(
+        let context = self.context();
+        let token = Address::from(context.agent_id.address());
+        Ok(make_cap_request_for_call(
+            context.clone(),
             token,
-            provenance.source(),
-            provenance.signature(),
+            function,
+            parameters.to_string(),
         ))
     }
 
+    /// wrapper for the dpki create_agent_key trait function
     fn dpki_create_agent_key(&mut self, agent_name: String) -> Result<(), HolochainError> {
         let params = json!({ "agent_name": agent_name }).to_string();
         let cap_request = self.dpki_cap_request(DPKI_TRAIT_FN_ADD_AGENT, &params)?;
-        let result = self.call(
+        let _result = self.call(
             DPKI_ZOME_NAME,
             cap_request,
             DPKI_TRAIT_FN_ADD_AGENT,
             &params,
         )?;
         Ok(())
+    }
+
+    // wrapper for the dpki init trait function
+    fn dpki_init(&mut self, params: String) -> Result<(), HolochainError> {
+        let params = json!({ "params": params }).to_string();
+        let cap_request = self.dpki_cap_request(DPKI_TRAIT_FN_INIT, &params)?;
+        let _result = self.call(DPKI_ZOME_NAME, cap_request, DPKI_TRAIT_FN_INIT, &params)?;
+        Ok(())
+    }
+
+    // wrapper for the dpki is_initialized trait function
+    fn dpki_is_initialized(&mut self) -> Result<bool, HolochainError> {
+        let params = "{}";
+        let cap_request = self.dpki_cap_request(DPKI_TRAIT_FN_IS_INITIALIZED, params)?;
+        let result = self.call(
+            DPKI_ZOME_NAME,
+            cap_request,
+            DPKI_TRAIT_FN_IS_INITIALIZED,
+            params,
+        )?;
+        let result: bool = result.try_into()?;
+        Ok(result)
     }
 }

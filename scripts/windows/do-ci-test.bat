fc64c51cfd97ae0f77b92da1c23c6fc577d8e55b
David Meister <thedavidmeister@gmail.com>
2019-04-08 17:38:35 +1000

    debug

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -1,9 +1,9 @@
 IF NOT [%wasm_path] == [] (
- cargo build --manifest-path "%test_path%%wasm_path%Cargo.toml" --release --target wasm32-unknown-unknown --target-dir "%hc_target_prefix%%test_path%%wasm_path%target" --features vendored
+ cargo build --manifest-path "%test_path%%wasm_path%Cargo.toml" --release --target wasm32-unknown-unknown --target-dir "%hc_target_prefix%%test_path%%wasm_path%target"
 )
 
 IF NOT [%wasm_path_2] == [] (
- cargo build --manifest-path "%test_path%%wasm_path_2%Cargo.toml" --release --target wasm32-unknown-unknown --target-dir "%hc_target_prefix%%test_path%%wasm_path_2%target" --features vendored
+ cargo build --manifest-path "%test_path%%wasm_path_2%Cargo.toml" --release --target wasm32-unknown-unknown --target-dir "%hc_target_prefix%%test_path%%wasm_path_2%target"
 )
 
-cargo test --release -p "%test_p%" --target-dir "%hc_target_prefix%%test_path%target" --features vendored
+cargo test --release -p "%test_p%" --target-dir "%hc_target_prefix%%test_path%target"

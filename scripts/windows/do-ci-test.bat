fffc4d7b5009f2588ee7128a8fc2ae1c95d4a050
David Meister <thedavidmeister@gmail.com>
2019-04-11 21:01:44 +1000

    debug do-ci-test.bat

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -1,23 +1,23 @@
 IF NOT "%wasm_path%" == "" (
- set manifest-path="%test_path%\%wasm_path%\Cargo.toml"
- set target-dir="%test_path%\%wasm_path%\target"
+ set manifest-path=%test_path%\%wasm_path%\Cargo.toml
+ set target-dir=%test_path%\%wasm_path%\target
  IF NOT "%hc_target_prefix%" == "" (
-  set target-dir="%hc_target_prefix%\%target-dir%"
+  set target-dir=%hc_target_prefix%\%target-dir%
  )
  cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
 )
 
 IF NOT "%wasm_path_2%" == "" (
- set manifest-path="%test_path%\%wasm_path_2%\Cargo.toml"
- set target-dir="%test_path%\%wasm_path_2%\target"
+ set manifest-path=%test_path%\%wasm_path_2%\Cargo.toml
+ set target-dir=%test_path%\%wasm_path_2%\target
  IF NOT "%hc_target_prefix%" == "" (
-  set target-dir="%hc_target_prefix%\%target-dir%"
+  set target-dir=%hc_target_prefix%\%target-dir%
  )
  cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
 )
 
-set target-dir="%test_path%\target"
+set target-dir=%test_path%\target
 IF NOT "%hc_target_prefix%" == "" (
- set target-dir="%hc_target_prefix%\%target-dir%"
+ set target-dir=%hc_target_prefix%\%target-dir%
 )
 cargo test --release -p "%test_p%" --target-dir "%target-dir%"

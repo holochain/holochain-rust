4a748bf18fa15c7cc4132a84875376343bbf93ad
David Meister <thedavidmeister@gmail.com>
2019-04-11 20:28:16 +1000

    debug do-ci-test.bat

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -1,11 +1,17 @@
-echo "( Join-Path %test_path% %wasm_path% Cargo.toml )"
+foo=Join-Path %test_path% %wasm_path% Cargo.toml )
+echo %foo%
 
 IF NOT "%wasm_path%" == "" (
- cargo build --manifest-path "( Join-Path %test_path% %wasm_path% Cargo.toml )" --release --target wasm32-unknown-unknown --target-dir "( Join-Path %hc_target_prefix% %test_path% %wasm_path% target )"
+ manifest-path=Join-Path %test_path% %wasm_path% Cargo.toml
+ target-dir=Join-Path %hc_target_prefix% %test_path% %wasm_path% target
+ cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
 )
 
 IF NOT "%wasm_path_2%" == "" (
- cargo build --manifest-path "( Join-Path %test_path% %wasm_path_2% Cargo.toml )" --release --target wasm32-unknown-unknown --target-dir "( Join-Path %hc_target_prefix% %test_path% %wasm_path_2% target )"
+ manifest-path=Join-Path %test_path% %wasm_path_2% Cargo.toml
+ target-dir=Join-Path %hc_target_prefix% %test_path% %wasm_path_2% target
+ cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
 )
 
-cargo test --release -p "%test_p%" --target-dir "( Join-Path %hc_target_prefix% %test_path% target )"
+target-dir=Join-Path %hc_target_prefix% %test_path% target
+cargo test --release -p "%test_p%" --target-dir "%target-dir%"

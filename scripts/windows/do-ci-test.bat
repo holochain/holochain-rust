39d229b40ea409a0ebf7c78836d942238ce77c67
David Meister <thedavidmeister@gmail.com>
2019-04-11 21:58:39 +1000

    lint

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -1,17 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo !test_path!
-echo !wasm_path!
-
 IF NOT "!wasm_path!" == "" (
  set manifest-path=!test_path!\!wasm_path!\Cargo.toml
  set target-dir=!test_path!\!wasm_path!\target
  IF NOT "!hc_target_prefix!" == "" (
   set target-dir=!hc_target_prefix!\!target-dir!
  )
- echo "x: !manifest-path!"
- echo "y: !target-dir!"
  cargo build --manifest-path !manifest-path! --release --target wasm32-unknown-unknown --target-dir !target-dir!
 )
 
@@ -21,8 +16,6 @@ IF NOT "!wasm_path_2!" == "" (
  IF NOT "!hc_target_prefix!" == "" (
   set target-dir=!hc_target_prefix!\!target-dir!
  )
- echo !manifest-path!
- echo !target-dir!
  cargo build --manifest-path !manifest-path%! --release --target wasm32-unknown-unknown --target-dir !target-dir!
 )
 
@@ -30,5 +23,4 @@ set target-dir=!test_path!\target
 IF NOT "!hc_target_prefix!" == "" (
  set target-dir=!hc_target_prefix!\!target-dir!
 )
-echo !target-dir!
 cargo test --release -p !test_p! --target-dir !target-dir!

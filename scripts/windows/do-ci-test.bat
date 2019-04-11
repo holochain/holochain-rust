e6408e21e70f385ffe4778738d68bc18bd4d7272
David Meister <thedavidmeister@gmail.com>
2019-04-11 20:53:18 +1000

    debug do-ci-test.bat

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -1,17 +1,23 @@
-for /f %%i in ('Join-Path %test_path% %wasm_path% Cargo.toml') do set foo=%i
-echo %foo%
-
 IF NOT "%wasm_path%" == "" (
- for /f %%i in ('Join-Path %test_path% %wasm_path% Cargo.toml') do set manifest-path=%i
- for /f %%i in ('Join-Path %hc_target_prefix% %test_path% %wasm_path% target') do set target-dir=%i
+ set manifest-path="%test_path%\%wasm_path%\Cargo.toml"
+ set target-dir="%test_path%\%wasm_path%\target"
+ IF NOT "%hc_target_prefix%" == "" (
+  set target-dir="%hc_target_prefix%\%target-dir%"
+ )
  cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
 )
 
 IF NOT "%wasm_path_2%" == "" (
- for /f %%i in ('Join-Path %test_path% %wasm_path_2% Cargo.toml') do set manifest-path=%i
- for /f %%i in ('Join-Path %hc_target_prefix% %test_path% %wasm_path_2% target') do set target-dir=%i
+ set manifest-path="%test_path%\%wasm_path_2%\Cargo.toml"
+ set target-dir="%test_path%\%wasm_path_2%\target"
+ IF NOT "%hc_target_prefix%" == "" (
+  set target-dir="%hc_target_prefix%\%target-dir%"
+ )
  cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
 )
 
-for /f %%i in ('Join-Path %hc_target_prefix% %test_path% target') do set target-dir=%i
+set target-dir="%test_path%\target"
+IF NOT "%hc_target_prefix%" == "" (
+ set target-dir="%hc_target_prefix%\%target-dir%"
+)
 cargo test --release -p "%test_p%" --target-dir "%target-dir%"

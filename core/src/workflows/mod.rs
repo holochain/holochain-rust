8288e98be7238912583103fafe0e75ae50f8a70f
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-04-11 13:16:31 -0400

    cargo fmt mod

--- core/src/workflows/mod.rs
+++ core/src/workflows/mod.rs
@@ -1,6 +1,7 @@
 pub mod application;
 pub mod author_entry;
 pub mod get_entry_result;
+pub mod get_link_result;
 pub mod handle_custom_direct_message;
 pub mod hold_entry;
 pub mod hold_entry_remove;
@@ -8,4 +9,3 @@ pub mod hold_entry_update;
 pub mod hold_link;
 pub mod remove_link;
 pub mod respond_validation_package_request;
-pub mod get_link_result;

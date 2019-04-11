dd430923e84a5eb886909f03c2135acc1d0e107a
David Meister <thedavidmeister@gmail.com>
2019-04-11 21:42:13 +1000

    try delayed expansion

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -1,34 +1,34 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo %test_path%
-echo %wasm_path%
+echo !test_path!
+echo !wasm_path!
 
-IF NOT "%wasm_path%" == "" (
- set manifest-path=%test_path%%wasm_path%Cargo.toml
- set target-dir=%test_path%%wasm_path%target
- IF NOT "%hc_target_prefix%" == "" (
-  set target-dir=%hc_target_prefix%%target-dir%
+IF NOT "!wasm_path!" == "" (
+ set manifest-path=!test_path!!wasm_path!Cargo.toml
+ set target-dir=!test_path!!wasm_path!target
+ IF NOT "!hc_target_prefix!" == "" (
+  set target-dir=!hc_target_prefix!!target-dir!
  )
- echo "x: %manifest-path%"
- echo "y: %target-dir%"
- cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
+ echo "x: !manifest-path!"
+ echo "y: !target-dir!"
+ cargo build --manifest-path !manifest-path! --release --target wasm32-unknown-unknown --target-dir !target-dir!
 )
 
-IF NOT "%wasm_path_2%" == "" (
- set manifest-path="%test_path%\%wasm_path_2%\Cargo.toml"
- set target-dir="%test_path%\%wasm_path_2%\target"
- IF NOT "%hc_target_prefix%" == "" (
-  set target-dir="%hc_target_prefix%\%target-dir%"
+IF NOT "!wasm_path_2!" == "" (
+ set manifest-path=!test_path!\!wasm_path_2!\Cargo.toml
+ set target-dir=!test_path!\!wasm_path_2!\target
+ IF NOT "!hc_target_prefix!" == "" (
+  set target-dir=!hc_target_prefix!\!target-dir!
  )
- echo "%manifest-path%"
- echo "%target-dir%"
- cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
+ echo !manifest-path!
+ echo !target-dir!
+ cargo build --manifest-path !manifest-path%! --release --target wasm32-unknown-unknown --target-dir !target-dir!
 )
 
-set target-dir="%test_path%\target"
-IF NOT "%hc_target_prefix%" == "" (
- set target-dir="%hc_target_prefix%\%target-dir%"
+set target-dir=!test_path!\target
+IF NOT "!hc_target_prefix!" == "" (
+ set target-dir=!hc_target_prefix!\!target-dir!
 )
-echo %target-dir%
-cargo test --release -p "%test_p%" --target-dir "%target-dir%"
+echo !target-dir!
+cargo test --release -p !test_p! --target-dir !target-dir!

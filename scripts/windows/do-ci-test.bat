74756cb4ca63febdfcb4b80884e6b58fb55888eb
David Meister <thedavidmeister@gmail.com>
2019-04-11 20:34:33 +1000

    debug do-ci-test.bat

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -1,17 +1,17 @@
-foo=Join-Path %test_path% %wasm_path% Cargo.toml )
+SET foo=Join-Path %test_path% %wasm_path% Cargo.toml )
 echo %foo%
 
 IF NOT "%wasm_path%" == "" (
- manifest-path=Join-Path %test_path% %wasm_path% Cargo.toml
- target-dir=Join-Path %hc_target_prefix% %test_path% %wasm_path% target
+ SET manifest-path=Join-Path %test_path% %wasm_path% Cargo.toml
+ SET target-dir=Join-Path %hc_target_prefix% %test_path% %wasm_path% target
  cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
 )
 
 IF NOT "%wasm_path_2%" == "" (
- manifest-path=Join-Path %test_path% %wasm_path_2% Cargo.toml
- target-dir=Join-Path %hc_target_prefix% %test_path% %wasm_path_2% target
+ SET manifest-path=Join-Path %test_path% %wasm_path_2% Cargo.toml
+ SET target-dir=Join-Path %hc_target_prefix% %test_path% %wasm_path_2% target
  cargo build --manifest-path "%manifest-path%" --release --target wasm32-unknown-unknown --target-dir "%target-dir%"
 )
 
-target-dir=Join-Path %hc_target_prefix% %test_path% target
+SET target-dir=Join-Path %hc_target_prefix% %test_path% target
 cargo test --release -p "%test_p%" --target-dir "%target-dir%"

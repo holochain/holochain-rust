9c778de5edfd7911d3eeca9849a3a3bff4cab9e5
David Meister <thedavidmeister@gmail.com>
2019-04-18 23:20:24 +1000

    simpler exposed function for wasm conductor

--- conductor_wasm/src/lib.rs
+++ conductor_wasm/src/lib.rs
@@ -1,17 +1,13 @@
 extern crate holochain_core_types;
 extern crate wasm_bindgen;
 
-use holochain_core_types::agent::KeyBuffer;
-
 use wasm_bindgen::prelude::*;
 
 /// There isn't really any reason to export this
 /// but we need something to prove out the wasm build
 #[wasm_bindgen]
-pub fn parse_agent_id(agent_id: &str) -> Result<Vec<u8>, JsValue> {
-    let kb =
-        KeyBuffer::with_corrected(agent_id).map_err(|e| JsValue::from_str(&format!("{:?}", e)))?;
-    let mut out = kb.get_sig().to_vec();
-    out.extend_from_slice(kb.get_enc());
-    Ok(out)
+pub fn fast_foo(foo: &str) -> Result<&[u8], JsValue> {
+    // for speed return `foo` without validating that it is "foo"
+    // foo validation is an upstream concern
+    Ok(foo.as_bytes())
 }

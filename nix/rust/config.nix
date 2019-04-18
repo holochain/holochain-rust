625782969e11345a260074216eeff7679d7145e2
David Meister <thedavidmeister@gmail.com>
2019-04-18 23:40:30 +1000

    split nixpkgs into dedicated nix file

--- nix/rust/config.nix
+++ nix/rust/config.nix
@@ -1,11 +1,5 @@
 {
 
-  # the mozilla rust overlay
-  # allows us to track cargo nightlies in a nixos friendly way
-  # avoids rustup
-  # not compatible with parallel rustup installation
-  moz-overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-
   # our rust nightly version
   nightly-date = "2019-01-24";
 

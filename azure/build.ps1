9ac070d028533d4cb309b51e00cdd9027918fd81
Chris Belfield <chris@devopshero.io>
2019-04-18 12:30:10 +0100

    comment out auto login

--- azure/build.ps1
+++ azure/build.ps1
@@ -1,28 +1,26 @@
-# Auto login image
-# SO THIS IS ALREADY BAKED IN
-#Reason to create callme.ps1 file is to make script copy-paste-able to PS windows and still be able to ask for password
+##  Auto login image
+##  THIS IS ALREADY BAKED IN
+# 'Write-Warning "This script will overwrite current auto-logon settings if they exist"
 
-'Write-Warning "This script will overwrite current auto-logon settings if they exist"
+# $user = whoami
+# $securePwd = Read-Host "Please enter password for current user to be saved for auto-logon" -AsSecureString 
 
-$user = whoami
-$securePwd = Read-Host "Please enter password for current user to be saved for auto-logon" -AsSecureString 
+# #http://stackoverflow.com/questions/21741803/powershell-securestring-encrypt-decrypt-to-plain-text-not-working
+# $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePwd)
+# $pwd = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-#http://stackoverflow.com/questions/21741803/powershell-securestring-encrypt-decrypt-to-plain-text-not-working
-$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePwd)
-$pwd = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+# Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -ErrorAction SilentlyContinue
+# New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -PropertyType String -Value 1
 
-Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -ErrorAction SilentlyContinue
-New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -PropertyType String -Value 1
+# Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUsername -ErrorAction SilentlyContinue
+# New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUsername -PropertyType String -Value $user 
 
-Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUsername -ErrorAction SilentlyContinue
-New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUsername -PropertyType String -Value $user 
+# Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -ErrorAction SilentlyContinue
+# New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -PropertyType String -Value $pwd
 
-Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -ErrorAction SilentlyContinue
-New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -PropertyType String -Value $pwd
+# del .\callme.ps1' > callme.ps1
 
-del .\callme.ps1' > callme.ps1
-
-.\callme.ps1
+# .\callme.ps1
 #######
 # SO THIS IS ALREADY BAKED IN
 
@@ -402,8 +400,6 @@ nodist npm add 5
 nodist npm 5
 
 npm install --global --production windows-build-tools
-
-
 
 #######################
 

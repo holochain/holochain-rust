0e37980017724d6954be256f9d1d4ceb87805fbe
Eric Harris-Braun <eric@harris-braun.com>
2019-04-19 10:03:39 -0400

    fix breaking test

--- conductor_api/src/conductor/base.rs
+++ conductor_api/src/conductor/base.rs
@@ -959,12 +959,11 @@ pub mod tests {
 
     use self::tempfile::tempdir;
     use test_utils::*;
-    #[cfg(not(windows))]
-    extern crate ws;
-    #[cfg(not(windows))]
-    use self::ws::{connect, Message};
-    #[cfg(not(windows))]
-    extern crate parking_lot;
+
+//    commented while test_signals_through_admin_websocket is broken
+//    extern crate ws;
+//    use self::ws::{connect, Message};
+//    extern crate parking_lot;
 
     pub fn test_dna_loader() -> DnaLoader {
         let loader = Box::new(|path: &PathBuf| {

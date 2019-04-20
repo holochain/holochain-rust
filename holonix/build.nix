e1147d579da4edc9adb776f465a53451ea2f5d8f
David Meister <thedavidmeister@gmail.com>
2019-04-20 18:52:12 +1000

    holonix/qt/c-bindings/src/test.nix

--- holonix/build.nix
+++ holonix/build.nix
@@ -10,5 +10,6 @@ in
 ++ import ./git/build.nix
 ++ import ./node/build.nix
 ++ import ./openssl/build.nix
+++ import ./qt/build.nix
 ++ import ./release/build.nix
 ++ import ./rust/build.nix
--- shell.nix
+++ shell.nix
@@ -67,26 +67,16 @@ let
    bash <(curl -s https://codecov.io/bash);
   '';
 
-  # simplified version of the c bindings test command in makefile
-  # hardcodes hc_dna to test rather than looping/scanning like make does
-  # might want to make this more sophisticated if we end up with many tests
-  hc-test-c-bindings = pkgs.writeShellScriptBin "hc-test-c-bindings"
-  ''
-  cargo build -p holochain_dna_c_binding
-  ( cd c_binding_tests/hc_dna && qmake -o $@Makefile $@qmake.pro && make )
-  ./target/debug/c_binding_tests/hc_dna/test_executable
-  '';
-
   hc-test = pkgs.writeShellScriptBin "hc-test"
   ''
    hc-build-wasm
    HC_SIMPLE_LOGGER_MUTE=1 RUST_BACKTRACE=1 cargo test --all --release --target-dir "$HC_TARGET_PREFIX"target "$1";
-   hc-test-c-bindings
   '';
 
   hc-test-all = pkgs.writeShellScriptBin "hc-test-all"
   ''
    hc-fmt-check \
+   && hc-qt-c-bindings-test \
    && hc-build-wasm \
    && hc-install-cli \
    && hc-install-conductor \
@@ -396,11 +386,11 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
 
-    coreutils
-    cmake
-    python
-
-    qt5.qmake
+    # I forgot what these are for!
+    # Reinstate and organise them ᕙ༼*◕_◕*༽ᕤ
+    # coreutils
+    # cmake
+    # python
 
     hc-cargo-toml-set-ver
     hc-cargo-toml-test-ver
@@ -418,7 +408,6 @@ stdenv.mkDerivation rec {
     hc-test-cli
     hc-test-app-spec
     hc-test-node-conductor
-    hc-test-c-bindings
 
     hc-test-all
 

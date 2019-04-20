e5176f88e28014e72891d703c972959a30047ee2
David Meister <thedavidmeister@gmail.com>
2019-04-20 18:11:42 +1000

    holonix/release/git/src/notes.nix

--- holonix/release/git/build.nix
+++ holonix/release/git/build.nix
@@ -1,8 +1,10 @@
 let
   branch = import ./src/branch.nix;
   merge-back = import ./src/merge-back.nix;
+  notes = import ./src/notes.nix;
 in
 [
   branch
   merge-back
+  notes
 ]
--- shell.nix
+++ shell.nix
@@ -318,68 +318,6 @@ Then run `nix-shell --run hc-prepare-release`
    | cat
   '';
 
-  release-notes-template = ''
-# ${release.core.version.current} release {{ release-date }}
-
-{{ pulse-notes }}
-
-See the [Dev Pulse](${release.pulse.url}) & [change log](https://github.com/holochain/holochain-rust/blob/release-${release.core.version.current}/CHANGELOG.md) for complete details.
-
-## **Installation**
-
-This release consists of binary builds of:
-
-- the [`hc` development command-line tool](https://github.com/holochain/holochain-rust/blob/${release.core.tag}/cli/README.md)
-- [`holochain` deployment conductor](https://github.com/holochain/holochain-rust/blob/${release.core.tag}/conductor/README.md) for different platforms.
-
-To install, simply download and extract the binary for your platform.
-See our [installation quick-start instructions](https://developer.holochain.org/start.html) for details.
-
-Rust and NodeJS are both required for `hc` to build and test DNA:
-
-- [Rust](https://www.rust-lang.org/en-US/install.html)
-  - Must be `nightly-${rust.nightly-date}` build with the WASM build target.
-    Once you have first installed rustup:
-    ```
-    rustup toolchain install nightly-${rust.nightly-date}
-    rustup default nightly-${rust.nightly-date}
-    rustup target add wasm32-unknown-unknown --toolchain nightly-${rust.nightly-date}
-    ```
-- [Node.js](https://nodejs.org) version 8 or higher
-  - E2E tests for Holochain apps are written in Javascript client-side and executed in NodeJS through websockets
-  - For further info, check out [the holochain-nodejs module](https://www.npmjs.com/package/@holochain/holochain-nodejs)
-
-### **Which Binary?**
-
-Download only the binaries for your operating system.
-
-- MacOS: `cli-${release.core.tag}-x86_64-apple-darwin.tar.gz`
-- Linux: `cli-${release.core.tag}-x86_64-ubuntu-linux-gnu.tar.gz`
-- Windows:
-  - mingw build system: `cli-${release.core.tag}-x86_64-pc-windows-gnu.tar.gz`
-  - Visual Studio build system: `cli-${release.core.tag}-x86_64-pc-windows-msvc.tar.gz`
-
-All binaries are for 64-bit operating systems.
-32-bit systems are NOT supported.
-  '';
-  hc-generate-release-notes = pkgs.writeShellScriptBin "hc-generate-release-notes"
-  ''
-   TEMPLATE=$( echo '${release-notes-template}' )
-
-   DATE_PLACEHOLDER='{{ release-date }}'
-   DATE=$( date --iso -u )
-   WITH_DATE=''${TEMPLATE/$DATE_PLACEHOLDER/$DATE}
-
-   PULSE_PLACEHOLDER='{{ pulse-notes }}'
-   # magic
-   # gets a markdown version of pulse
-   # greps for everything from summary to details (not including details heading)
-   # deletes null characters that throw warnings in bash
-   PULSE_NOTES=$( curl -s https://md.unmediumed.com/${release.pulse.url} | grep -Pzo "(?s)(###\s+\**Summary.*)(?=###\s+\**Details)" | tr -d '\0' )
-   WITH_NOTES=''${WITH_DATE/$PULSE_PLACEHOLDER/$PULSE_NOTES}
-   echo "$WITH_NOTES"
-  '';
-
   hc-check-release-artifacts = pkgs.writeShellScriptBin "hc-check-release-artifacts"
   ''
   echo
@@ -523,7 +461,6 @@ stdenv.mkDerivation rec {
     hc-prepare-release
     hc-changelog-grep-pr-references
     hc-ensure-changelog-version
-    hc-generate-release-notes
     hc-readme-grep-nightly
     hc-build-release-artifacts
 

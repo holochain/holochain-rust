2fd6f303b525cdc416b7491798fd7f0be5030a19
David Meister <thedavidmeister@gmail.com>
2019-04-20 19:21:09 +1000

    holonix/release/src/prepare.nix

--- holonix/release/build.nix
+++ holonix/release/build.nix
@@ -1,7 +1,9 @@
 let
-
+  prepare = import ./src/prepare.nix;
 in
-[]
+[
+  prepare
+]
 ++ import ./github/build.nix
 ++ import ./npm/build.nix
 ++ import ./pulse/build.nix
--- shell.nix
+++ shell.nix
@@ -177,41 +177,6 @@ Then run `nix-shell --run hc-prepare-release`
   fi
   '';
 
-  hc-prepare-release = pkgs.writeShellScriptBin "hc-prepare-release"
-  ''
-   echo
-   echo "IMPORTANT: make sure git-hub is setup on your machine"
-   echo "1. Visit https://github.com/settings/tokens/new"
-   echo "2. Generate a token called 'git-hub' with 'user' and 'repo' scopes"
-   echo "3. git config --global hub.oauthtoken <token>"
-   echo "4. git config --global hub.username <username>"
-   echo
-   echo "Current nix-shell config:"
-   echo
-   echo "pulse-url-hash: ${release.pulse.url-hash}"
-   echo "pulse-version: ${release.pulse.version}"
-   echo "pulse-commit: ${release.pulse.commit}"
-   echo "core-previous-version: ${release.core.version.previous}"
-   echo "core-version: ${release.core.version.current}"
-   echo "node-conductor-previous-version: ${release.node-conductor.version.previous}"
-   echo "node-conductor-version: ${release.node-conductor.version.current}"
-   git hub --version
-   echo
-   read -r -p "Are you sure you want to cut a new release based on the current config in shell.nix? [y/N] " response
-   case "$response" in
-    [yY][eE][sS]|[yY])
-     hc-release-pulse-tag \
-     && hc-release-git-branch \
-     && hc-prepare-crate-versions \
-     && hc-ensure-changelog-version \
-     && hc-prepare-release-pr \
-     ;;
-    *)
-     exit 1
-     ;;
-   esac
-  '';
-
   hc-do-release = pkgs.writeShellScriptBin "hc-do-release"
   ''
   echo
@@ -411,7 +376,6 @@ stdenv.mkDerivation rec {
     hc-prepare-crate-versions
     hc-check-release-artifacts
 
-    hc-prepare-release
     hc-changelog-grep-pr-references
     hc-ensure-changelog-version
     hc-readme-grep-nightly

36641753d4975f18c68dfadd33850f5db4e5742e
David Meister <thedavidmeister@gmail.com>
2019-04-20 19:36:53 +1000

    holonix/release/github/src/check-artifacts.nix

--- holonix/release/github/build.nix
+++ holonix/release/github/build.nix
@@ -1,11 +1,13 @@
 let
   branch = import ./src/branch.nix;
+  check-artifacts = import ./src/check-artifacts.nix;
   merge-back = import ./src/merge-back.nix;
   notes = import ./src/notes.nix;
   pr = import ./src/pr.nix;
 in
 [
   branch
+  check-artifacts
   merge-back
   notes
   pr
--- shell.nix
+++ shell.nix
@@ -175,63 +175,6 @@ let
    | cat
   '';
 
-  hc-check-release-artifacts = pkgs.writeShellScriptBin "hc-check-release-artifacts"
-  ''
-  echo
-  echo "Checking core artifacts"
-  echo
-
-  echo
-  echo "checking ${release.core.tag}"
-  echo
-
-  core_binaries=( "cli" "conductor" )
-  core_platforms=( "apple-darwin" "pc-windows-gnu" "pc-windows-msvc" "unknown-linux-gnu" )
-
-  for binary in "''${core_binaries[@]}"
-  do
-   for platform in "''${core_platforms[@]}"
-   do
-    file="$binary-${release.core.tag}-x86_64-$platform.tar.gz"
-    url="https://github.com/holochain/holochain-rust/releases/download/${release.core.tag}/$file"
-    echo
-    echo "pinging $file for release $release..."
-    if curl -Is "$url" | grep -q "HTTP/1.1 302 Found"
-     then echo "FOUND ✔"
-     else echo "NOT FOUND ⨯"
-    fi
-    echo
-   done
-  done
-
-  echo
-  echo "Checking node conductor artifacts"
-  echo
-
-  echo
-  echo "checking ${release.node-conductor.tag}"
-  echo
-
-  node_versions=( "57" "64" "67" )
-  conductor_platforms=( "darwin" "linux" "win32" )
-
-  for node_version in "''${node_versions[@]}"
-  do
-   for platform in "''${conductor_platforms[@]}"
-   do
-    file="index-v${release.node-conductor.version.current}-node-v''${node_version}-''${platform}-x64.tar.gz"
-    url="https://github.com/holochain/holochain-rust/releases/download/${release.node-conductor.tag}/$file"
-    echo
-    echo "pinging $file for release $release..."
-    if curl -Is "$url" | grep -q "HTTP/1.1 302 Found"
-     then echo "FOUND ✔"
-     else echo "NOT FOUND ⨯"
-    fi
-    echo
-   done
-  done
-  '';
-
   build-release-artifact = params:
   ''
    export artifact_name=`sed "s/unknown/generic/g" <<< "${params.path}-${release.core.version.current}-${rust.generic-linux-target}"`
@@ -307,7 +250,6 @@ stdenv.mkDerivation rec {
     hc-codecov
 
     hc-prepare-crate-versions
-    hc-check-release-artifacts
 
     hc-changelog-grep-pr-references
     hc-ensure-changelog-version

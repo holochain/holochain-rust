d7f7731c8981a457da52ced087d7f94796c1d504
David Meister <thedavidmeister@gmail.com>
2019-04-21 12:52:47 +1000

    move OPENSSL_STATIC into holonix/openssl/config.nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ let
 
   pkgs = import ./holonix/nixpkgs/nixpkgs.nix;
   darwin = import ./holonix/darwin/config.nix;
+  openssl = import ./holonix/openssl/config.nix;
   rust = import ./holonix/rust/config.nix;
 
 in
@@ -11,11 +12,6 @@ stdenv.mkDerivation rec {
 
   buildInputs = import ./holonix/build.nix;
 
-  RUSTFLAGS = rust.compile.flags;
-  CARGO_INCREMENTAL = rust.compile.incremental;
-  RUST_LOG = rust.log;
-  NUM_JOBS = rust.compile.jobs;
-
   # non-nixos OS can have a "dirty" setup with rustup installed for the current
   # user.
   # `nix-shell` can inherit this e.g. through sourcing `.bashrc`.
@@ -24,9 +20,13 @@ stdenv.mkDerivation rec {
   # rust version through this environment variable.
   # https://github.com/rust-lang/rustup.rs#environment-variables
   # https://github.com/NixOS/nix/issues/903
-  RUSTUP_TOOLCHAIN = "${rust.nightly.version}";
+  RUSTUP_TOOLCHAIN = rust.nightly.version;
+  RUSTFLAGS = rust.compile.flags;
+  CARGO_INCREMENTAL = rust.compile.incremental;
+  RUST_LOG = rust.log;
+  NUM_JOBS = rust.compile.jobs;
 
-  OPENSSL_STATIC = "1";
+  OPENSSL_STATIC = openssl.static;
 
   shellHook = ''
    # cargo installs things to the user's home so we need it on the path

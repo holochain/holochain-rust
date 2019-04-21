4814a186404054a6620676589b339247212738a6
David Meister <thedavidmeister@gmail.com>
2019-04-21 19:42:06 +1000

    placeholder mac hashes for binaries

--- holonix/dist/src/hc.nix
+++ holonix/dist/src/hc.nix
@@ -1,8 +1,15 @@
 let
+ pkgs = import ../../nixpkgs/nixpkgs.nix;
  lib = import ./lib.nix;
 in
 lib.binary-derivation {
  name = "cli";
- sha256 = "15frnjn3q4mfsg53dy59mwnkhzwkf6iwm0d5jix2d575i8cyn5xi";
+ sha256 = if pkgs.stdenv.isDarwin
+ then
+  # TODO - this hash will be wrong on mac!
+  "15frnjn3q4mfsg53dy59mwnkhzwkf6iwm0d5jix2d575i8cyn5xi"
+ else
+  "15frnjn3q4mfsg53dy59mwnkhzwkf6iwm0d5jix2d575i8cyn5xi"
+ ;
  binary = "hc";
 }
--- holonix/dist/src/holochain.nix
+++ holonix/dist/src/holochain.nix
@@ -1,8 +1,15 @@
 let
+ pkgs = import ../../nixpkgs/nixpkgs.nix;
  lib = import ./lib.nix;
 in
 lib.binary-derivation {
  name = "conductor";
- sha256 = "0wdlv85vwwp9cwnmnsp20aafrxljsxlc6m00h0905q0cydsf86kq";
+ sha256 = if pkgs.stdenv.isDarwin
+ then
+  # TODO - this hash will be wrong on mac!
+  "0wdlv85vwwp9cwnmnsp20aafrxljsxlc6m00h0905q0cydsf86kq"
+ else
+  "0wdlv85vwwp9cwnmnsp20aafrxljsxlc6m00h0905q0cydsf86kq"
+ ;
  binary = "holochain";
 }

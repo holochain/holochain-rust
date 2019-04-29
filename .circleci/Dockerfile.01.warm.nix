66580cab3593db7cc7be2e6bb358ac8377ee057e
David Meister <thedavidmeister@gmail.com>
2019-04-29 22:35:56 +1000

    debug

--- .circleci/Dockerfile.01.warm.nix
+++ .circleci/Dockerfile.01.warm.nix
@@ -2,5 +2,7 @@ FROM holochain/holochain-rust:circle..start
 
 RUN ls
 
+RUN ls ./holonix/dist
+
 # run a no-op to warm the nix store
 RUN nix-shell --run "echo 1" --show-trace --max-jobs 4 --cores 0

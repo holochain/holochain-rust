35079e0af475b3045f01306554224d71a84b6130
David Meister <thedavidmeister@gmail.com>
2019-04-30 00:51:44 +1000

    less things in default.nix

--- default.nix
+++ default.nix
@@ -2,12 +2,18 @@ let
 
   pkgs = import ./holonix/nixpkgs/nixpkgs.nix;
 
+  cli = import ./holonix/dist/cli/build.nix;
+  conductor = import ./holonix/dist/conductor.build.nix;
+
 in
 with pkgs;
 stdenv.mkDerivation rec {
 
  name = "holonix-binaries";
 
- buildInputs = import ./holonix/dist/build.nix;
+ buildInputs = [
+  cli
+  conductor
+ ];
 
 }

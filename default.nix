9dd8648b2be398dd51433f18170cdc268bdd51c7
David Meister <thedavidmeister@gmail.com>
2019-04-30 01:01:01 +1000

    lint

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
   pkgs = import ./holonix/nixpkgs/nixpkgs.nix;
 
   cli = import ./holonix/dist/cli/build.nix;
-  conductor = import ./holonix/dist/conductor.build.nix;
+  conductor = import ./holonix/dist/conductor/build.nix;
 
 in
 with pkgs;

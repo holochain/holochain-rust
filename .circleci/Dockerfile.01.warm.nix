ab3c21e126a792755fac699c25248bf5ddf9b662
David Meister <thedavidmeister@gmail.com>
2019-04-29 22:47:40 +1000

    don't ignore holonix in dockerfile

--- .circleci/Dockerfile.01.warm.nix
+++ .circleci/Dockerfile.01.warm.nix
@@ -1,8 +1,4 @@
 FROM holochain/holochain-rust:circle..start
 
-RUN ls
-
-RUN ls ./holonix/dist
-
 # run a no-op to warm the nix store
 RUN nix-shell --run "echo 1" --show-trace --max-jobs 4 --cores 0

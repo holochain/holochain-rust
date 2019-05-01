ec458f3e868fcaad1f59cf986a0ae7add60b45f8
David Meister <thedavidmeister@gmail.com>
2019-05-01 20:04:52 +1000

    working nix build for cli and conductor

--- default.nix
+++ default.nix
@@ -1,19 +1,4 @@
-let
-
-  pkgs = import ./holonix/nixpkgs/nixpkgs.nix;
-
+{
   cli = import ./holonix/dist/cli/build.nix;
   conductor = import ./holonix/dist/conductor/build.nix;
-
-in
-with pkgs;
-stdenv.mkDerivation rec {
-
- name = "holochain-binaries";
-
- buildInputs = [
-  cli
-  conductor
- ];
-
 }

7d1ff9952aea67964481bdd7f97e19c9f3f632d5
Willem Olding <willemolding@gmail.com>
2019-05-08 12:46:25 +1000

    adds test that agent keystore was added to conductor

--- conductor_api/src/conductor/test_admin.rs
+++ conductor_api/src/conductor/test_admin.rs
@@ -49,10 +49,12 @@ pub mod tests {
     #[test]
     fn test_add_test_agent() {
         let test_name = "test_add_test_agent";
+        let agent_id = "testAgent1".to_string();
         let mut conductor = create_test_conductor(test_name, 5001);
         let agent_address = conductor
-            .add_test_agent("testAgent1".to_string(), "Test Agent 1".to_string())
+            .add_test_agent(agent_id.clone(), "Test Agent 1".to_string())
             .expect("Could not add test agent");
-        assert_eq!(agent_address.len(), 63,)
+        assert_eq!(agent_address.len(), 63,);
+        assert!(conductor.get_keystore_for_agent(&agent_id).is_ok());
     }
 }

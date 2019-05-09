19a4cb462f26aa39ba8e9fdd822827f4f5bf66fd
Eric Harris-Braun <eric@harris-braun.com>
2019-05-09 10:43:04 -0400

    Release 0.0.15-alpha1

--- holonix/release/pulse/config.nix
+++ holonix/release/pulse/config.nix
@@ -3,9 +3,9 @@ let
   # the unique hash at the end of the medium post url
   # e.g. https://medium.com/@holochain/foos-and-bars-4867d777de94
   # would be 4867d777de94
-  url-hash = "f653f10a82d8";
+  url-hash = "58e5caf1916";
   # current dev-pulse iteration, as seen by general public
-  version = "27";
+  version = "28";
   hash-list = "https://bit.ly/2LiQuJk";
  };
 

158e2eb4f1c4fe4bc46ef33d8f81322a833105db
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-05-13 14:40:47 -0400

    Fixes to eavi

--- core_types/src/eav/eavi.rs
+++ core_types/src/eav/eavi.rs
@@ -279,7 +279,8 @@ pub fn eav_round_trip_test_runner(
                 Some(entity_content.address()).into(),
                 Some(attribute.clone()).into(),
                 Some(value_content.address()).into(),
-                IndexFilter::LatestByAttribute
+                IndexFilter::LatestByAttribute,
+                None
             ))
             .expect("could not fetch eav"),
     );
@@ -320,7 +321,8 @@ pub fn eav_round_trip_test_runner(
                     e.into(),
                     a.into(),
                     v.into(),
-                    IndexFilter::LatestByAttribute
+                    IndexFilter::LatestByAttribute,
+                    None
                 ))
                 .expect("could not fetch eav")
         );

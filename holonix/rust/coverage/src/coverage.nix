f00b1499701e459931989151bd2dfee9a990a046
David Meister <thedavidmeister@gmail.com>
2019-05-16 05:10:55 +1000

    run tarpaulin 0.8.0 on circle for debugging

--- holonix/rust/coverage/src/coverage.nix
+++ holonix/rust/coverage/src/coverage.nix
@@ -7,7 +7,17 @@ let
   # @see https://github.com/xd009642/tarpaulin/issues/190
   script = pkgs.writeShellScriptBin name
   ''
-  cargo tarpaulin --ignore-tests --timeout 600 --all --out Xml -v -e holochain_core_api_c_binding -e hdk -e hc -e holochain_core_types_derive
+  cargo tarpaulin \
+    --ignore-tests \
+    --timeout 600 \
+    --all \
+    --debug \
+    --out Xml \
+    -v \
+    -e holochain_core_api_c_binding \
+    -e hdk \
+    -e hc \
+    -e holochain_core_types_derive
   '';
 in
 script
--- holonix/rust/coverage/src/install.nix
+++ holonix/rust/coverage/src/install.nix
@@ -3,11 +3,13 @@ let
 
   name = "hc-rust-coverage-install";
 
+  version = "0.8.0";
+
   script = pkgs.writeShellScriptBin name
   ''
    if ! cargo --list | grep --quiet tarpaulin;
    then
-    RUSTFLAGS="--cfg procmacro2_semver_exempt" cargo install cargo-tarpaulin;
+    RUSTFLAGS="--cfg procmacro2_semver_exempt" cargo install --version ${version} cargo-tarpaulin;
    fi;
   '';
 in

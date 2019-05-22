5e41b8ec06b26809042dd7a95fca6877648f47a0
David Meister <thedavidmeister@gmail.com>
2019-05-23 09:15:31 +1000

    removing pipes on newlines

--- holonix/release/rust/manifest/src/version-sync.nix
+++ holonix/release/rust/manifest/src/version-sync.nix
@@ -11,24 +11,21 @@ let
     -name "Cargo.toml" \
     -not -path "**/.git/**" \
     -not -path "**/.cargo/**" \
-    -not -path "./nodejs_conductor*" \
-    | xargs -I {} \
+    -not -path "./nodejs_conductor*" | xargs -I {} \
     sed -i 's/^\s*version\s*=\s*"${release.core.version.previous}"\s*$/version = "${release.core.version.current}"/g' {}
 
    echo "bumping core versions from ${release.core.version.previous} to ${release.core.version.current} in readmes"
    find . \
     -iname "readme.md" \
     -not -path "**/.git/**" \
-    -not -path "**/.cargo/**" \
-    | xargs -I {} \
+    -not -path "**/.cargo/**" | xargs -I {} \
     sed -i 's/${release.core.version.previous}/${release.core.version.current}/g' {}
 
    echo "bumping versions from ${release.core.version.previous} to ${release.core.version.current} in CLI"
    find . \
     -type f \
     -not -path "**/.git/**" \
-    -path "./cli/*" \
-    | xargs -I {} \
+    -path "./cli/*" | xargs -I {} \
     sed -i 's/${release.core.version.previous}/${release.core.version.current}/g' {}
 
    echo "bumping node conductor version from ${release.node-conductor.version.previous} to ${release.node-conductor.version.current}"

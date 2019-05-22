f37fc0cb73b8cd880e707e0d6ecd8507aeb41063
Willem Olding <willemolding@gmail.com>
2019-05-22 13:47:55 +1000

    adds empty callback for agent_entry that always is Ok

--- core/src/nucleus/validation/mod.rs
+++ core/src/nucleus/validation/mod.rs
@@ -11,6 +11,7 @@ use holochain_core_types::{
 use std::sync::Arc;
 
 mod app_entry;
+mod agent_entry;
 mod header_address;
 mod link_entry;
 mod provenances;
@@ -116,13 +117,12 @@ pub async fn validate_entry(
         // a grant should always be private, so it should always pass
         EntryType::CapTokenGrant => Ok(()),
 
-        // TODO: actually check agent against app specific membrane validation rule
-        // like for instance: validate_agent_id(
-        //                      entry.clone(),
-        //                      validation_data,
-        //                      context,
-        //                    )?
-        EntryType::AgentId => Ok(()),
+        // Runs the AgentId entry callbacks for each zome
+        EntryType::AgentId => await!(agent_entry::validate_agent_entry(
+            entry.clone(),
+            validation_data,
+            context,
+        )),
 
         _ => Err(ValidationError::NotImplemented),
     }

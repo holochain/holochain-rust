eb16936e2996a5f63a37477db7264984f2985fff
David Meister <thedavidmeister@gmail.com>
2019-05-23 09:13:25 +1000

    semicolons

--- holonix/release/pulse/src/tag.nix
+++ holonix/release/pulse/src/tag.nix
@@ -14,15 +14,15 @@ let
    git fetch --tags
    if git tag | grep -q "${release-pulse.tag}"
     then
-     echo "pulse tag for pulse ${release-pulse.version} already exists locally! doing nothing..."
-     echo "pulse commit: $(git show-ref -s ${release-pulse.tag})"
-     echo "to push upstream run: git push ${git.github.upstream} ${release-pulse.tag}"
+     echo "pulse tag for pulse ${release-pulse.version} already exists locally! doing nothing...";
+     echo "pulse commit: $(git show-ref -s ${release-pulse.tag})";
+     echo "to push upstream run: git push ${git.github.upstream} ${release-pulse.tag}";
     else
-     echo "tagging..."
-     git tag -a ${release-pulse.tag} ${release.commit} -m 'Dev pulse ${release-pulse.version}'
-     echo "pushing..."
-     git push ${git.github.upstream} ${release-pulse.tag}
-     echo $'pulse tag ${release-pulse.tag} created and pushed'
+     echo "tagging...";
+     git tag -a ${release-pulse.tag} ${release.commit} -m 'Dev pulse ${release-pulse.version}';
+     echo "pushing...";
+     git push ${git.github.upstream} ${release-pulse.tag};
+     echo $'pulse tag ${release-pulse.tag} created and pushed';
    fi
    echo
    echo 'pulse tag on github: https://github.com/holochain/holochain-rust/releases/tag/${release-pulse.tag}'

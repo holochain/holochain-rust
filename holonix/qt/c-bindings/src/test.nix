47ef6b1717010bb9ae2a5a9ce132063e21a4a182
David Meister <thedavidmeister@gmail.com>
2019-05-23 08:56:11 +1000

    whitespace

--- holonix/qt/c-bindings/src/test.nix
+++ holonix/qt/c-bindings/src/test.nix
@@ -8,10 +8,10 @@ let
 
   script = pkgs.writeShellScriptBin name
   ''
-   hc-qt-c-bindings-flush
-   cargo build -p holochain_dna_c_binding
-   ( cd c_binding_tests/hc_dna && qmake -o $@Makefile $@qmake.pro && make )
-   ./target/debug/c_binding_tests/hc_dna/test_executable
+  hc-qt-c-bindings-flush
+  cargo build -p holochain_dna_c_binding
+  ( cd c_binding_tests/hc_dna && qmake -o $@Makefile $@qmake.pro && make )
+  ./target/debug/c_binding_tests/hc_dna/test_executable
   '';
 in
 script

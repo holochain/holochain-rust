db056ade04008f8088cbd689ccb3708732ca2991
Willem Olding <willemolding@gmail.com>
2019-05-22 14:39:30 +1000

    adds logic to run validation callback in all zomes for the agent id

--- core/src/nucleus/validation/agent_entry.rs
+++ core/src/nucleus/validation/agent_entry.rs
@@ -1,49 +1,40 @@
 use crate::{
     context::Context,
     nucleus::{
-        // actions::{
-            // get_entry::get_entry_from_dht, run_validation_callback::run_validation_callback,
-        // },
+        actions::{
+            run_validation_callback::run_validation_callback,
+        },
         validation::{
-            // entry_to_validation_data, ValidationError,
-            ValidationResult},
-        // CallbackFnCall,
+            ValidationResult
+        },
+        CallbackFnCall,
     },
 };
 use holochain_core_types::{
-    // cas::content::{Address, AddressableContent},
-    entry::{
-        // entry_type::AppEntryType, 
-        Entry},
+    entry::Entry,
+    json::JsonString,
+    cas::content::AddressableContent,
     validation::ValidationData,
 };
-// use holochain_wasm_utils::api_serialization::validation::EntryValidationArgs;
 use std::sync::Arc;
+use futures::future;
+use futures_util::future::FutureExt;
 
 pub async fn validate_agent_entry(
-    _entry: Entry,
+    entry: Entry,
     _validation_data: ValidationData,
-    _context: &Arc<Context>,
+    context: &Arc<Context>,
 ) -> ValidationResult {
+    let dna = context.get_dna().expect("Callback called without DNA set!");
+
+    let _results = await!(future::join_all(
+        dna.zomes
+        .iter()
+        .map(|(zome_name, _)| {
+            let call = CallbackFnCall::new(&zome_name, "__hdk_validate_agent_entry", JsonString::empty_object());
+            run_validation_callback(entry.address(), call, &context).boxed()
+        })
+    ));
+
     Ok(())
-    // let dna = context.get_dna().expect("Callback called without DNA set!");
-
-    // let zome_name = dna
-    //     .get_zome_name_for_app_entry_type(&app_entry_type)
-    //     .ok_or(ValidationError::NotImplemented)?;
-    // if let Some(expected_link_update) = link.clone() {
-    //     get_entry_from_dht(&context.clone(), &expected_link_update).map_err(|_| {
-    //         ValidationError::UnresolvedDependencies(vec![expected_link_update.clone()])
-    //     })?;
-    // };
-
-    // let params = EntryValidationArgs {
-    //     validation_data: entry_to_validation_data(context.clone(), &entry, link, validation_data)
-    //         .map_err(|_| {
-    //         ValidationError::Fail("Could not get entry validation".to_string())
-    //     })?,
-    // };
-    // let call = CallbackFnCall::new(&zome_name, "__hdk_validate_app_entry", params);
-
-    // await!(run_validation_callback(entry.address(), call, &context))
 }

f389aecaa3cda347a798a3fc9d3ff5bf5ef5c083
David Meister <thedavidmeister@gmail.com>
2019-05-27 21:58:35 +1000

    dist audit

--- holonix/dist/cli/config.nix
+++ holonix/dist/cli/config.nix
@@ -2,8 +2,8 @@
  name = "cli";
 
  sha256 = {
-  linux = "05l7gz57ccxf3mkk9gyr2f9axmq53bz3hzj4w7yhd182c1xrp16r";
-  darwin = "0rw41zzzxd0wk0h76hg2nl7ssvyrvx14mb34axschixrjgv8yxgv";
+  linux = "1pq8kb9w7jfqyy30dqwr5vk11w6b37v23w8ck6mk199dpgbl4v8h";
+  darwin = "1brkj80aqf48v03v3w4wpqqvdywx43jr3paw9kjy3w7096bzzmmm";
  };
 
  binary = "hc";

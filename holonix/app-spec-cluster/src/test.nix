a0524e1e6a81f24ddf621ec305afc66d8ea1bb3e
Connor Turland <connorturland@gmail.com>
2019-05-28 11:49:06 -0400

    make defaults explicit

--- holonix/app-spec-cluster/src/test.nix
+++ holonix/app-spec-cluster/src/test.nix
@@ -9,7 +9,7 @@ let
   hc-conductor-rust-install
    ( cd hc_cluster_test && npm install && ./node_modules/.bin/tsc)
    ( cd app_spec && mkdir -p dist && hc package --output dist/app_spec.dna.json --strip-meta )
-   ( EMULATION_HOLOCHAIN_BIN_PATH=./.cargo/bin/holochain node ./app_spec/cluster_test/index.js )
+   ( EMULATION_HOLOCHAIN_BIN_PATH=./.cargo/bin/holochain node ./app_spec/cluster_test/index.js 2 false)
   '';
 in
 script

52b3505b3ac0b6b4439825a7d7ad32aa5b851bee
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-05-31 09:37:06 -0400

    Update index.js

--- app_spec/cluster_test/index.js
+++ app_spec/cluster_test/index.js
@@ -38,7 +38,7 @@ const scenarioTest = async (numConductors = 2, debugging = false) => {
         })
       }
     }
-  }, 5000)
+  }, 35000)
 
   let countHolding = 0
   cluster.batch(conductor => conductor.onSignal(async signal => {

260d2a65ac2944301aba450d85dc923a79c34e5f
dymayday <meidhy.demagny@holo.host>
2019-05-31 23:18:01 +0200

    Fix test_check_dna_consistency

--- conductor_api/src/conductor/base.rs
+++ conductor_api/src/conductor/base.rs
@@ -1412,7 +1412,7 @@ pub mod tests {
         [[dnas]]
         id = "bridge-caller"
         file = "bridge/caller.dna"
-        hash = "QmdCoUqyeAKuVsTFasJnUeReaqqFvfT6toMeWieCCtrF3V"
+        hash = "QmTLP5vrg1edcucavGwMXcLLMAof2nd16P6XBzLk9kxQ4x"
 
         [[instances]]
         id = "test-instance-1"

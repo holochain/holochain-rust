1b934471143f97bf7155d306b55631af6575b966
Willem Olding <willemolding@gmail.com>
2019-06-13 16:38:35 +1000

    adds proc macro template

--- cli/src/cli/generate.rs
+++ cli/src/cli/generate.rs
@@ -47,7 +47,11 @@ pub fn generate(zome_name: &PathBuf, language: &str) -> DefaultResult<()> {
     // match against all supported languages
     match language {
         "rust" => scaffold(
-            &scaffold::rust::RustScaffold::new(&zome_name_string),
+            &scaffold::rust::RustScaffold::new(&zome_name_string, scaffold::rust::HdkMacroStyle::Declerative),
+            code_dir,
+        )?,
+        "rust-proc" => scaffold(
+            &scaffold::rust::RustScaffold::new(&zome_name_string, scaffold::rust::HdkMacroStyle::Procedural),
             code_dir,
         )?,
         "assemblyscript" => scaffold(

2c82579ed4b22460563879f61337ac9631d167bb
Willem Olding <willemolding@gmail.com>
2019-06-13 16:45:05 +1000

    remove handle prefix from zome funcs

--- cli/src/cli/scaffold/rust-proc-macro/lib.rs
+++ cli/src/cli/scaffold/rust-proc-macro/lib.rs
@@ -56,14 +56,14 @@ mod my_zome {
     }   
 
     #[zome_fn("hc_public")]
-    fn handle_create_my_entry(entry: MyEntry) -> ZomeApiResult<Address> {
+    fn create_my_entry(entry: MyEntry) -> ZomeApiResult<Address> {
         let entry = Entry::App("my_entry".into(), entry.into());
         let address = hdk::commit_entry(&entry)?;
         Ok(address)
     }
 
     #[zome_fn("hc_public")]
-    fn handle_get_my_entry(address: Address) -> ZomeApiResult<Option<Entry>> {
+    fn get_my_entry(address: Address) -> ZomeApiResult<Option<Entry>> {
         hdk::get_entry(&address)
     }
 

728f539b327e0b525b72f084987c1dba637cc746
Nicolas Luck <nicolas@lucksus.eu>
2019-06-19 19:22:03 +0200

    Implement handle_fetch_entry for all meta aspects

--- core/src/network/handler/fetch.rs
+++ core/src/network/handler/fetch.rs
@@ -5,12 +5,14 @@ use crate::{
     instance::dispatch_action,
     network::entry_aspect::EntryAspect,
     nucleus,
+    workflows::get_entry_result::get_entry_with_meta_workflow,
 };
-use holochain_core_types::cas::content::Address;
+use boolinator::*;
+use holochain_core_types::{
+    cas::content::Address, eav::Attribute, entry::Entry, error::HolochainError, time::Timeout,
+};
 use holochain_net::connection::json_protocol::FetchEntryData;
 use std::sync::Arc;
-use holochain_core_types::error::HolochainError;
-use boolinator::*;
 
 /// The network has requested a DHT entry from us.
 /// Lets try to get it and trigger a response.
@@ -20,21 +22,40 @@ pub fn handle_fetch_entry(get_dht_data: FetchEntryData, context: Arc<Context>) {
     let address = Address::from(get_dht_data.entry_address.clone());
     let mut aspects = vec![];
 
-    if let Ok(content) = get_content_aspect(&address, context.clone()) {
-        aspects.push(content);
-
-
-    } else {
-        context.log(format!("warn/net/handle_fetch_entry: Could not get content aspect of requested entry {:?}", address));
+    match get_content_aspect(&address, context.clone()) {
+        Ok(content_aspect) => {
+            aspects.push(content_aspect);
+            match get_meta_aspects(&address, context.clone()) {
+                Ok(mut meta_aspects) => aspects.append(&mut meta_aspects),
+                Err(get_meta_error) => {
+                    context.log(format!(
+                        "error/net/handle_fetch_entry: Error getting meta aspects for entry ({:?}), error: {:?}",
+                        address,
+                        get_meta_error,
+                    ));
+                }
+            }
+        }
+        Err(get_content_error) => {
+            context.log(format!(
+                "warn/net/handle_fetch_entry: Could not get content aspect of requested entry ({:?}), error: {:?}",
+                address,
+                get_content_error,
+            ));
+        }
     }
 
     let action_wrapper = ActionWrapper::new(Action::RespondFetch((get_dht_data, aspects)));
     dispatch_action(context.action_channel(), action_wrapper.clone());
 }
 
-fn get_content_aspect(entry_address: &Address, context: Arc<Context>) -> Result<EntryAspect, HolochainError> {
-    let entry_with_meta = nucleus::actions::get_entry::get_entry_with_meta(&context, entry_address.clone())?
-        .ok_or(HolochainError::EntryNotFoundLocally)?;
+fn get_content_aspect(
+    entry_address: &Address,
+    context: Arc<Context>,
+) -> Result<EntryAspect, HolochainError> {
+    let entry_with_meta =
+        nucleus::actions::get_entry::get_entry_with_meta(&context, entry_address.clone())?
+            .ok_or(HolochainError::EntryNotFoundLocally)?;
 
     let _ = entry_with_meta
         .entry
@@ -62,6 +83,65 @@ fn get_content_aspect(entry_address: &Address, context: Arc<Context>) -> Result<
         entry_with_meta.entry,
         headers[0].clone(),
     ))
+}
+
+fn get_meta_aspects(
+    entry_address: &Address,
+    context: Arc<Context>,
+) -> Result<Vec<EntryAspect>, HolochainError> {
+    let eavis = context
+        .state()
+        .expect("Could not get state for handle_fetch_entry")
+        .dht()
+        .get_all_metas(entry_address)?;
+
+    let (aspects, errors): (Vec<_>, Vec<_>) = eavis
+        .iter()
+        .filter(|eavi| match eavi.attribute() {
+            Attribute::LinkTag(_, _) => true,
+            Attribute::RemovedLink(_, _) => true,
+            Attribute::CrudLink => true,
+            _ => false,
+        })
+        .map(|eavi| {
+            let value_entry = context
+                .block_on(get_entry_with_meta_workflow(
+                    &context,
+                    &eavi.value(),
+                    &Timeout::default(),
+                ))?
+                .ok_or(HolochainError::from(
+                    "Entry linked in EAV not found! This should never happen.",
+                ))?;
+            let header = value_entry.headers[0].to_owned();
+
+            match eavi.attribute() {
+                Attribute::LinkTag(_, _) => {
+                    let link_data = unwrap_to!(value_entry.entry_with_meta.entry => Entry::LinkAdd);
+                    Ok(EntryAspect::LinkAdd(link_data.clone(), header))
+                }
+                Attribute::RemovedLink(_, _) => {
+                    let (link_data, removed_link_entries) =
+                        unwrap_to!(value_entry.entry_with_meta.entry => Entry::LinkRemove);
+                    Ok(EntryAspect::LinkRemove(
+                        (link_data.clone(), removed_link_entries.clone()),
+                        header,
+                    ))
+                }
+                Attribute::CrudLink => Ok(EntryAspect::Update(
+                    value_entry.entry_with_meta.entry,
+                    header,
+                )),
+                _ => unreachable!(),
+            }
+        })
+        .partition(Result::is_ok);
+
+    if errors.len() > 0 {
+        Err(errors[0].to_owned().err().unwrap())
+    } else {
+        Ok(aspects.into_iter().map(Result::unwrap).collect())
+    }
 }
 
 /*

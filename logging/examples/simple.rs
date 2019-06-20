989d536fd6c764006d158e1222fee6f7ad5760cb
dymayday <meidhy.demagny@holo.host>
2019-06-20 19:56:17 +0200

    Re-init idiomatic rust logging impl

---
+++ logging/examples/simple.rs
@@ -0,0 +1,23 @@
+#[macro_use]
+extern crate log;
+use logging::{FastLoggerBuilder, tag::TagFilter};
+
+
+fn main() {
+    let _logger = FastLoggerBuilder::new()
+        .set_level_from_str("Trace")
+        .add_tag_filter(TagFilter::new("Abort", false, "Red"))
+        .build()
+        .expect("Fail to instanciate the logging factory.");
+
+    // logger.add_tag_filter(TagFilter::new("^Abort", true, "Red"));
+
+    trace!("Track me if you can.");
+    debug!("What's bugging you today?");
+    info!("Some interesting info here");
+    warn!("You've been warned Sir!");
+    error!("Abort the mission!!");
+
+    // Let's give some time to the working thread to finish logging...
+    std::thread::sleep(std::time::Duration::from_millis(10));
+}

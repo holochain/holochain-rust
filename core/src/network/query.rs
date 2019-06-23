21889844b3e53abd520a4cfbdbface3288290a6b
Eric Harris-Braun <eric@harris-braun.com>
2019-06-23 10:08:23 -0400

    remove useless test

--- core/src/network/query.rs
+++ core/src/network/query.rs
@@ -13,23 +13,3 @@ pub enum NetworkQueryResult {
     Entry(Option<EntryWithMetaAndHeader>),
     Links(Vec<(Address, CrudStatus)>, String, String),
 }
-/*
-#[cfg(test)]
-pub mod tests {
-    use super::*;
-    fn test_roudtrip_network_query_get_entry_encoding() {
-        let query = NetworkQuery::GetEntry;
-        let encoded_query: Vec<u8> = query.into();
-        let decoded_query: NetworkQuery = encoded_query.into();
-        assert_eq!(query,decoded_query);
-    }
-
-    fn test_roudtrip_network_query_get_links_encoding() {
-        let query = NetworkQuery::GetLinks("foo_link_type","foo_link_tag");
-        let encoded_query: Vec<u8> = query.into();
-        let decoded_query: NetworkQuery = encoded_query.into();
-        assert_eq!(query,decoded_query);
-    }
-
-}
-*/

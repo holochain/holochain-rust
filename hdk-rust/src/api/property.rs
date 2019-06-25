a62c62787bf4ca849704be076086e308c3958685
Nicolas Luck <nicolas@lucksus.eu>
2019-06-25 19:19:30 +0200

    rustfmt

--- hdk-rust/src/api/property.rs
+++ hdk-rust/src/api/property.rs
@@ -15,7 +15,5 @@ pub fn property<S: Into<String>>(name: S) -> ZomeApiResult<JsonString> {
     properties
         .get(name.into())
         .map(|value| JsonString::from(value.clone()))
-        .ok_or_else(|| ZomeApiError::from(
-            "field does not exist in DNA properties".to_string(),
-        ))
+        .ok_or_else(|| ZomeApiError::from("field does not exist in DNA properties".to_string()))
 }

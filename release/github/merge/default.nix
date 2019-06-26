0fd1335695e6d23fc018e9d81c7d962965125c50
David Meister <thedavidmeister@gmail.com>
2019-06-26 22:19:08 +1000

    fix merge back git commands for release

--- release/github/merge/default.nix
+++ release/github/merge/default.nix
@@ -11,11 +11,9 @@ let
     git add . && git commit -am 'Release ${release.version.current}';
     git push;
     git pull ${github.upstream} master;
-    git push ${github.upstream} master;
-    git checkout -b ${release.branch}-merge-back
+    git push ${github.upstream} HEAD:master;
     git pull ${github.upstream} develop;
-    git push ${github.upstream} develop;
-    git checkout ${release.branch}
+    git push ${github.upstream} HEAD:develop;
    else
     echo "current branch is not ${release.branch}!";
     exit 1;

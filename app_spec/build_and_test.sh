c797133dbcaf32b151edc2b8518bb8be5b48b7b5
David Meister <thedavidmeister@gmail.com>
2019-06-26 19:36:35 +1000

    defer to holonix shell hook including vagrant prefix

--- app_spec/build_and_test.sh
+++ app_spec/build_and_test.sh
@@ -19,5 +19,8 @@ echo "==========================================================================
 echo "Running test.js in node"
 echo "------------------------------------------------------------------------------------"
 cd test
+# --no-bin-links is required for windows vagrant support
+# more precisely symlinks are not supported without additional work on the host
+# e.g. https://superuser.com/questions/1115329/vagrant-shared-folder-and-symbolic-links-under-windows-10
 npm install --no-bin-links
 npm run test-ci
--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let
- holonix-release-tag = "2019-06-26-windows-npm-debug";
+ holonix-release-tag = "2019-06-26-windows-npm-debug?5";
  holonix-release-sha256 = "0mhrp677p45ihajajanav7cjvfhb2qn4g262vr06wy1zkj20mm0g";
 
  holonix = import (fetchTarball {
-  url = "https://github.com/holochain/holonix/tarball/${holonix-release-tag}?1";
+  url = "https://github.com/holochain/holonix/tarball/${holonix-release-tag}";
   # sha256 = "${holonix-release-sha256}";
  });
  # holonix = import ../holonix;
@@ -12,22 +12,6 @@ with holonix.pkgs;
 {
  core-shell = stdenv.mkDerivation (holonix.shell // {
   name = "core-shell";
-
-  shellHook = ''
-
-   # cargo should install binaries into this repo rather than globally
-
-   # https://github.com/rust-lang/rustup.rs/issues/994
-
-   export CARGO_HOME=~/.cargo
-
-   export CARGO_INSTALL_ROOT=~/.cargo
-
-   export PATH="$CARGO_INSTALL_ROOT/bin:$PATH"
-
-   export HC_TARGET_PREFIX=~/nix-holochain/
-
-  '';
 
   buildInputs = []
    ++ holonix.shell.buildInputs

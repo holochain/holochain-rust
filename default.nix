38b0ee5c8542f3cdbb43bdf1d19a748c993f7dff
David Meister <thedavidmeister@gmail.com>
2019-06-27 18:37:04 +1000

    bump rust nightly

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 let
- holonix-release-tag = "0.0.1";
+ holonix-release-tag = "2019-06-27-rust-nightly";
  holonix-release-sha256 = "1n26n9q4i2k11n1m7disjs7s5s11lq29icqyk8qngqs5gf7kq4pi";
 
  holonix = import (fetchTarball {
   url = "https://github.com/holochain/holonix/tarball/${holonix-release-tag}";
-  sha256 = "${holonix-release-sha256}";
+  # sha256 = "${holonix-release-sha256}";
  });
  # holonix = import ../holonix;
 in

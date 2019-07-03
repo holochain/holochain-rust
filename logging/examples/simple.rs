b74bb3bff91c76b6773660d9ec581654d39d1c19
dymayday <meidhy.demagny@holo.host>
2019-07-03 13:36:23 +0200

    Add some example code

--- logging/examples/simple.rs
+++ logging/examples/simple.rs
@@ -3,18 +3,19 @@ extern crate log;
 use logging::{tag::TagFilter, FastLoggerBuilder};
 
 fn main() {
-    let _logger = FastLoggerBuilder::new()
+    FastLoggerBuilder::new()
         .set_level_from_str("Trace")
         .add_tag_filter(TagFilter::new("Abort", false, "Red"))
+        .add_tag_filter(TagFilter::new("warned", false, "Yellow"))
+        .add_tag_filter(TagFilter::new("twice", true, "Yellow"))
         .build()
         .expect("Fail to instanciate the logging factory.");
-
-    // logger.add_tag_filter(TagFilter::new("^Abort", true, "Red"));
 
     trace!("Track me if you can.");
     debug!("What's bugging you today?");
     info!("Some interesting info here");
     warn!("You've been warned Sir!");
+    warn!("Let's not warned twice about the same stuff.");
     error!("Abort the mission!!");
 
     // Let's give some time to the working thread to finish logging...

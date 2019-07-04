d3f12603b6e660720bd5abe4b789bddc8973d95f
dymayday <meidhy.demagny@holo.host>
2019-07-04 16:24:35 +0200

    Rename tag file to rule.rs

---
+++ logging/src/rule.rs
@@ -0,0 +1,131 @@
+//! Tagging capability: add capability to filter by regex and attribute some color to them.
+
+use regex::Regex;
+use std::default::Default;
+
+#[derive(Clone, Debug)]
+pub struct RuleFilter {
+    pub pattern: Option<String>,
+    exclude: bool,
+    color: Option<String>,
+    re: Regex,
+}
+
+impl RuleFilter {
+    pub fn new(pattern: &str, exclude: bool, color: &str) -> Self {
+        Self {
+            pattern: Some(pattern.to_owned()),
+            exclude,
+            color: Some(color.to_owned()),
+            re: Regex::new(&pattern).expect("Fail to init TagFilter's regex."),
+        }
+    }
+    /// Returns if we should exclude this log entry or not.
+    pub fn exclude(&self) -> bool {
+        self.exclude
+    }
+
+    /// Returns the color of the log entry.
+    pub fn get_color(&self) -> String {
+        match &self.color {
+            Some(color) => color.clone(),
+            None => String::default(),
+        }
+    }
+
+    /// Returns true if we should log this sentence.
+    pub fn should_log(&self, args: &str) -> bool {
+        self.re.is_match(args) && !self.exclude
+    }
+
+    pub fn regex(&self) -> Regex {
+        self.re.clone()
+    }
+    pub fn is_match(&self, args: &str) -> bool {
+        self.re.is_match(args)
+    }
+}
+
+impl Default for RuleFilter {
+    fn default() -> Self {
+        Self {
+            pattern: Some(String::default()),
+            exclude: false,
+            color: Some(String::from("white")),
+            re: Regex::new(&String::default()).expect("Fail to init TagFilter's regex."),
+        }
+    }
+}
+
+pub struct RuleFilterBuilder {
+    pattern: String,
+    exclude: bool,
+    color: String,
+}
+
+impl RuleFilterBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn set_pattern(&mut self, pattern: &str) -> &mut Self {
+        self.pattern = pattern.to_owned();
+        self
+    }
+
+    pub fn set_exclusion(&mut self, exclude: bool) -> &mut Self {
+        self.exclude = exclude;
+        self
+    }
+
+    pub fn set_color(&mut self, color: &str) -> &mut Self {
+        self.color = color.to_owned();
+        self
+    }
+
+    pub fn build(&self) -> RuleFilter {
+        let pattern = match self.pattern.len() {
+            0 => None,
+            _ => Some(self.pattern.clone()),
+        };
+
+        RuleFilter {
+            pattern,
+            exclude: self.exclude,
+            color: Some(self.color.to_owned()),
+            re: Regex::new(&self.pattern).expect("Fail to init TagFilter's regex."),
+        }
+    }
+}
+
+impl Default for RuleFilterBuilder {
+    fn default() -> Self {
+        Self {
+            pattern: String::default(),
+            exclude: false,
+            color: String::from("white"),
+        }
+    }
+}
+
+#[test]
+fn should_log_test() {
+    let rule_filter = RuleFilterBuilder::new()
+        .set_pattern("foo")
+        .set_exclusion(false)
+        .build();
+
+    assert_eq!(rule_filter.should_log("bar"), false);
+    assert_eq!(rule_filter.should_log("xfooy"), true);
+}
+
+#[test]
+fn is_match_test() {
+    let rule_filter = RuleFilterBuilder::new()
+        .set_pattern("foo")
+        .set_exclusion(false)
+        .build();
+
+    assert_eq!(rule_filter.is_match("bar"), false);
+    assert_eq!(rule_filter.is_match("xfooy"), true);
+}

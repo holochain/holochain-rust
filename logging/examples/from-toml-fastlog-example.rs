86e2b89452015704fb3e49573a21894d3dc1c10a
dymayday <meidhy.demagny@holo.host>
2019-07-04 10:05:48 +0200

    Add new example from toml

---
+++ logging/examples/from-toml-fastlog-example.rs
@@ -0,0 +1,41 @@
+#[macro_use]
+extern crate log;
+use logging::FastLoggerBuilder;
+
+fn main() {
+    let toml = r#"
+    [logger]
+    level = "debug"
+
+        [[logger.rules]]
+        pattern = "Abort"
+        color = "red"
+
+        [[logger.rules]]
+        pattern = "warned"
+        color = "yellow"
+
+        [[logger.rules]]
+        pattern = "twice"
+        exclude = true
+        color = "blue"
+
+    "#;
+
+    FastLoggerBuilder::from_toml(toml)
+        .expect("Fail to instantiate the logger from toml.")
+        .build()
+        .expect("Fail to build logger from toml.");
+
+    trace!("Track me if you can.");
+    debug!("What's bugging you today?");
+    info!("Some interesting info here");
+    warn!("You've been warned Sir!");
+    // This next one will not be logged according to our rule defined in the toml
+    warn!("Let's not warn twice about the same stuff.");
+    // And this one will be printed in red
+    error!("Abort the mission!!");
+
+    // Let's give some time to the working thread to finish logging...
+    std::thread::sleep(std::time::Duration::from_millis(10));
+}

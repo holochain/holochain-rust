ad93d697bbc19aeeea2babd5d90de0f973de34e7
dymayday <dymayday@gmail.com>
2019-08-05 16:36:25 +0200

    Add some log stuff to the logging prelude

--- logging/src/prelude.rs
+++ logging/src/prelude.rs
@@ -6,11 +6,15 @@
 //!
 //! ```
 //! use logging::prelude::*;
-//! # logging::FastLoggerBuilder::new().build_test().unwrap();
+//! # logging::FastLoggerBuilder::new()
+//!     .build_test().unwrap();
+//! # // Test if the re-export from the prelude works
+//! # assert_eq!(Level::Debug, Level::Debug);
 //! # info!("Here we go!");
 //! ```
 
 
 // log macro re-export
-pub use log::{trace, debug, info, warn, error};
+pub use log::{log, trace, debug, info, warn, error};
+pub use log::Level;
 pub use crate::{FastLogger, FastLoggerBuilder, rule::{RuleFilter, RuleFilterBuilder}};

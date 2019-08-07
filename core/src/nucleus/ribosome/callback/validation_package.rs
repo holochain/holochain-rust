a5a8f760389e92eee35d559aa655b6791f8ff8ad
Willem Olding <willemolding@gmail.com>
2019-08-06 18:33:46 -0700

    add validation package for ChainHeader entry

--- core/src/nucleus/ribosome/callback/validation_package.rs
+++ core/src/nucleus/ribosome/callback/validation_package.rs
@@ -109,6 +109,7 @@ pub fn get_validation_package_definition(
         EntryType::Deletion => JsonString::from(ValidationPackageDefinition::ChainFull),
         EntryType::CapTokenGrant => JsonString::from(ValidationPackageDefinition::Entry),
         EntryType::AgentId => JsonString::from(ValidationPackageDefinition::Entry),
+        EntryType::ChainHeader => JsonString::from(ValidationPackageDefinition::Entry), 
         _ => Err(HolochainError::NotImplemented(
             "get_validation_package_definition/3".into(),
         ))?,

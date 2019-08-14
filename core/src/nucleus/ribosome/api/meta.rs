55e1b03705de74fd0d67810da584c3c30d5102da
Ashanti Mutinta <Ashantimutinta@gmail.com>
2019-08-14 11:25:46 -0400

    added version test

--- core/src/nucleus/ribosome/api/meta.rs
+++ core/src/nucleus/ribosome/api/meta.rs
@@ -38,3 +38,31 @@ pub fn invoke_meta(runtime: &mut Runtime, args: &RuntimeArgs) -> ZomeApiResult {
 
    runtime.store_result(result)
 }
+
+#[cfg(test)]
+mod test_super {
+    use crate::nucleus::ribosome::{
+        api::{tests::test_zome_api_function, ZomeApiFunction},
+        Defn,
+    };
+    use holochain_core_types::{hdk_version::HDK_VERSION};
+    use holochain_json_api::json::*;
+    /// test that bytes passed to debug end up in the log
+    #[test]
+    fn test_zome_api_meta_functions() {
+        let (call_result, _) = test_zome_api_function(
+            ZomeApiFunction::Meta.as_str(),
+            format!(
+                r#"{{ "method" : "Version" }}"#
+            )
+            .as_bytes()
+            .to_vec(),
+        );
+        let call_result_json = format!(r#"{{"ok":true,"value":"{{\"Version\":\"{}\"}}","error":"null"}}"#,HDK_VERSION.to_string());
+        assert_eq!(
+            JsonString::from_json(&*call_result_json),
+            call_result,
+        );
+    }
+
+}

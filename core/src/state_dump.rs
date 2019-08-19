bb05a7ba637e51384dcf560c752d0fecae3c9f16
Nicolas Luck <nicolas@lucksus.eu>
2019-08-19 15:29:13 +0200

    Fix build for the reversing of the source chain on state dump

--- core/src/state_dump.rs
+++ core/src/state_dump.rs
@@ -38,7 +38,8 @@ impl From<Arc<Context>> for StateDump {
             )
         };
 
-        let source_chain: Vec<ChainHeader> = agent.iter_chain().rev().collect();
+        let source_chain: Vec<ChainHeader> = agent.iter_chain().collect();
+        let source_chain: Vec<ChainHeader> = source_chain.into_iter().rev().collect();
 
         let running_calls: Vec<ZomeFnCall> = nucleus
             .zome_calls

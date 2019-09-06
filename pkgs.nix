d4e9054d73c770c1c5abd4f875015f72e19d2ace
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-06 18:26:05 +0000

    Repin holo-nixpkgs (fix aarch64-unknown-linux-musl cross)

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/220c6890488f1211d9dc5e98e6ba2e5174302187.tar.gz";
-  sha256 = "03vw84rg6g3sd3b4krhkzk652hr6flqm4dhh4mh7v8w1wn0p695l";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/4c2a19f313cd2574f38ae2ea543bc43590619d77.tar.gz";
+  sha256 = "1g0cfw7snmaay07z1nynfj9i7hsncf93snrwyc650zhdxylns3an";
 })

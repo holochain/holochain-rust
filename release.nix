f24e855a407f5199ee6eaa74154ff395fce0c7b6
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-06 19:15:06 +0000

    Remove release-lib.nix, rename platforms, add aarch64-linux-gnu-native

--- release.nix
+++ release.nix
@@ -9,16 +9,12 @@ in
 with nixpkgs {};
 
 let
-  # nativeRelease = import "${pkgs.path}/pkgs/top-level/release-lib.nix" {
-  #   nixpkgsArgs.overlays = [ overlay ];
-  #   supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
-  # };
-
   platforms = {
-    aarch64-linux = pkgsCross.aarch64-multiplatform-musl.pkgsStatic;
-    x86_64-linux = pkgsCross.musl64.pkgsStatic;
-    x86_64-linux-native = nixpkgs { system = "x86_64-linux"; };
-    x86_64-windows = pkgsCross.mingwW64.pkgsStatic;
+    aarch64-linux-musl-cross = pkgsCross.aarch64-multiplatform-musl.pkgsStatic;
+    aarch64-linux-gnu-native = nixpkgs { system = "aarch64-linux"; };
+    x86_64-linux-musl-cross = pkgsCross.musl64.pkgsStatic;
+    x86_64-linux-gnu-native = nixpkgs { system = "x86_64-linux"; };
+    x86_64-windows-gnu-cross = pkgsCross.mingwW64.pkgsStatic;
   };
 in
 
@@ -26,7 +22,4 @@ in
   holochain-cli = lib.mapAttrs (lib.const (lib.getAttr "holochain-cli")) platforms;
 
   holochain-conductor = lib.mapAttrs (lib.const (lib.getAttr "holochain-conductor")) platforms;
-
-  # native = with nativeRelease;
-  #   mapTestOn (packagePlatforms (lib.getAttrs [ "holochain-cli" "holochain-conductor" ] pkgs));
 }

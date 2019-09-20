10f1b021d546f51b1e800fec3e01b7737dbf4f5d
Nicolas Luck <nicolas@lucksus.org>
2019-09-20 17:27:59 -0400

    Revert "Update to Url 2.1.0 and tungstenite 0.9.1 to follow lib3h"
    This reverts commit c3d1757e187b55a963ee435dec8c01b02f2716e9.

--- net/src/ipc/transport_wss/mod.rs
+++ net/src/ipc/transport_wss/mod.rs
@@ -304,7 +304,7 @@ impl<T: Read + Write + std::fmt::Debug> TransportWss<T> {
                         }
                         Err(e.into())
                     }
-                    Err(tungstenite::error::Error::ConnectionClosed) => {
+                    Err(tungstenite::error::Error::ConnectionClosed(_)) => {
                         // close event will be published
                         Ok(())
                     }

8c22eb320d1c4f8be977866bc5d2a1df8c78806a
Nicolas Luck <nicolas@lucksus.eu>
2019-09-23 18:03:01 +0200

    WIP: use sim1h without ghost actor

--- net/src/sim1h_worker.rs
+++ net/src/sim1h_worker.rs
@@ -5,13 +5,14 @@ use crate::connection::{
     NetResult,
 };
 use holochain_json_api::{error::JsonError, json::JsonString};
-use lib3h::{
-    engine::{ghost_engine_wrapper::LegacyLib3h},
-    error::Lib3hError
-};
-use sim1h::ghost_actor::SimGhostActor;
-
-use lib3h_protocol::protocol_client::Lib3hClientProtocol;
+use lib3h_protocol::{protocol_client::Lib3hClientProtocol, Address};
+use log::debug;
+use sim1h::dht::bbdht::dynamodb::client::{Client, client_from_endpoint};
+use url::Url;
+use sim1h::workflow::from_client::join_space::join_space;
+use lib3h_protocol::protocol_server::Lib3hServerProtocol;
+use log::warn;
+use lib3h_protocol::data_types::{GenericResultData, Opaque};
 
 #[derive(Deserialize, Serialize, Clone, Debug, DefaultJson, PartialEq)]
 pub struct Sim1hConfig{
@@ -22,25 +23,167 @@ pub struct Sim1hConfig{
 #[allow(non_snake_case)]
 pub struct Sim1hWorker {
     handler: NetHandler,
-    net_engine : LegacyLib3h<SimGhostActor, Lib3hError>
+    dynamo_db_client: Client,
+    inbox: Vec<Lib3hClientProtocol>,
 }
 
 
 impl Sim1hWorker {
     pub fn advertise(self) -> url::Url {
-        self.net_engine.advertise()
+        Url::parse("ws://example.com").unwrap()
     }
-
 }
 
 impl Sim1hWorker {
     /// Create a new websocket worker connected to the lib3h NetworkEngine
     pub fn new(handler: NetHandler, config: Sim1hConfig) -> NetResult<Self> {
-    	let ghost_engine = SimGhostActor::new(&config.dynamo_url);
-    	Ok(Self {
-    		handler,
-    		net_engine: LegacyLib3h::new("core", ghost_engine),
-    	})
+        let dynamo_db_client = client_from_endpoint(config.dynamo_url);
+    	Ok(Self { handler, dynamo_db_client, inbox: Vec::new() })
+    }
+
+    fn handle_client_message(&mut self, data: Lib3hClientProtocol) -> NetResult<Lib3hServerProtocol> {
+        match data {
+            // Success response to a request (any Command with an `request_id` field.)
+            Lib3hClientProtocol::SuccessResult(generic_result_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(generic_result_data))
+            },
+            // Failure response to a request (any Command with an `request_id` field.)
+            // Can also be a response to a mal-formed request.
+            Lib3hClientProtocol::FailureResult(generic_result_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(generic_result_data))
+            },
+            // Connect to the specified multiaddr
+            Lib3hClientProtocol::Connect(connect_data) => {
+                //let log_context = "Lib3hToClient::Connected";
+                //connected(&log_context, &self.dynamo_db_client, &connect_data);
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: connect_data.request_id,
+                    space_address: Address::new(),
+                    to_agent_id: Address::new(),
+                    result_info: Opaque::new(),
+                }))
+            }
+
+            // -- Space -- //
+            // Order the p2p module to be part of the network of the specified space.
+            Lib3hClientProtocol::JoinSpace(space_data) => {
+                //let ClientToLib3h::JoinSpace(space_data)= ClientToLib3h::from(data);
+                let log_context = "ClientToLib3h::JoinSpace";
+                Ok(Lib3hServerProtocol::from(join_space(&log_context, &self.dynamo_db_client, &space_data)?))
+            }
+            // Order the p2p module to leave the network of the specified space.
+            Lib3hClientProtocol::LeaveSpace(space_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: space_data.request_id,
+                    space_address: space_data.space_address,
+                    to_agent_id: space_data.agent_id,
+                    result_info: Opaque::new(),
+                }))
+            }
+
+            // -- Direct Messaging -- //
+            // Send a message directly to another agent on the network
+            Lib3hClientProtocol::SendDirectMessage(dm_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: dm_data.request_id,
+                    space_address: dm_data.space_address,
+                    to_agent_id: dm_data.to_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+            // Our response to a direct message from another agent.
+            Lib3hClientProtocol::HandleSendDirectMessageResult(dm_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: dm_data.request_id,
+                    space_address: dm_data.space_address,
+                    to_agent_id: dm_data.to_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+            // -- Entry -- //
+            // Request an Entry from the dht network
+            Lib3hClientProtocol::FetchEntry(fetch_entry_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: fetch_entry_data.request_id,
+                    space_address: fetch_entry_data.space_address,
+                    to_agent_id: fetch_entry_data.provider_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+            // Successful data response for a `HandleFetchEntryData` request
+            Lib3hClientProtocol::HandleFetchEntryResult(fetch_entry_result_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: fetch_entry_result_data.request_id,
+                    space_address: fetch_entry_result_data.space_address,
+                    to_agent_id: fetch_entry_result_data.provider_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+            // Publish data to the dht.
+            Lib3hClientProtocol::PublishEntry(provided_entry_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: "".into(),
+                    space_address: provided_entry_data.space_address,
+                    to_agent_id: provided_entry_data.provider_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+            // Tell network module Core is holding this entry
+            Lib3hClientProtocol::HoldEntry(provided_entry_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: "".into(),
+                    space_address: provided_entry_data.space_address,
+                    to_agent_id: provided_entry_data.provider_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+            // Request some info / data from a Entry
+            Lib3hClientProtocol::QueryEntry(query_entry_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: query_entry_data.request_id,
+                    space_address: query_entry_data.space_address,
+                    to_agent_id: query_entry_data.requester_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+            // Response to a `HandleQueryEntry` request
+            Lib3hClientProtocol::HandleQueryEntryResult(query_entry_result_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: query_entry_result_data.request_id,
+                    space_address: query_entry_result_data.space_address,
+                    to_agent_id: query_entry_result_data.requester_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+
+            // -- Entry lists -- //
+            Lib3hClientProtocol::HandleGetAuthoringEntryListResult(entry_list_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: entry_list_data.request_id,
+                    space_address: entry_list_data.space_address,
+                    to_agent_id: entry_list_data.provider_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+            Lib3hClientProtocol::HandleGetGossipingEntryListResult(entry_list_data) => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: entry_list_data.request_id,
+                    space_address: entry_list_data.space_address,
+                    to_agent_id: entry_list_data.provider_agent_id,
+                    result_info: Opaque::new(),
+                }))
+            },
+
+            // -- N3h specific functinonality -- //
+            Lib3hClientProtocol::Shutdown => {
+                Ok(Lib3hServerProtocol::FailureResult(GenericResultData{
+                    request_id: "".into(),
+                    space_address: Address::new(),
+                    to_agent_id: Address::new(),
+                    result_info: Opaque::new(),
+                }))
+            },
+        }
     }
 }
 
@@ -50,26 +193,35 @@ impl NetWorker for Sim1hWorker {
     /// We got a message from core
     /// -> forward it to the NetworkEngine
     fn receive(&mut self, data: Lib3hClientProtocol) -> NetResult<()> {
-        self.net_engine.post(data.clone())?;
-        // Done
+        debug!(">>NET>> {:?}", data);
+        self.inbox.push(data);
         Ok(())
     }
 
     /// Check for messages from our NetworkEngine
     fn tick(&mut self) -> NetResult<bool> {
-        // Tick the NetworkEngine and check for incoming protocol messages.
-        let (did_something, output) = self.net_engine.process()?;
-        if did_something {
-            for msg in output {
-                self.handler.handle(Ok(msg))?;
+        println!("sim1h tick");
+        let mut did_something = false;
+        let messages = self.inbox.drain(..).collect::<Vec<_>>();
+        for data in messages {
+            match self.handle_client_message(data) {
+                Ok(response) => {
+                    if let Err(error) = self.handler.handle(Ok(response)) {
+                        warn!("Error returned from network handler in Sim1h: {:?}", error);
+                    }
+                },
+                Err(error) => {
+                    warn!("Error handling client message in Sim1hWorker: {:?}", error);
+                },
             }
+            did_something = true;
         }
         Ok(did_something)
     }
 
     /// Set the advertise as worker's endpoint
     fn p2p_endpoint(&self) -> Option<url::Url> {
-        Some(self.net_engine.advertise())
+        None
     }
 
     /// Set the advertise as worker's endpoint
@@ -87,7 +239,7 @@ mod tests {
 		protocol_server::Lib3hServerProtocol,
 		protocol_client::Lib3hClientProtocol,
 	};
-	use url::Url;
+	//use url::Url;
 
 	fn test_worker() -> (Sim1hWorker, crossbeam_channel::Receiver<NetResult<Lib3hServerProtocol>>) {
 		let (s,r) = crossbeam_channel::unbounded();
@@ -98,7 +250,7 @@ mod tests {
             Sim1hWorker::new(
                 handler,
                 Sim1hConfig{
-                    dynamo_url:"http://derp:8000".into()
+                    dynamo_url:"http://localhost:8000".into()
                 }
             ).unwrap(),
             r,
@@ -109,12 +261,21 @@ mod tests {
     fn call_to_boostrap_fails() {
     	let (mut worker, r) = test_worker();
 
+        /*
     	let connect_data = ConnectData {
     		request_id: String::from("request-id-0"),
     		peer_uri: Url::parse("http://bs").unwrap(),
     		network_id: String::from("network-id"),
     	};
     	let message = Lib3hClientProtocol::Connect(connect_data);
+    	*/
+
+        let space_data = SpaceData {
+            request_id: String::from("request-id-0"),
+            space_address: Address::new(),
+            agent_id: Address::new(),
+        };
+        let message = Lib3hClientProtocol::JoinSpace(space_data);
 
     	// send the bootstrap message
     	worker.receive(message).expect("could not send message");

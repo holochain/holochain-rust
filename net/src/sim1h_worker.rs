edd5f22641568af7ec2ea931c7eee81c098f3011
Nicolas Luck <nicolas@lucksus.org>
2019-09-23 14:37:07 +0200

    WIP: use sim1h without ghost actor

--- net/src/sim1h_worker.rs
+++ net/src/sim1h_worker.rs
@@ -5,13 +5,15 @@ use crate::connection::{
     NetResult,
 };
 use holochain_json_api::{error::JsonError, json::JsonString};
-use lib3h::{
-    engine::{ghost_engine_wrapper::LegacyLib3h},
-    error::Lib3hError
+use lib3h_protocol::{
+    protocol_client::Lib3hClientProtocol,
 };
-use sim1h::ghost_actor::SimGhostActor;
-use lib3h_protocol::protocol_client::Lib3hClientProtocol;
 use log::debug;
+use sim1h::dht::bbdht::dynamodb::client::{Client, client_from_endpoint};
+use sim1h::workflow::to_client::connected::connected;
+use url::Url;
+use sim1h::workflow::from_client::join_space::join_space;
+
 #[derive(Deserialize, Serialize, Clone, Debug, DefaultJson, PartialEq)]
 pub struct Sim1hConfig{
     pub dynamo_url: String,
@@ -21,25 +23,98 @@ pub struct Sim1hConfig{
 #[allow(non_snake_case)]
 pub struct Sim1hWorker {
     handler: NetHandler,
-    net_engine : LegacyLib3h<SimGhostActor, Lib3hError>
+    dynamo_db_client: Client,
+    inbox: Vec<Lib3hClientProtocol>,
 }
 
 
 impl Sim1hWorker {
     pub fn advertise(self) -> url::Url {
-        self.net_engine.advertise()
+        Url::parse("ws://example.com").unwrap()
     }
-
 }
 
 impl Sim1hWorker {
     /// Create a new websocket worker connected to the lib3h NetworkEngine
     pub fn new(handler: NetHandler, config: Sim1hConfig) -> NetResult<Self> {
-    	let ghost_engine = SimGhostActor::new(&config.dynamo_url);
-    	Ok(Self {
-    		handler,
-    		net_engine: LegacyLib3h::new("core", ghost_engine),
-    	})
+        let dynamo_db_client = client_from_endpoint(config.dynamo_url);
+    	Ok(Self { handler, dynamo_db_client, inbox: Vec::new() })
+    }
+
+    fn handle_client_message(&mut self, data: Lib3hClientProtocol) -> NetResult<Option<Lib3hClientProtocol>> {
+        match data {
+            // Success response to a request (any Command with an `request_id` field.)
+            Lib3hClientProtocol::SuccessResult(_generic_result_data) => {},
+            // Failure response to a request (any Command with an `request_id` field.)
+            // Can also be a response to a mal-formed request.
+            Lib3hClientProtocol::FailureResult(_generic_result_data) => {},
+            // Connect to the specified multiaddr
+            Lib3hClientProtocol::Connect(connect_data) => {
+                let log_context = "Lib3hToClient::Connected";
+                connected(&log_context, &self.dynamo_db_client, &connect_data);
+                Ok(None)
+            }
+
+            // -- Space -- //
+            // Order the p2p module to be part of the network of the specified space.
+            Lib3hClientProtocol::JoinSpace(space_data) => {
+                //let ClientToLib3h::JoinSpace(space_data)= ClientToLib3h::from(data);
+                let log_context = "ClientToLib3h::JoinSpace";
+                Ok(Lib3hClientProtocol::from(join_space(&log_context, &self.dbclient, &space_data)?))
+            }
+            // Order the p2p module to leave the network of the specified space.
+            Lib3hClientProtocol::LeaveSpace(space_data) => {
+                Ok(None)
+            }
+
+            // -- Direct Messaging -- //
+            // Send a message directly to another agent on the network
+            Lib3hClientProtocol::SendDirectMessage(DirectMessageData) => {
+                Ok(None)
+            },
+            // Our response to a direct message from another agent.
+            Lib3hClientProtocol::HandleSendDirectMessageResult(DirectMessageData) => {
+                Ok(None)
+            },
+            // -- Entry -- //
+            // Request an Entry from the dht network
+            Lib3hClientProtocol::FetchEntry(FetchEntryData) => {
+                Ok(None)
+            },
+            // Successful data response for a `HandleFetchEntryData` request
+            Lib3hClientProtocol::HandleFetchEntryResult(FetchEntryResultData) => {
+                Ok(None)
+            },
+            // Publish data to the dht.
+            Lib3hClientProtocol::PublishEntry(ProvidedEntryData) => {
+                Ok(None)
+            },
+            // Tell network module Core is holding this entry
+            Lib3hClientProtocol::HoldEntry(ProvidedEntryData) => {
+                Ok(None)
+            },
+            // Request some info / data from a Entry
+            Lib3hClientProtocol::QueryEntry(QueryEntryData) => {
+                Ok(None)
+            },
+            // Response to a `HandleQueryEntry` request
+            Lib3hClientProtocol::HandleQueryEntryResult(QueryEntryResultData) => {
+                Ok(None)
+            },
+
+            // -- Entry lists -- //
+            Lib3hClientProtocol::HandleGetAuthoringEntryListResult(EntryListData) => {
+                Ok(None)
+            },
+            Lib3hClientProtocol::HandleGetGossipingEntryListResult(EntryListData) => {
+                Ok(None)
+            },
+
+            // -- N3h specific functinonality -- //
+            Lib3hClientProtocol::Shutdown => {
+                Ok(None)
+            },
+        }
     }
 }
 
@@ -50,21 +125,17 @@ impl NetWorker for Sim1hWorker {
     /// -> forward it to the NetworkEngine
     fn receive(&mut self, data: Lib3hClientProtocol) -> NetResult<()> {
         debug!(">>NET>> {:?}", data);
-        self.net_engine.post(data.clone())?;
-        // Done
+        self.inbox.push(data);
         Ok(())
     }
 
     /// Check for messages from our NetworkEngine
     fn tick(&mut self) -> NetResult<bool> {
         println!("sim1h tick");
-        // Tick the NetworkEngine and check for incoming protocol messages.
-        let (did_something, output) = self.net_engine.process()?;
-        if did_something {
-            for msg in output {
-                debug!("<<NET<< {:?}", msg);
-                self.handler.handle(Ok(msg))?;
-            }
+        let mut did_something = false;
+        for data in self.inbox.drain(..) {
+            self.handle_client_message(data);
+            did_something = true;
         }
         Ok(did_something)
     }

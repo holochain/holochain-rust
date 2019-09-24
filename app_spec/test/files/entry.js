0c0553006d7f577368a55f4fe4da901c57518b76
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2019-09-24 12:49:29 -0400

    Update entry.js

--- app_spec/test/files/entry.js
+++ app_spec/test/files/entry.js
@@ -46,11 +46,10 @@ module.exports = scenario => {
       
         t.ok(createResult.Ok)
       
-        const updateParamsV2 = { post_address: createResult.Ok, new_content: "Hello Holo world 3211" }
+        const updateParamsV2 = { post_address: createResult.Ok, new_content: "Hello Holo world 321" }
         const UpdateResultV2 = await bob.app.callSync("blog", "update_post", updateParamsV2)
-        t.deepEqual(JSON.parse(UpdateResultV2.Err.Internal).kind.ValidationFailed, "Trying to modify with same data");
-      
-      
+        t.deepEqual(UpdateResultV2.Err, { Internal: 'Workflow failed' });
+    
       })
       
       scenario('update_post', async (s, t, { alice, bob }) => {

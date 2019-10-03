e82e16aacf177125743e33521ee47e71e58ff195
Michael Dougherty <maackle.d@gmail.com>
2019-10-02 23:54:49 -0700

    More merge fixes

--- core/src/context.rs
+++ core/src/context.rs
@@ -22,7 +22,7 @@ use holochain_core_types::{
         Entry,
     },
     error::{HcResult, HolochainError},
-    sync::{HcMutex as Mutex, HcRwLock as RwLock, HcRwLockReadGuard as RwLockReadGuard},
+    sync::{HcMutex as Mutex, HcRwLock as RwLock, HcMutexGuard as MutexGuard, HcRwLockReadGuard as RwLockReadGuard},
 };
 
 use holochain_net::{p2p_config::P2pConfig, p2p_network::P2pNetwork};
@@ -53,7 +53,7 @@ impl P2pNetworkWrapper {
     }
 }
 
-pub struct P2pNetworkMutexGuardWrapper<'a>(std::sync::MutexGuard<'a, Option<P2pNetwork>>);
+pub struct P2pNetworkMutexGuardWrapper<'a>(MutexGuard<'a, Option<P2pNetwork>>);
 
 impl<'a> P2pNetworkMutexGuardWrapper<'a> {
     pub fn as_ref(&self) -> Result<&P2pNetwork, HolochainError> {

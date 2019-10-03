ee563b279c6353d96c23f9a1a2ef52e1dee41b3c
Michael Dougherty <maackle.d@gmail.com>
2019-10-03 16:29:20 -0700

    Update js test scaffold to something a little better

--- cli/src/cli/js-tests-scaffold/index.js
+++ cli/src/cli/js-tests-scaffold/index.js
@@ -1,17 +1,11 @@
-
-
-///////////////////////////////////
-///
-///
-///            TODO
-///
-///
-////////////////////////////////////
+/// NB: The try-o-rama config patterns are still not quite stabilized.
+/// See the try-o-rama README [https://github.com/holochain/try-o-rama]
+/// for a potentially more accurate example
 
 const path = require('path')
 const tape = require('tape')
 
-const { Orchestrator, Config, tapeExecutor } = require('@holochain/try-o-rama')
+const { Orchestrator, Config, tapeExecutor, singleConductor, combine  } = require('@holochain/try-o-rama')
 
 process.on('unhandledRejection', error => {
   // Will print "unhandledRejection err is not defined"
@@ -21,25 +15,53 @@ process.on('unhandledRejection', error => {
 const dnaPath = path.join(__dirname, "../dist/<<DNA_NAME>>.dna.json")
 const dna = Diorama.dna(dnaPath, '<<DNA_NAME>>')
 
-const diorama = new Diorama({
-  instances: {
-    alice: dna,
-    bob: dna,
+const orchestrator = new Orchestrator({
+  middleware: combine(
+    // squash all instances from all conductors down into a single conductor,
+    // for in-memory testing purposes.
+    // Remove this middleware for other "real" network types which can actually
+    // send messages across conductors
+    singleConductor,
+
+    // use the tape harness to run the tests, injects the tape API into each scenario
+    // as the second argument
+    tapeExecutor(require('tape'))
+  ),
+
+  globalConfig: {
+    logger: true,
+    network: 'memory',  // must use singleConductor middleware if using in-memory network
   },
-  bridges: [],
-  debugLog: false,
-  executor: tapeExecutor(require('tape')),
-  middleware: backwardCompatibilityMiddleware,
+
+  // the following are optional:
+
+  waiter: {
+    softTimeout: 5000,
+    hardTimeout: 10000,
+  },
 })
 
-diorama.registerScenario("description of example test", async (s, t, { alice }) => {
+const conductorConfig = {
+  instances: {
+    myInstanceName: Config.dna('/path/to/my/dna/change/me.dna.json', 'scaffold-test')
+  }
+}
+
+orchestrator.registerScenario("description of example test", async (s, t) => {
+
+  const {alice, bob} = await s.players({alice: conductorConfig, bob: conductorConfig})
+
   // Make a call to a Zome function
   // indicating the function, and passing it an input
-  const addr = await alice.call("my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
-  const result = await alice.call("my_zome", "get_my_entry", {"address": addr.Ok})
+  const addr = await alice.call("myInstanceName", "my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
+
+  // Wait for all network activity to
+  await s.consistency()
+
+  const result = await alice.call("myInstanceName", "my_zome", "get_my_entry", {"address": addr.Ok})
 
   // check for equality of the actual and expected results
   t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
 })
 
-diorama.run()
+orchestrator.run()

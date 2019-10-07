eb14ec9a8cb491793d3b6489a898cbcc9d01017b
Willem Olding <willemolding@gmail.com>
2019-10-07 11:47:29 +1100

    fmt

--- conductor/src/main.rs
+++ conductor/src/main.rs
@@ -25,7 +25,7 @@ use holochain_conductor_api::{
     conductor::{mount_conductor_from_config, Conductor, CONDUCTOR},
     config::{self, load_configuration, Configuration},
 };
-use holochain_core_types::{error::HolochainError};
+use holochain_core_types::error::HolochainError;
 use signal_hook::{iterator::Signals, SIGINT, SIGTERM};
 use std::{
     fs::File,

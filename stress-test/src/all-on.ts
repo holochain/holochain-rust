cee4d438874440e7bd9dc624d7364d68b7947bfd
Michael Dougherty <maackle.d@gmail.com>
2019-10-09 22:34:35 -0700

    N=10 M=2

--- stress-test/src/all-on.ts
+++ stress-test/src/all-on.ts
@@ -21,7 +21,7 @@ const trace = R.tap(x => console.log('{T}', x))
 module.exports = scenario => {
 
   const N = 10
-  const M = 1
+  const M = 2
   const batch = batchSimple(N, M)
 
   scenario('one at a time', async (s, t) => {

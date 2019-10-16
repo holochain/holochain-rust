370a3c3caf05301acefd21b1c23845927bbba251
Ashanti Mutinta <Ashantimutinta@gmail.com>
2019-10-16 13:16:08 -0400

    changes to wasm_compile

--- crates/holochain_wasm/test/default.nix
+++ crates/holochain_wasm/test/default.nix
@@ -5,7 +5,7 @@ let
   script = pkgs.writeShellScriptBin name
   ''
   hc-conductor-wasm-compile
-  ( cd ./holochain_wasm/npm_package && npm install && npm test );
+  ( cd crates/holochain_wasm/npm_package && npm install && npm test );
   '';
 in
 {

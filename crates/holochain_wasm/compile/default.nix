e4c8b606f5b00a6d25efee56d5df405f1fe8fb51
David Meister <thedavidmeister@gmail.com>
2019-10-17 13:48:18 +1100

    fix hc-conductor-wasm-compile

--- crates/holochain_wasm/compile/default.nix
+++ crates/holochain_wasm/compile/default.nix
@@ -5,8 +5,8 @@ let
   script = pkgs.writeShellScriptBin name
   ''
   hc-conductor-wasm-install
-  cd crates/holochain_wasm && cargo build --release -p holochain_conductor_wasm --target wasm32-unknown-unknown
-  wasm-bindgen target/wasm32-unknown-unknown/release/holochain_conductor_wasm.wasm --out-dir npm_package/gen --nodejs
+  ( cd crates/holochain_wasm && cargo build --release -p holochain_conductor_wasm --target wasm32-unknown-unknown )
+  wasm-bindgen --out-dir crates/holochain_wasm/npm_package/gen --nodejs target/wasm32-unknown-unknown/release/holochain_conductor_wasm.wasm
   '';
 in
 {

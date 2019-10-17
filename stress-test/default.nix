15fe4502d07693c8726e4f705e9b2093ef3fe18a
Nicolas Luck <nicolas@lucksus.eu>
2019-10-17 19:39:08 +0200

    Don't continue stress test with old built if compilation fails

--- stress-test/default.nix
+++ stress-test/default.nix
@@ -2,15 +2,15 @@
 let
   script-stress-sim1h = pkgs.writeShellScriptBin "hc-stress-test-sim1h"
   ''
-  hc-cli-install
-  hc-conductor-install
+  hc-cli-install &&
+  hc-conductor-install &&
    ( cd stress-test && npm install && APP_SPEC_NETWORK_TYPE=sim1h AWS_ACCESS_KEY_ID=bla AWS_SECRET_ACCESS_KEY=blup npm test -- "$@" )
   '';
 
   script-stress-sim2h = pkgs.writeShellScriptBin "hc-stress-test-sim2h"
   ''
-  hc-cli-install
-  hc-conductor-install
+  hc-cli-install &&
+  hc-conductor-install &&
    ( cd stress-test && npm install && APP_SPEC_NETWORK_TYPE=sim2h npm test -- "$@" )
   '';
 in

638ec9d211ba10a910901c55eb6c24e47f869f26
Willem Olding <willemolding@gmail.com>
2019-10-18 11:02:51 +1100

    make clippy happy

--- crates/core_types/src/hdk_version.rs
+++ crates/core_types/src/hdk_version.rs
@@ -6,10 +6,9 @@ lazy_static! {
             "HDK_VERSION",
             "failed to obtain hdk version from build environment. Check build.rs"
         );
-        HDKVersion::new(version).expect(&format!(
-            "Failed to create HDK Version '{}'. Check Build.rs",
-            version
-        ))
+        HDKVersion::new(version).unwrap_or_else(|_| {
+            panic!("Failed to create HDK Version '{}'. Check Build.rs", version)
+        })
     };
 }
 

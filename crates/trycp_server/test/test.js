e6d06ec5f97aa0139d56393b8d405a7ef4e1f9dc
Eric Harris-Braun <eric@harris-braun.com>
2019-10-22 17:17:49 -0400

    make the mrrm a little more mocklike

--- crates/trycp_server/test/test.js
+++ crates/trycp_server/test/test.js
@@ -1,27 +1,28 @@
 var WebSocket = require('rpc-websockets').Client
 
-const { spawn } = require('child_process');
-const port = "3000"
-const trycp = spawn('trycp_server', ['-p', port]);
-trycp.stdout.on('data', (data) => {
-    var regex = new RegExp("waiting for connections on port "+port);
-    if (regex.test(data)){
-        console.log("boink")
-        doTest()
-    }
-    console.log(`stdout: ${data}`);
-});
-trycp.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-});
 
+//doTest("ws://localhost:9000")
+magic_remote_machine_manager("3000")
+function magic_remote_machine_manager(port) {
+    const { spawn } = require('child_process');
+    const trycp = spawn('trycp_server', ['-p', port]);
+    trycp.stdout.on('data', (data) => {
+        var regex = new RegExp("waiting for connections on port "+port);
+        if (regex.test(data)){
+            doTest("ws://localhost:"+port)
+        }
+        console.log(`stdout: ${data}`);
+    });
+    trycp.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+}
 
 // instantiate Client and connect to an RPC server
-function doTest() {
-    const url = "ws://localhost:"+port
+async function  doTest(url) {
     console.log("starting up at ",url)
     var ws = new WebSocket(url)
-    ws.on('open', function() {
+    ws.on('open', async function() {
         console.log("making ping call")
         // call an RPC method with parameters
 
@@ -29,25 +30,21 @@ function doTest() {
             console.log(result)
         })
 
-        const config_toml = "[config]"
+        const config_toml = "[config]"  // THIS IS A BROKEN CONFIG
         const config = Buffer.from(config_toml).toString('base64')
         console.log("making player call with config", config)
-        ws.call('player', {"id": "my-player", "config": config}).then(function(result) {
-            console.log(result)
-        })
+        let result = await ws.call('player', {"id": "my-player", "config": config})
+        console.log(result)
 
         console.log("making spawn call")
-        ws.call('spawn', {"id": "my-player"}).then(function(result) {
-            console.log(result)
-        })
+        result = await ws.call('spawn', {"id": "my-player"})
+        console.log(result)
 
         console.log("making kill call")
-        ws.call('kill', {"id": "my-player"}).then(function(result) {
-            console.log(result)
-            ws.close()
-        })
+        result = await ws.call('kill', {"id": "my-player"})
+        console.log(result)
 
         // close a websocket connection
-        //ws.close()
+        ws.close()
     })
 }

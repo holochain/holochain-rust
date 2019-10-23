1fc1ed332917911f00b6b6648b020f60f3b90ad2
Nicolas Luck <nicolas@lucksus.eu>
2019-10-23 21:36:40 +0200

    Fine grained log rules for stress test

--- stress-test/src/index.ts
+++ stress-test/src/index.ts
@@ -35,7 +35,46 @@ const orchestrator = new Orchestrator({
   middleware,
   globalConfig: {
     network,
-    logger: true
+    logger: {
+      type: 'debug',
+      rules: {
+        rules: [
+          {
+            exclude: true,
+            pattern: '.*parity.*'
+          },
+          {
+            exclude: true,
+            pattern: '.*mio.*'
+          },
+          {
+            exclude: true,
+            pattern: '.*tokio.*'
+          },
+          {
+            exclude: true,
+            pattern: '.*hyper.*'
+          },
+          {
+            exclude: true,
+            pattern: '.*rusoto_core.*'
+          },
+          {
+            exclude: true,
+            pattern: '.*want.*'
+          },
+          {
+            exclude: true,
+            pattern: '.*::sync.*'
+          },
+          {
+            exclude: true,
+            pattern: '.*rpc.*'
+          }
+        ]
+      },
+      state_dump: true
+    }
   }
 })
 

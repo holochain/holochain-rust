e67fc8ae76de0fe95a0e2e777946dceba548ce9f
David Meister <thedavidmeister@gmail.com>
2019-10-23 19:59:42 +1100

    WIP on working hc-release-hook-publish

--- release/publish/default.nix
+++ release/publish/default.nix
@@ -7,20 +7,10 @@ set -euox pipefail
 echo "packaging for crates.io"
 # order is important here due to dependencies
 for crate in \
- cli \
- common \
- conductor_api \
- conductor_lib \
  core_types \
- dpki \
  hdk \
  hdk_v2 \
- holochain \
- holochain_wasm \
- net \
- wasm_utils \
- sim2h \
- sim2h_server
+ wasm_utils
 do
  cargo publish --manifest-path "crates/$crate/Cargo.toml"
  sleep 10

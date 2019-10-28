de05c2962259fe50b769a889fda3086a075f89e8
Carmelo Piccione <carmelo.piccione@holo.host>
2019-10-28 13:25:07 -0400

    Metrics crate for publishing data for perf testing

---
+++ crates/metrics/src/metrics.rs
@@ -0,0 +1,51 @@
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Metric {
+   name: String,
+   value: f64
+}
+
+impl Metric {
+
+    pub fn new(name:&str, value:f64) -> Self {
+        Self {name:name.to_string(), value}
+    }
+
+}
+
+pub trait MetricPublisher {
+
+    fn publish(&mut self, metric: &Metric);
+}
+
+
+pub struct StdoutMetricPublisher;
+
+
+impl StdoutMetricPublisher {
+    pub fn new() -> Self {
+        StdoutMetricPublisher
+    }
+}
+impl MetricPublisher for StdoutMetricPublisher {
+
+    fn publish(&mut self, metric: &Metric) {
+       println!("{} {}", metric.name, metric.value); 
+    }
+
+}
+
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn can_publish_to_stdout() {
+       let publisher = StdoutMetricPublisher;
+       let metric = Metric("latency", 100);
+
+       publisher.publish(metric);
+    }
+
+
+}

3d465cad367c214e81da8259ac96fbb16a846cfe
Carmelo Piccione <carmelo.piccione@holo.host>
2019-10-28 13:25:34 -0400

    cargo fmt

--- crates/metrics/src/metrics.rs
+++ crates/metrics/src/metrics.rs
@@ -1,26 +1,23 @@
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct Metric {
-   name: String,
-   value: f64
+    name: String,
+    value: f64,
 }
 
 impl Metric {
-
-    pub fn new(name:&str, value:f64) -> Self {
-        Self {name:name.to_string(), value}
+    pub fn new(name: &str, value: f64) -> Self {
+        Self {
+            name: name.to_string(),
+            value,
+        }
     }
-
 }
 
 pub trait MetricPublisher {
-
     fn publish(&mut self, metric: &Metric);
 }
 
-
 pub struct StdoutMetricPublisher;
-
 
 impl StdoutMetricPublisher {
     pub fn new() -> Self {
@@ -28,24 +25,20 @@ impl StdoutMetricPublisher {
     }
 }
 impl MetricPublisher for StdoutMetricPublisher {
-
     fn publish(&mut self, metric: &Metric) {
-       println!("{} {}", metric.name, metric.value); 
+        println!("{} {}", metric.name, metric.value);
     }
-
 }
-
 
 #[cfg(test)]
 mod test {
 
     #[test]
     fn can_publish_to_stdout() {
-       let publisher = StdoutMetricPublisher;
-       let metric = Metric("latency", 100);
+        let publisher = StdoutMetricPublisher;
+        let metric = Metric("latency", 100);
 
-       publisher.publish(metric);
+        publisher.publish(metric);
     }
-
 
 }

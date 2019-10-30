be13556be4428f1784b45791fbf023202d2849b4
David Meister <thedavidmeister@gmail.com>
2019-10-30 12:01:37 +1100

    debug

--- rust/wasm/compile/default.nix
+++ rust/wasm/compile/default.nix
@@ -14,7 +14,7 @@ let
  ''
  set -euxo pipefail
  export WASM_PATH=${path}/
- cargo build --release --target wasm32-unknown-unknown --manifest-path "$WASM_PATH"Cargo.toml --target-dir "$HC_TARGET_PREFI"/"$WASM_PATH"target;
+ cargo build --release --target wasm32-unknown-unknown --manifest-path "$WASM_PATH"Cargo.toml --target-dir "$HC_TARGET_PREFIX"/"$WASM_PATH"target;
  '';
 
  script = pkgs.writeShellScriptBin name

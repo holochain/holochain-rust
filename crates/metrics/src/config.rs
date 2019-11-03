5533e47d58a5d500a1a9d494034e83fa5c7420c8
Carmelo Piccione <carmelo.piccione@holo.host>
2019-11-03 14:52:04 -0500

    RwLock merge fix

--- crates/metrics/src/config.rs
+++ crates/metrics/src/config.rs
@@ -1,5 +1,5 @@
 use crate::{cloudwatch::CloudWatchMetricPublisher, LoggerMetricPublisher, MetricPublisher};
-use holochain_core_types::sync::HcRwLock;
+use holochain_locksmith::RwLock;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
@@ -16,12 +16,12 @@ impl Default for MetricPublisherConfig {
 }
 
 impl MetricPublisherConfig {
-    pub fn create_metric_publisher(&self) -> Arc<HcRwLock<dyn MetricPublisher>> {
-        let publisher: Arc<HcRwLock<dyn MetricPublisher>> = match self {
-            Self::Logger => Arc::new(HcRwLock::new(LoggerMetricPublisher::new())),
+    pub fn create_metric_publisher(&self) -> Arc<RwLock<dyn MetricPublisher>> {
+        let publisher: Arc<RwLock<dyn MetricPublisher>> = match self {
+            Self::Logger => Arc::new(RwLock::new(LoggerMetricPublisher::new())),
             Self::CloudWatch(maybe_region) => {
                 let region = maybe_region.clone().unwrap_or_default();
-                Arc::new(HcRwLock::new(CloudWatchMetricPublisher::new(&region)))
+                Arc::new(RwLock::new(CloudWatchMetricPublisher::new(&region)))
             }
         };
         publisher

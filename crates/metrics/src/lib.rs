f4bde0d73395fec84b0fd25f4595e8c8bbc307b4
Carmelo Piccione <carmelo.piccione@holo.host>
2019-11-03 17:32:22 -0500

    Stats aggregator utils

--- crates/metrics/src/lib.rs
+++ crates/metrics/src/lib.rs
@@ -5,9 +5,13 @@
 #[macro_use]
 extern crate log;
 
+#[macro_use]
+extern crate shrinkwraprs;
+
 pub mod cloudwatch;
 pub mod config;
 pub mod metrics;
+pub mod stats;
 
 pub use cloudwatch::*;
 pub use config::*;
---
+++ crates/metrics/src/stats.rs
@@ -0,0 +1,48 @@
+/// Extends the metric api with statistical aggregation functions
+use stats::OnlineStats;
+use std::collections::HashMap;
+use std::iter::FromIterator;
+use crate::Metric;
+
+#[derive(Shrinkwrap)]
+pub struct Stats(HashMap<String, OnlineStats>);
+
+fn empty_stat() -> OnlineStats {
+    OnlineStats::new()
+}
+
+impl FromIterator<Metric> for Stats {
+    fn from_iter<I: IntoIterator<Item=Metric>>(source: I) -> Stats {
+
+       Stats(source.into_iter().fold(HashMap::new(),
+        |mut stats_by_metric_name, metric| {
+            let entry = stats_by_metric_name.entry(metric.name);
+
+            let online_stats = entry.or_insert_with(empty_stat);
+            online_stats.add(metric.value);
+            stats_by_metric_name
+       }))
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn can_aggregate_stats_from_iterator() {
+        let latency_data = vec![50.0, 100.0, 150.0].into_iter().map(|x| Metric::new("latency", x));
+        let size_data = vec![1.0, 10.0, 100.0].into_iter().map(|x| Metric::new("size", x));
+        let all_data = latency_data.chain(size_data);
+        let stats = Stats::from_iter(all_data);
+
+        let latency_stats = stats.get("latency").expect("latency stats to be present");
+
+        assert_eq!(latency_stats.mean(), 100.0);
+        let size_stats = stats.get("size").expect("size stats to be present");
+
+        assert_eq!(size_stats.mean(), 90.0);
+     }
+
+}

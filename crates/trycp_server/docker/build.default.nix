c3ed4d692e97feff5b86b40c728de895106781fb
David Meister <thedavidmeister@gmail.com>
2019-11-03 22:12:42 +1000

    working smaller trycp_server

--- crates/trycp_server/docker/build.default.nix
+++ crates/trycp_server/docker/build.default.nix
@@ -1,12 +1,26 @@
 let
+ config = { };
  holonix = import (fetchTarball {
   url = https://github.com/holochain/holonix/archive/v0.0.39.tar.gz;
   sha256 = "1c8jk9cmpayjy7bndymx5c7wshwgd7a2xqda4lvp2y7ds72i8jhz";
-  }) { config = {}; };
+  }) { };
 in
 {
  dev-shell = holonix.pkgs.stdenv.mkDerivation({
-  buildInputs = [];
+  CARGO_HOME = "/holochain";
+  name = "dev-shell";
+  shellHook = ''
+  export PATH="$PATH:$CARGO_HOME/bin"
+  '';
+  buildInputs = [
+   holonix.pkgs.wget
+  ]
+  ++ holonix.rust.buildInputs
+  ++ (holonix.pkgs.callPackage ../default.nix {
+   config = config;
+   pkgs = holonix.pkgs;
+  }).buildInputs
+  ;
  });
 
  holochain = {

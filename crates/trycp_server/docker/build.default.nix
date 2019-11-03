be04a2a162230862fba5913f1e961c0e850a0f4d
David Meister <thedavidmeister@gmail.com>
2019-11-03 12:44:34 +1000

    wip on minimal trycp docker

--- crates/trycp_server/docker/build.default.nix
+++ crates/trycp_server/docker/build.default.nix
@@ -1,1 +1,17 @@
-let 
+let
+ holonix = import (fetchTarball {
+  url = https://github.com/holochain/holonix/archive/v0.0.39.tar.gz;
+  sha256 = "1c8jk9cmpayjy7bndymx5c7wshwgd7a2xqda4lvp2y7ds72i8jhz";
+  }) { config = {}; };
+in
+{
+ dev-shell = holonix.pkgs.stdenv.mkDerivation({
+  buildInputs = [];
+ });
+
+ holochain = {
+  hc = holonix.holochain.hc;
+  holochain = holonix.holochain.holochain;
+ };
+
+}

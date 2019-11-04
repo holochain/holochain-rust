3d1a67f9a04fc4df5aa3e6710309358092e9e420
Carmelo Piccione <carmelo.piccione@holo.host>
2019-11-04 13:47:28 -0500

    Constructor to create log streams

--- crates/metrics/src/cloudwatch.rs
+++ crates/metrics/src/cloudwatch.rs
@@ -15,23 +15,14 @@ pub struct CloudWatchMetricPublisher {
 impl From<Metric> for MetricDatum {
     fn from(metric: Metric) -> Self {
         let cloud_watch_metric = MetricDatum {
-            /// <p>Array of numbers that is used along with the <code>Values</code> array. Each number in the <code>Count</code> array is the number of times the corresponding value in the <code>Values</code> array occurred during the period. </p> <p>If you omit the <code>Counts</code> array, the default of 1 is used as the value for each count. If you include a <code>Counts</code> array, it must include the same amount of values as the <code>Values</code> array.</p>
             counts: None,
-            /// <p>The dimensions associated with the metric.</p>
             dimensions: None,
-            /// <p>The name of the metric.</p>
             metric_name: metric.name.clone(),
-            /// <p>The statistical values for the metric.</p>
             statistic_values: None,
-            /// <p>Valid values are 1 and 60. Setting this to 1 specifies this metric as a high-resolution metric, so that CloudWatch stores the metric with sub-minute resolution down to one second. Setting this to 60 specifies this metric as a regular-resolution metric, which CloudWatch stores at 1-minute resolution. Currently, high resolution is available only for custom metrics. For more information about high-resolution metrics, see <a href="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/publishingMetrics.html#high-resolution-metrics">High-Resolution Metrics</a> in the <i>Amazon CloudWatch User Guide</i>. </p> <p>This field is optional, if you do not specify it the default of 60 is used.</p>
             storage_resolution: None,
-            /// <p>The time the metric data was received, expressed as the number of milliseconds since Jan 1, 1970 00:00:00 UTC.</p>
             timestamp: None,
-            /// <p>When you are using a <code>Put</code> operation, this defines what unit you want to use when storing the metric.</p> <p>In a <code>Get</code> operation, this displays the unit that is used for the metric.</p>
             unit: None,
-            /// <p>The value for the metric.</p> <p>Although the parameter accepts numbers of type Double, CloudWatch rejects values that are either too small or too large. Values must be in the range of 8.515920e-109 to 1.174271e+108 (Base 10) or 2e-360 to 2e360 (Base 2). In addition, special values (for example, NaN, +Infinity, -Infinity) are not supported.</p>
             value: Some(metric.value),
-            /// <p>Array of numbers representing the values for the metric during the period. Each unique value is listed just once in this array, and the corresponding number in the <code>Counts</code> array specifies the number of times that value occurred during the period. You can include up to 150 unique values in each <code>PutMetricData</code> action that specifies a <code>Values</code> array.</p> <p>Although the <code>Values</code> array accepts numbers of type <code>Double</code>, CloudWatch rejects values that are either too small or too large. Values must be in the range of 8.515920e-109 to 1.174271e+108 (Base 10) or 2e-360 to 2e360 (Base 2). In addition, special values (for example, NaN, +Infinity, -Infinity) are not supported.</p>
             values: None,
         };
         cloud_watch_metric
@@ -73,7 +64,8 @@ impl MetricPublisher for CloudWatchMetricPublisher {
 #[derive(Clone)]
 pub struct CloudWatchLogger {
     pub client: CloudWatchLogsClient,
-    pub log_group_name: String,
+    pub log_group_name: Option<String>,
+    pub log_stream_name: Option<String>
 }
 
 const LOG_LIMIT: i64 = 1000000;
@@ -90,7 +82,7 @@ impl CloudWatchLogger {
             query_string,
             start_time: start_time.duration_since(UNIX_EPOCH).unwrap().as_secs() as i64,
             end_time: end_time.duration_since(UNIX_EPOCH).unwrap().as_secs() as i64,
-            log_group_name: Some(self.log_group_name.clone()),
+            log_group_name: self.log_group_name.clone(),
             log_group_names: None,
         };
 
@@ -101,7 +93,7 @@ impl CloudWatchLogger {
             query_id: query.query_id.unwrap(),
         };
 
-        let query_result: GetQueryResultsResponse = self
+       let query_result: GetQueryResultsResponse = self
             .client
             .get_query_results(get_query_results_request)
             .sync()
@@ -111,7 +103,26 @@ impl CloudWatchLogger {
 
         log_records
     }
+
+
+    pub fn with_log_stream(log_stream_name:String, log_group_name:String, region: &Region) -> Self {
+        let client = CloudWatchLogsClient::new(region.clone());
+
+        let log_stream_request = CreateLogStreamRequest {
+           log_group_name: log_group_name.clone(),
+           log_stream_name: log_stream_name.clone()
+        };
+
+        client.create_log_stream(log_stream_request).sync().unwrap();
+        Self {
+            client,
+            log_stream_name: Some(log_stream_name),
+            log_group_name: Some(log_group_name)
+        } 
+    }
+
 }
+
 
 impl MetricPublisher for CloudWatchLogger {
     fn publish(&mut self, metric: &Metric) {
@@ -122,12 +133,12 @@ impl MetricPublisher for CloudWatchLogger {
                 .unwrap()
                 .as_millis() as i64,
         };
-        // TODO study stream api
-        let log_stream_name = "TODO".to_string();
         let put_log_events_request = PutLogEventsRequest {
             log_events: vec![input_log_event],
-            log_group_name: self.log_group_name.clone(),
-            log_stream_name,
+            log_group_name: self.log_group_name.clone().unwrap_or_else(
+                || panic!("log_group_name must be set")),
+            log_stream_name: self.log_stream_name.clone().unwrap_or_else(
+                || panic!("log_stream_name must be set")),
             sequence_token: None,
         };
         self.client.put_log_events(put_log_events_request);

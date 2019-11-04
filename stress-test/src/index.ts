b46b374f09b0d7b704cee3d940338b0fa9202445
Eric Harris-Braun <eric@harris-braun.com>
2019-11-04 16:22:54 -0500

    wip zome-hammer stress test

--- stress-test/src/index.ts
+++ stress-test/src/index.ts
@@ -84,6 +84,7 @@ const M = parseInt(process.argv[3], 10) || 1
 
 console.log(`Running stress tests with N=${N}, M=${M}`)
 
-require('./all-on')(orchestrator.registerScenario, N, M)
+//require('./all-on')(orchestrator.registerScenario, N, M)
+require('./zome-hammer')(orchestrator.registerScenario, 1)
 
 orchestrator.run()
---
+++ stress-test/src/zome-hammer.ts
@@ -0,0 +1,26 @@
+import { Config } from '@holochain/try-o-rama'
+import * as R from 'ramda'
+import { ScenarioApi } from '@holochain/try-o-rama/lib/api'
+import { Player } from '@holochain/try-o-rama/lib/player'
+import { ConductorConfig } from '@holochain/try-o-rama/lib/types'
+
+const dna = Config.dna('passthrough-dna.dna.json', 'passthrough')
+const trace = R.tap(x => console.log('{T}', x))
+
+module.exports = (scenario, hammerCount) => {
+    scenario('hammer on a zome call', async (s, t) => {
+        const { alice } = await s.players({ alice: {instances: {app: dna}} }, true)
+
+        let calls = []
+        for (let i = 0; i < hammerCount; i++) {
+            calls.push(alice.call('app','main', 'commit_entry', { content: trace(`call-${i}`) }))
+        }
+
+        Promise.all(calls).then(function(values) {
+            console.log(values);
+        });
+
+        await s.consistency()
+
+    })
+}

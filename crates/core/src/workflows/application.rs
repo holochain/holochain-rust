c36c30b5df6eb4af8c83a61915592c11ab5c8de8
Nicolas Luck <nicolas@lucksus.eu>
2019-11-06 20:47:09 +0100

    Fix initialize for the loading case -> fix loading

--- crates/core/src/workflows/application.rs
+++ crates/core/src/workflows/application.rs
@@ -8,15 +8,24 @@ use holochain_core_types::{
     dna::Dna,
     error::{HcResult, HolochainError},
 };
+use log::error;
 use std::sync::Arc;
 
 pub async fn initialize(
     instance: &Instance,
-    dna: Option<Dna>,
+    maybe_dna: Option<Dna>,
     context: Arc<Context>,
 ) -> HcResult<Arc<Context>> {
     let instance_context = instance.initialize_context(context.clone());
-    let dna = dna.ok_or(HolochainError::DnaMissing)?;
+
+    let dna = if let Some(dna) = maybe_dna {
+        dna
+    } else {
+        context.get_dna().ok_or_else(|| {
+            error!("No DNA provided during loading and none found in state");
+            HolochainError::DnaMissing
+        })?
+    };
 
     // 2. Initialize the local chain if not already
     let first_initialization = match get_dna_and_agent(&instance_context).await {

4212dc3990a47ef296aeabdec5a144df5ca42961
Eric Harris-Braun <eric@harris-braun.com>
2019-11-07 20:21:40 -0500

    update holonix for trycp dockerbuild

--- crates/trycp_server/docker/build.default.nix
+++ crates/trycp_server/docker/build.default.nix
@@ -1,8 +1,8 @@
 let
  config = { };
  holonix = import (fetchTarball {
-  url = https://github.com/holochain/holonix/archive/v0.0.39.tar.gz;
-  sha256 = "1c8jk9cmpayjy7bndymx5c7wshwgd7a2xqda4lvp2y7ds72i8jhz";
+  url = https://github.com/holochain/holonix/archive/v0.0.44.tar.gz;
+  sha256 = "0819439idwhdbavmlcy99c2ai5d9a0k7rbimbsk47p9vndw3s6cy";
   }) { };
 in
 {

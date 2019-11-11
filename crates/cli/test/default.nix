c9e8e66f6c446a5832be7a8c7507612d6dafd895
Willem Olding <willemolding@gmail.com>
2019-11-11 13:48:05 +1100

    add commands to nix and call from CLI to generate and test

--- crates/cli/test/default.nix
+++ crates/cli/test/default.nix
@@ -1,14 +1,33 @@
 { pkgs }:
 let
 
-  name = "hc-cli-test";
-
-  script = pkgs.writeShellScriptBin name
+  test_script = pkgs.writeShellScriptBin "hc-cli-test"
   ''
   ( cd crates/cli && cargo test )
   bats crates/cli/test/hc.bats
   '';
+
+  test_gen_rust_script = pkgs.writeShellScriptBin "hc-cli-generate-rust-test"
+  ''
+  	hc init /tmp/test_gen_rust
+	cd /tmp/test_gen_rust
+	hc generate zomes/my_zome rust
+	hc test
+  '';
+
+  test_gen_rust_proc_script = pkgs.writeShellScriptBin "hc-cli-generate-rust-proc-test"
+  ''
+  	hc init /tmp/test_gen_rust_proc
+	cd /tmp/test_gen_rust_proc
+	hc generate zomes/my_zome rust-proc
+	hc test
+  '';
 in
+
 {
- buildInputs = [ script ];
+ buildInputs = [ 
+ 	test_script
+ 	test_gen_rust_script
+ 	test_gen_rust_proc_script
+ ];
 }

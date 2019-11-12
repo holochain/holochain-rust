36bc4aa6b5ad4f2146e5a99eb6630d6182640daf
David Meister <thedavidmeister@gmail.com>
2019-11-12 15:47:24 +1100

    remove windows and travis cruft

--- build.bat
+++
@@ -1,6 +0,0 @@
-cd crates/conductor_lib/wasm-test && cargo +nightly-2019-07-14 build --release --target wasm32-unknown-unknown && cd ../../
-cd crates/conductor_lib/test-bridge-caller && cargo +nightly-2019-07-14 build --release --target wasm32-unknown-unknown && cd ../../
-cd crates/hdk/wasm-test && cargo +nightly-2019-07-14 build --release --target wasm32-unknown-unknown && cd ../../
-cd crates/wasm_utils/wasm-test/integration-test && cargo +nightly-2019-07-14 build --release --target wasm32-unknown-unknown && cd ../../../
-cd crates/core/src/nucleus/actions/wasm-test && cargo +nightly-2019-07-14 build --release --target wasm32-unknown-unknown && cd ../../../../../
-cargo +nightly-2019-07-14 build

e9e21e21f453f1302dca2fc1760022d9c9fb8f51
Perry Kundert <perry@hardconsulting.com>
2019-11-12 14:28:20 -0700

    Clean up some compilation issues substituting HDK_VERSION for GIT_HASH

--- crates/conductor_lib/src/holochain.rs
+++ crates/conductor_lib/src/holochain.rs
@@ -139,9 +139,9 @@ impl Holochain {
             );
 
             if let Ok(json_string) = maybe_json_string {
-                if json_string != holochain_core_types::HDK_VERSION.into() {
+                if json_string.to_string() != holochain_core_types::hdk_version::HDK_VERSION.to_string() {
                     eprintln!("WARNING! The HDK Version of the runtime and the zome don't match.");
-                    eprintln!("Runtime HDK Version: {}", holochain_core_types::HDK_VERSION);
+                    eprintln!("Runtime HDK Version: {}", holochain_core_types::hdk_version::HDK_VERSION.to_string());
                     eprintln!("Zome HDK Version: {}", json_string);
                 }
             }
--- crates/hdk/src/api/version.rs
+++ crates/hdk/src/api/version.rs
@@ -1,20 +1,17 @@
 use crate::{
-    error::{ZomeApiError, ZomeApiResult},
+    error::ZomeApiResult,
     Dispatch,
 };
 use holochain_wasm_utils::api_serialization::meta::{MetaArgs, MetaMethod, MetaResult};
 
-///this method will return the current version of the HDK based on the latest git tag
+///this method will return the current version of the HDK
 pub fn version<S: Into<String>>() -> ZomeApiResult<String> {
     let meta = Dispatch::Meta.with_input(MetaArgs {
         method: MetaMethod::Version,
     })?;
-    let version = match meta {
-        MetaResult::Version(ver) => Ok(ver),
-        _ => Err(ZomeApiError::Internal(
-            "Wrong Meta Type, Problem In Core".to_string(),
-        )),
-    }?;
+    let version: String = match meta {
+        MetaResult::Version(ver) => ver.to_string(),
+    };
 
     Ok(version)
 }

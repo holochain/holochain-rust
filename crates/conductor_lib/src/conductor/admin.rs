c25557422f93c5c829fb6f0c635b370f38891bfd
Willem Olding <willemolding@gmail.com>
2019-11-13 20:12:54 +1100

    adds default back to lmdb

--- crates/conductor_lib/src/conductor/admin.rs
+++ crates/conductor_lib/src/conductor/admin.rs
@@ -201,13 +201,14 @@ impl ConductorAdmin for Conductor {
             id: id.to_string(),
             dna: dna_id.to_string(),
             agent: agent_id.to_string(),
-            storage: StorageConfiguration::Pickle {
+            storage: StorageConfiguration::Lmdb {
                 path: storage_path
                     .to_str()
                     .ok_or_else(|| {
                         HolochainError::ConfigError(format!("invalid path {:?}", storage_path))
                     })?
                     .into(),
+                initial_mmap_bytes: None,
             },
         };
         new_config.instances.push(new_instance_config);

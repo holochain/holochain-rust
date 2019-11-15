fefd74c154bd4c8b7d517dfd6e6df65023a625b7
Ashanti Mutinta <Ashantimutinta@gmail.com>
2019-11-15 14:19:12 -0500

    conditional compilation

--- crates/core_types/src/flamerwrapper.rs
+++ crates/core_types/src/flamerwrapper.rs
@@ -1,33 +1,46 @@
+#[cfg(not(target_arch = "wasm32"))]
 extern crate flame;
-use std::{fs::File,env};
+#[cfg(not(target_arch = "wasm32"))]
+use std::fs::File;
+use std::env;
 const FLAME_ENV : &str = "COMPILE_WITH_FLAME";
 const FLAME_PATH : &str = "FLAME_GRAPH_PATH" ;
 pub struct FlamerWrapper;
 
 impl FlamerWrapper
 {
+    #[allow(unused_variables)]
     pub fn start(guard_name:&'static str )
     {
         if let Ok(setting) = env::var(FLAME_ENV)
         {
             if setting =="YES"
             {
+                #[cfg(not(target_arch = "wasm32"))]
                 flame::start(guard_name);
+                #[cfg(target_arch = "wasm32")]
+                warn!("cannot compile flamer for wasm, will not start guard");
             }
         }
     }
-
+    
+    #[allow(unused_variables)]
     pub fn end(guard_name:&'static str)
     {
         if let Ok(setting) = env::var(FLAME_ENV)
         {
             if setting =="YES"
             {
+                #[cfg(not(target_arch = "wasm32"))]
                 flame::end(guard_name);
+                #[cfg(target_arch = "wasm32")]
+                warn!("cannot compile flamer for wasm");
             }
         }
     }
+  
 
+    #[allow(unused_variables)]
     pub fn dump_html() 
     {
         if let Ok(setting) = env::var(FLAME_ENV)
@@ -36,16 +49,22 @@ impl FlamerWrapper
             {
                 if let Ok(path) = env::var(FLAME_PATH)
                 {
+                    #[cfg(not(target_arch = "wasm32"))]
                     File::create(path).map(|file_for_flame|{
+                        debug!("about to dump to flame graph");
                         flame::dump_html(file_for_flame).unwrap_or_else(|_|{
                             warn!("Flame graph enabled but cannot print to path")
                         })
                     }).unwrap_or_else(|_|{
                         warn!("Path provided for flame graph not valid will not be able to dump html")
-                    })
+                    });
+                    #[cfg(target_arch = "wasm32")]
+                    warn!("flame graphs not enabled for wasm")
+                   
                     
                 }
             }
         }
     }
+  
 }

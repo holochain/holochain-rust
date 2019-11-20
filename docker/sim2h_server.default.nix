9ae08a344244fbf3617cb683b8c3fbe513437e83
David Meister <thedavidmeister@gmail.com>
2019-11-20 20:56:35 +1100

    fix sim2h_server install inside docker

--- docker/sim2h_server.default.nix
+++ docker/sim2h_server.default.nix
@@ -1,11 +1,16 @@
 let
  minimal = import ./minimal.default.nix;
+ sim2h_server = minimal.holonix.pkgs.callPackage ../crates/sim2h_server/default.nix {
+  pkgs = minimal.holonix.pkgs;
+  config = { };
+ };
 in
 {
  dev-shell = minimal.holonix.pkgs.stdenv.mkDerivation(minimal.shell-config // {
   buildInputs = [
    minimal.holonix.pkgs.wget
   ]
+  ++ sim2h_server.buildInputs
   ++ minimal.shell-config.buildInputs
   ;
  });

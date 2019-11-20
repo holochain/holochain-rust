b2d8b313bfe70d82194152747208f54c891b2a8d
David Meister <thedavidmeister@gmail.com>
2019-11-20 21:28:43 +1100

    add more tools to trycp_server docker

--- docker/trycp_server.default.nix
+++ docker/trycp_server.default.nix
@@ -8,9 +8,6 @@ in
 {
  dev-shell = minimal.holonix.pkgs.stdenv.mkDerivation(minimal.shell-config // {
   buildInputs = [
-   minimal.holonix.pkgs.wget
-   minimal.holonix.pkgs.ps
-   minimal.holonix.pkgs.more
   ]
   ++ trycp_server.buildInputs
   ++ minimal.shell-config.buildInputs

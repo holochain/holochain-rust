c3dbb05baac0cb2d713e3f5d410ae775893a16d4
Eric Harris-Braun <eric@harris-braun.com>
2019-11-23 18:44:27 +0100

    fmt

--- crates/core_types/build.rs
+++ crates/core_types/build.rs
@@ -49,7 +49,10 @@ fn main() {
         let git_hash = String::from_utf8(output.stdout).unwrap();
         println!("cargo:rustc-env=GIT_HASH={}", git_hash);
     }
-    if let Ok(output) = Command::new("git").args(&["rev-parse","--abbrev-ref", "HEAD"]).output() {
+    if let Ok(output) = Command::new("git")
+        .args(&["rev-parse", "--abbrev-ref", "HEAD"])
+        .output()
+    {
         let git_branch = String::from_utf8(output.stdout).unwrap();
         println!("cargo:rustc-env=GIT_BRANCH={}", git_branch);
     }

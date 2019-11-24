7b5231f747a01afbff05c86b8a9f432abdc15485
Nicolas Luck <nicolas@lucksus.eu>
2019-11-24 20:21:52 +0100

    Don't waste threads during validation if we're blocking the calling thread anyway

--- crates/core/src/nucleus/actions/run_validation_callback.rs
+++ crates/core/src/nucleus/actions/run_validation_callback.rs
@@ -1,5 +1,4 @@
 use crate::{
-    action::{Action, ActionWrapper},
     context::Context,
     nucleus::{
         ribosome::{self, runtime::WasmCallData},
@@ -8,7 +7,7 @@ use crate::{
     },
 };
 use futures::{future::Future, task::Poll};
-use holochain_core_types::{error::HolochainError, ugly::lax_send_sync};
+use holochain_core_types::error::HolochainError;
 use holochain_persistence_api::{cas::content::Address, hash::HashString};
 use snowflake;
 use std::{pin::Pin, sync::Arc};
@@ -21,65 +20,47 @@ use holochain_metrics::Metric;
 /// Dispatches an `Action::ReturnValidationResult` after completion of the WASM call.
 /// Returns a future that waits for the result to appear in the nucleus state.
 pub async fn run_validation_callback(
-    address: Address,
+    _address: Address,
     call: CallbackFnCall,
     context: &Arc<Context>,
 ) -> ValidationResult {
-    let id = snowflake::ProcessUniqueId::new();
-    let clone_address = address.clone();
     let cloned_context = context.clone();
 
     let clock = std::time::SystemTime::now();
     let call2 = call.clone();
 
-    context.clone().spawn_task(move || {
-        let validation_result: ValidationResult = match ribosome::run_dna(
-            Some(call2.clone().parameters.to_bytes()),
-            WasmCallData::new_callback_call(cloned_context.clone(), call2),
-        ) {
-            Ok(call_result) => {
-                if call_result.is_null() {
-                    Ok(())
-                } else {
-                    Err(ValidationError::Fail(call_result.to_string()))
-                }
+    let validation_result: ValidationResult = match ribosome::run_dna(
+        Some(call2.clone().parameters.to_bytes()),
+        WasmCallData::new_callback_call(cloned_context.clone(), call2),
+    ) {
+        Ok(call_result) => {
+            if call_result.is_null() {
+                Ok(())
+            } else {
+                Err(ValidationError::Fail(call_result.to_string()))
             }
-            // TODO: have "not matching schema" be its own error
-            Err(HolochainError::RibosomeFailed(error_string)) => {
-                if error_string == "Argument deserialization failed" {
-                    Err(ValidationError::Error(
-                        String::from("JSON object does not match entry schema").into(),
-                    ))
-                } else {
-                    // an unknown error from the ribosome should panic rather than
-                    // silently failing validation
-                    panic!(error_string)
-                }
+        }
+        // TODO: have "not matching schema" be its own error
+        Err(HolochainError::RibosomeFailed(error_string)) => {
+            if error_string == "Argument deserialization failed" {
+                Err(ValidationError::Error(
+                    String::from("JSON object does not match entry schema").into(),
+                ))
+            } else {
+                // an unknown error from the ribosome should panic rather than
+                // silently failing validation
+                panic!(error_string)
             }
-            Err(error) => panic!(error.to_string()), // same here
-        };
-
-        lax_send_sync(
-            cloned_context.action_channel().clone(),
-            ActionWrapper::new(Action::ReturnValidationResult((
-                (id, clone_address),
-                validation_result,
-            ))),
-            "run_validation_callback",
-        );
-    });
-
-    let awaited = ValidationCallbackFuture {
-        context: context.clone(),
-        key: (id, address),
-    }
-    .await;
+        }
+        Err(error) => panic!(error.to_string()), // same here
+    };
 
     let metric_name = format!("{}.{}.latency", call.zome_name, call.fn_name);
     let latency = clock.elapsed().unwrap().as_millis();
     let metric = Metric::new(metric_name.as_str(), latency as f64);
     context.metric_publisher.write().unwrap().publish(&metric);
-    awaited
+
+    validation_result
 }
 
 /// ValidationFuture resolves to an Ok(ActionWrapper) or an Err(error_message:String).

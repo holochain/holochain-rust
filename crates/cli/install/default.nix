54becb870b4aba29d72902427f41cea0f0454031
David Meister <thedavidmeister@gmail.com>
2019-12-14 23:28:31 +0400

    try CARGO_TARGET_DIR for cargo install

--- crates/cli/install/default.nix
+++ crates/cli/install/default.nix
@@ -5,7 +5,7 @@ let
   script = pkgs.writeShellScriptBin name
   ''
   set -euxo pipefail
-  cargo install -f --path crates/cli
+  CARGO_TARGET_DIR=$HC_TARGET_PREFIX/cli/install cargo install -f --path crates/cli
   '';
 in
 {

6c7543979893c1e65fbcb8226d08a06b2f1f61ef
David Meister <thedavidmeister@gmail.com>
2019-12-15 14:46:22 +0400

    fix nix-shell

--- crates/holochain_wasm/compile/default.nix
+++ crates/holochain_wasm/compile/default.nix
@@ -8,7 +8,7 @@ let
   hc-conductor-wasm-bindgen-install
   echo $CARGO_TARGET_DIR
   ( cd crates/holochain_wasm && cargo build --release -p holochain_conductor_wasm --target wasm32-unknown-unknown )
-  wasm-bindgen --out-dir ./crates/holochain_wasm/npm_package/gen --nodejs "$CARGO_TARGET_DIR/wasm32-unknown-unknown/release/holochain_conductor_wasm.wasm
+  wasm-bindgen --out-dir ./crates/holochain_wasm/npm_package/gen --nodejs "$CARGO_TARGET_DIR"/wasm32-unknown-unknown/release/holochain_conductor_wasm.wasm
   '';
 in
 {

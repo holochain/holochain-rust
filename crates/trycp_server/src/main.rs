ab0ed64c0e953cc9063421f5ec4db01a7637041e
Carmelo Piccione <carmelo.piccione@holo.host>
2019-12-28 00:31:45 -0500

    clippy fix

--- crates/trycp_server/src/main.rs
+++ crates/trycp_server/src/main.rs
@@ -520,7 +520,8 @@ fn check_player_config(
 
 fn is_program_in_path(program: &str) -> bool {
     if let Ok(path) = env::var("PATH") {
-        for p in path.split(":") {
+        for p in path.split(':') {
+            // TODO this is not windows compatible
             let p_str = format!("{}/{}", p, program);
             if fs::metadata(p_str).is_ok() {
                 return true;

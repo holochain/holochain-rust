abfed371db8a132b3430dbc18bde30b37fe55cca
Ashanti Mutinta <ashantimutinta@gmail.com>
2020-01-02 07:54:46 -0500

    added profiling tool for conductor

--- profiling/default.nix
+++ profiling/default.nix
@@ -6,11 +6,16 @@ let
   cd crates/sim2h_server && cargo build -p sim2h_server --release && perf record --call-graph dwarf sim2h_server
   '';
 
+  script-hc-conductor-perf = pkgs.writeShellScriptBin "hc-conductor-perf"
+  ''
+  cd crates/holochain && cargo build -p holochain --release && perf record --call-graph dwarf holochain
+  '';
+
   script-hc-generate-flame-graph = pkgs.writeShellScriptBin "hc-generate-flame-graph"
   ''
   rm -rf FlameGraph && git clone https://github.com/brendangregg/FlameGraph && perf script | perl FlameGraph/stackcollapse-perf.pl | perl FlameGraph/flamegraph.pl > generated-graph.svg
   '';
 in
 {
-  buildInputs = [ script-sim2h-server-perf script-hc-generate-flame-graph];
+  buildInputs = [ script-sim2h-server-perf script-hc-generate-flame-graph script-hc-conductor-perf];
 }

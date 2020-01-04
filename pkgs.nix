0a47ccf45e5cc0c762390cbe2345d470bf967ebb
Perry Kundert <perry@hardconsulting.com>
2020-01-04 15:50:34 -0800

    Reference current holo-nixpkgs HC 0.0.41-alpha4 branch

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/d980d3fbc2e4d976bd4d2ac904b6987e290cfedb.tar.gz";
-  sha256 = "0cij78slz0qg28q6cni5i83bc7gh3qk2kxqcvvj50jz50z5s7d41";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/b8e533035cde7d34f7f3367fe760dbf3cd30dafa.tar.gz";
+  sha256 = "1x1gvh3vi0vfvwcv6addf1hq49shdqysd9nyjhw65y58gza29ann";
 })

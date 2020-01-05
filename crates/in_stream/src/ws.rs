b4112e4aadb0baa1b7ec8177021839a14438ae8e
Eric Harris-Braun <eric@harris-braun.com>
2020-01-05 17:00:52 -0500

    regularize intsream tracing for url

--- crates/in_stream/src/ws.rs
+++ crates/in_stream/src/ws.rs
@@ -298,15 +298,17 @@ impl<Sub: InStreamStd> InStream<&mut WsFrame, WsFrame> for InStreamWss<Sub> {
 
     fn read(&mut self, data: &mut WsFrame) -> Result<usize> {
         self.priv_process()?;
-        log::trace!("instream ws read from {} with connection state: {:?}",
-                self.remote_url, self.state
-            );
+        log::trace!(
+            "read from {} with connection state: {:?}",
+            self.remote_url,
+            self.state
+        );
         match &mut self.state {
             None => Err(ErrorKind::NotConnected.into()),
             Some(state) => match state {
                 WssState::Ready(wss) => {
                     let r = wss.read_message();
-                    log::trace!("instream ws read result: {:?}", r);
+                    log::trace!("read result from {}: {:?}", self.remote_url, r);
                     match r {
                         Ok(msg) => {
                             data.assume(msg);

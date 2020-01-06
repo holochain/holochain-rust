dd5c3851251050e89df3b4213456cc4633fc4ba1
Ashanti Mutinta <ashantimutinta@gmail.com>
2020-01-06 12:37:55 -0500

    fixed renaming

--- crates/locksmith/src/tracker.rs
+++ crates/locksmith/src/tracker.rs
@@ -5,7 +5,7 @@ use crate::{
     },
     error::LockType,
 };
-#[cfg(feature = "sync_backtrace_capture")]
+#[cfg(feature = "sync_backtrace")]
 use backtrace::Backtrace;
 use snowflake::ProcessUniqueId;
 use std::{
@@ -16,7 +16,7 @@ use std::{
 pub(crate) struct GuardTracker {
     pub(crate) puid: ProcessUniqueId,
     pub(crate) created: Instant,
-    #[cfg(feature = "sync_backtrace_capture")]
+    #[cfg(feature = "sync_backtrace")]
     pub(crate) backtrace: Backtrace,
     pub(crate) lock_type: LockType,
     pub(crate) immortal: bool,
@@ -29,7 +29,7 @@ impl GuardTracker {
             puid,
             lock_type,
             created: Instant::now(),
-            #[cfg(feature = "sync_backtrace_capture")]
+            #[cfg(feature = "sync_backtrace")]
             backtrace: Backtrace::new_unresolved(),
             immortal: false,
             annotation: None,
@@ -67,14 +67,14 @@ impl GuardTracker {
             return;
         }
         self.immortal = true;
-        #[cfg(feature = "sync_backtrace_capture")]
+        #[cfg(feature = "sync_backtrace")]
         self.backtrace.resolve();
         let annotation = self
             .annotation
             .as_ref()
             .map(|a| format!("\nAnnotation: {}\n", a))
             .unwrap_or_default();
-        #[cfg(feature = "sync_backtrace_capture")]
+        #[cfg(feature = "sync_backtrace")]
         error!(
             r"
 
@@ -92,7 +92,7 @@ Backtrace at the moment of guard creation follows:
             annotation=annotation,
             backtrace=self.backtrace
         );
-        #[cfg(not(feature = "sync_backtrace_capture"))]
+        #[cfg(not(feature = "sync_backtrace"))]
         error!(
             r"
 

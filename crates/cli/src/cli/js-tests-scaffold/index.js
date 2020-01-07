2b5eacacea4bca9e9d6130ee4f0d07ae47c06b7d
Tom <tomrgowan@gmail.com>
2020-01-07 18:49:37 +1100

    Update index.js

--- crates/cli/src/cli/js-tests-scaffold/index.js
+++ crates/cli/src/cli/js-tests-scaffold/index.js
@@ -2,16 +2,23 @@
 /// See the tryorama README [https://github.com/holochain/tryorama]
 /// for a potentially more accurate example
 
-const path = require('path')
+const path = require('path');
 
-const { Orchestrator, Config, combine, singleConductor, localOnly, tapeExecutor } = require('@holochain/tryorama')
+const {
+  Orchestrator,
+  Config,
+  combine,
+  singleConductor,
+  localOnly,
+  tapeExecutor,
+} = require('@holochain/tryorama');
 
 process.on('unhandledRejection', error => {
   // Will print "unhandledRejection err is not defined"
   console.error('got unhandledRejection:', error);
 });
 
-const dnaPath = path.join(__dirname, "../dist/<<DNA_NAME>>.dna.json")
+const dnaPath = path.join(__dirname, '../dist/app5.dna.json');
 
 const orchestrator = new Orchestrator({
   middleware: combine(
@@ -29,26 +36,37 @@ const orchestrator = new Orchestrator({
     // send messages across conductors
     singleConductor,
   ),
-})
+});
 
-const dna = Config.dna(dnaPath, 'scaffold-test')
-const conductorConfig = Config.gen({myInstanceName: dna})
+const dna = Config.dna(dnaPath, 'scaffold-test');
+const conductorConfig = Config.gen({myInstanceName: dna});
 
-orchestrator.registerScenario("description of example test", async (s, t) => {
-
-  const {alice, bob} = await s.players({alice: conductorConfig, bob: conductorConfig}, true)
+orchestrator.registerScenario('description of example test', async (s, t) => {
+  const {alice, bob} = await s.players(
+    {alice: conductorConfig, bob: conductorConfig},
+    true,
+  );
 
   // Make a call to a Zome function
   // indicating the function, and passing it an input
-  const addr = await alice.call("myInstanceName", "my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
+  const addr = await alice.call(
+    'myInstanceName',
+    'my_zome',
+    'create_my_entry',
+    {entry: {content: 'sample content'}},
+  );
 
   // Wait for all network activity to settle
-  await s.consistency()
+  await s.consistency();
 
-  const result = await bob.call("myInstanceName", "my_zome", "get_my_entry", {"address": addr.Ok})
+  const result = await bob.call('myInstanceName', 'my_zome', 'get_my_entry', {
+    address: addr.Ok,
+  });
 
   // check for equality of the actual and expected results
-  t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
-})
+  t.deepEqual(result, {
+    Ok: {App: ['my_entry', '{"content":"sample content"}']},
+  });
+});
 
-orchestrator.run()
+orchestrator.run();

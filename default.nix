6f8b8d62512ffec2f3f4353d14d1e790b9f66b5a
Ashanti Mutinta <ashantimutinta@gmail.com>
2020-01-14 03:10:55 -0500

    added needed pcre dependancy

--- default.nix
+++ default.nix
@@ -43,7 +43,7 @@ with holonix.pkgs;
   LIBCLANG_PATH="${holonix.pkgs.llvmPackages.libclang}/lib";
   hardeningDisable = [ "fortify" ];
 
-  buildInputs = [ holonix.pkgs.libiconv]
+  buildInputs = [ holonix.pkgs.libiconv holonix.pkgs.pcre]
    ++ holonix.shell.buildInputs
 
    ++ (holonix.pkgs.callPackage ./app_spec_proc_macro {

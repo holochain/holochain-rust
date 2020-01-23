2cb9e2cf0594b8d3e6c22d9debdae9b1e048baa9
Ashanti Mutinta <Ashantimutinta@gmail.com>
2020-01-23 11:34:25 -0500

    Hardening enabled for minimal

--- docker/minimal.default.nix
+++ docker/minimal.default.nix
@@ -5,6 +5,12 @@ let
   sha256 = config.holonix.github.sha256;
   }) { };
  shell-config = {
+  LIBCLANG_PATH="${pkgs.llvmPackages.libclang}/lib";
+
+ # needed for newrelic to compile its dependencies
+ # this is a hack to workaround this:
+ # https://github.com/NixOS/nixpkgs/issues/18995
+ hardeningDisable = [ "fortify" ];
   CARGO_HOME = "/holochain/.cargo";
   name = "dev-shell";
   buildInputs = [

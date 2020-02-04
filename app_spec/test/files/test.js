ad41bf53abf26cd3021f97cc128be12d42b34472
Michael Dougherty <michael.dougherty@holo.host>
2020-02-04 09:57:33 -0800

    Update app_spec/test/files/test.js
    Co-Authored-By: Nicolas Luck <nicolas@lucksus.eu>

--- app_spec/test/files/test.js
+++ app_spec/test/files/test.js
@@ -7,7 +7,7 @@ module.exports = scenario => {
     t.notEqual(alice.info('app').agentAddress, bob.info('app').agentAddress)
   })
 
-  scenario.only('send ping', async (s, t) => {
+  scenario('send ping', async (s, t) => {
     const { alice, bob } = await s.players({ alice: one, bob: one }, true)
     const params = { to_agent: bob.info('app').agentAddress, message: 'hello' }
     const result = await alice.call('app', 'blog', 'ping', params)

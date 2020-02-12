3e32a47a206a642fad1fec7a181d1c2d9ab7396d
neonphog <neonphog@gmail.com>
2020-02-12 11:27:14 -0700

    comment the kcov script - disable tarpaulin

--- rust/test/default.nix
+++ rust/test/default.nix
@@ -7,15 +7,36 @@ let
 
   hc-rust-coverage-kcov = pkgs.writeShellScriptBin "hc-rust-coverage-kcov"
   ''
+  # we need the kcov tool and curl for fetching the codecov.io uploader
   nix-env -f https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz -iA kcov curl
+
+  # using cargo-make for executing kcov
   cargo install cargo-make
+
+  # some tests require compiled wasm
   hc-rust-wasm-compile
+
+  # kcov does not work with the global /holochain-rust/target
   mkdir -p target
+
+  # actually kcov does not work with workspace target either
+  # we need to use targets in each crate - but that is slow
+  # use symlinks so we don't have to recompile deps over and over
   for i in ''$(find crates -maxdepth 1 -mindepth 1 -type d | sort); do
+
+    # delete all other test binaries so they don't get run multiple times
     rm -rf $(find target/debug -maxdepth 1 -mindepth 1 -type f)
+
     echo "-------"
     echo "coverage for '$i'"
     echo "-------"
+
+    # cd into crate dirs
+    # remove any pre-existing target dir
+    # symlink to shared target dir
+    # build the test binaries
+    # run the code coverage
+
     ( \
       cd $i && \
       rm -rf target || true && \
@@ -24,8 +45,12 @@ let
       cargo test --no-run && \
       cargo make coverage-kcov \
     )
+
+    # then remove the symlink so we don't double count the coverage reports
     rm -rf "''${i}/target"
   done
+
+  # upload to codecove.io
   bash <(curl -s https://codecov.io/bash)
   '';
 

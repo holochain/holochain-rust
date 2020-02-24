41678e69f584a3892bb5ca4dd9b037274f63897b
David Meister <thedavidmeister@gmail.com>
2020-02-24 22:22:13 +0400

    repro compiler issue

--- crates/core/src/nucleus/actions/run_validation_callback.rs
+++ crates/core/src/nucleus/actions/run_validation_callback.rs
@@ -4,14 +4,14 @@ use crate::{
         CallbackFnCall,
     },
     wasm_engine::{self, runtime::WasmCallData},
-    // NEW_RELIC_LICENSE_KEY,
+    NEW_RELIC_LICENSE_KEY,
 };
 use holochain_core_types::validation::ValidationResult;
 use holochain_core_types::validation::ValidationError;
 use holochain_core_types::error::HolochainError;
 use holochain_persistence_api::cas::content::Address;
 use std::sync::Arc;
-use holochain_wasm_types::WasmError;
+// use holochain_wasm_types::WasmError;
 use std::string::ToString;
 use holochain_metrics::with_latency_publishing;
 
@@ -22,7 +22,7 @@ use holochain_metrics::with_latency_publishing;
 /// Returns a future that waits for the result to appear in the nucleus state.
 #[no_autotrace] // TODO: get autotrace working for this future
 // TODO: uncommenting this causes line numbers to disappear in this file for compiler errors
-// #[holochain_tracing_macros::newrelic_autotrace(HOLOCHAIN_CORE)]
+#[holochain_tracing_macros::newrelic_autotrace(HOLOCHAIN_CORE)]
 pub async fn run_validation_callback(
     _address: Address,
     call: CallbackFnCall,
@@ -46,7 +46,7 @@ pub async fn run_validation_callback(
                 Ok(v) => v,
                 // TODO: have "not matching schema" be its own error
                 Err(HolochainError::Wasm(wasm_error)) => {
-                    if wasm_error == WasmError::ArgumentDeserializationFailed {
+                    if wasm_error == "WasmError::ArgumentDeserializationFailed" {
                         ValidationResult::Err(
                             ValidationError::Fail(
                             "JSON object does not match entry schema".into()

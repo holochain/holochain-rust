d2cabb81be19461fedf530f81301a7a5af4415bc
Eric Harris-Braun <eric@harris-braun.com>
2020-03-02 13:50:34 -0500

    add test for sim2h_url override

--- crates/core/src/network/reducers/init.rs
+++ crates/core/src/network/reducers/init.rs
@@ -206,4 +206,37 @@ pub mod test {
         let network = network_state.network.expect("No network connection set");
         assert_eq!(network.p2p_endpoint().as_str(), "wss://localhost:9000/");
     }
+
+    #[test]
+    pub fn should_not_set_sim2h_url_if_overridden() {
+        let p2p_config = P2pConfig::new_with_sim2h_backend("wss://0.0.0.0:9999");
+        let context: Arc<Context> = test_context(p2p_config);
+        let dna_address: Address = context.agent_id.address();
+        let agent_id = context.agent_id.content().to_string();
+        let handler = NetHandler::new(Box::new(|_| Ok(())));
+        let network_settings = crate::action::NetworkSettings {
+            p2p_config: context.p2p_config.clone(),
+            dna_address,
+            agent_id,
+            handler,
+        };
+        let action_wrapper = ActionWrapper::new(Action::InitNetwork(network_settings));
+
+        let mut network_state = NetworkState::new();
+        let mut root_state = test_store(context.clone());
+
+        let props = json!({"sim2h_url": "wss://localhost:9000"});
+        let mut dna = Dna::new();
+        dna.properties = props;
+
+        root_state = root_state.reduce(ActionWrapper::new(Action::InitializeChain(dna)));
+
+        std::env::set_var("HC_IGNORE_SIM2H_URL_PROPERTY", "true");
+        let result = reduce_init(&mut network_state, &root_state, &action_wrapper);
+
+        assert_eq!(result, ());
+
+        let network = network_state.network.expect("No network connection set");
+        assert_eq!(network.p2p_endpoint().as_str(), "wss://0.0.0.0:9999/");
+    }
 }

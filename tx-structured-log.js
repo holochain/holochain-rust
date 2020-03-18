33cd354379987817d94e22da6ec35a294070bca2
neonphog <neonphog@gmail.com>
2020-03-18 14:25:38 -0600

    tweak

--- tx-structured-log.js
+++ tx-structured-log.js
@@ -1,53 +1,57 @@
 #!/usr/bin/env node
 
 function get_json_lines(cb) {
-  let chunks = []
+  return new Promise((resolve, reject) => {
+    let chunks = []
 
-  const checkProcess = () => {
-    chunks = [chunks.join('')]
-    let idx = chunks[0].indexOf('\n')
-    while (idx > -1) {
-      const line = chunks[0].substr(0, idx)
-      chunks[0] = chunks[0].substr(idx + 1)
+    const checkProcess = () => {
+      chunks = [chunks.join('')]
+      let idx = chunks[0].indexOf('\n')
+      while (idx > -1) {
+        const line = chunks[0].substr(0, idx)
+        chunks[0] = chunks[0].substr(idx + 1)
 
-      const m = line.trim().match(/(?:^({.*)|<SL<(.*)>SL>)/)
-      if (m && m.length == 3) {
-        try {
-          const data = JSON.parse(m[1] || m[2])
+        const m = line.trim().match(/(?:^({.*)|<SL<(.*)>SL>)/)
+        if (m && m.length == 3) {
+          let data
+          try {
+            data = JSON.parse(m[1] || m[2])
+          } catch (e) {
+            continue
+          }
           cb(data)
-        } catch (e) {
-          /* pass */
         }
+
+        idx = chunks[0].indexOf('\n')
       }
-
-      idx = chunks[0].indexOf('\n')
     }
-  }
 
-  process.stdin.setEncoding('utf8')
+    process.stdin.setEncoding('utf8')
 
-  process.stdin.on('readable', () => {
-    while ((chunk = process.stdin.read()) !== null) {
-      chunks.push(chunk)
+    process.stdin.on('readable', () => {
+      while ((chunk = process.stdin.read()) !== null) {
+        chunks.push(chunk)
+        checkProcess()
+      }
+    })
+
+    process.stdin.on('end', () => {
+      chunks.push('\n')
       checkProcess()
-    }
-  })
+      resolve()
+    })
 
-  process.stdin.on('end', () => {
-    chunks.push('\n')
-    checkProcess()
-  })
-
-  process.stdin.on('error', e => {
-    console.error(e)
-    process.exit(1)
+    process.stdin.on('error', e => {
+      reject(e)
+    })
   })
 }
 
-function main() {
+async function main() {
   let first_time = null
   const req_ids = {}
-  get_json_lines(data => {
+  const open_dms = {}
+  await get_json_lines(data => {
     const timestamp = Date.parse(data.time)
     let t = 0
     if (!first_time) {
@@ -65,7 +69,15 @@ function main() {
         req_ids[data.fields.request_id] = timestamp
       }
     }
+    /*
+    if (data.msg_type === 'HandleSendDirectMessage') {
+      open_dms[data.request_id] = true
+    } else if (data.msg_type === 'HandleSendDirectMessageResult') {
+      delete open_dms[data.request_id]
+    }
+    */
     since_req_origin = (since_req_origin / 1000).toFixed(3)
+    console.log('fu')
     console.log(JSON.stringify({
       time: data.time,
       time_diff: t,
@@ -86,7 +98,11 @@ function main() {
       module_path: data.module_path,
     }))
   })
+  //console.error('UNFINISHED DirectMessage count', Object.keys(open_dms).length)
 }
 
 // entry point
-main()
+main().then(() => {}, err => {
+  console.error(err)
+  process.exit(1)
+})

ed030c21e8b25f27dfd6ce0d0c06131c85c76851
Eric Harris-Braun <eric@harris-braun.com>
2020-04-17 17:00:10 -0400

    fixes to async zome test

--- app_spec/test/files/test.js
+++ app_spec/test/files/test.js
@@ -14,7 +14,7 @@ module.exports = scenario => {
     t.deepEqual(result, { Ok: { msg_type: 'response', body: `got hello from ${alice.info('app').agentAddress}` } })
   })
 
-  scenario('multiple zome calls', async (s, t) => {
+  scenario.only('multiple zome calls', async (s, t) => {
     const { alice, bob } = await s.players({ alice: one, bob: one }, true)
     const params = { to_agent: bob.info('app').agentAddress, message: 'hello' }
 
@@ -22,7 +22,7 @@ module.exports = scenario => {
     await bob.kill()
     let results = []
     const f1 = alice.call('app', 'blog', 'ping', params).then(r => {results.push(2)})
-    const f2 = alice.call('app',"blog", "show_env", {}).then(r => {results.push(1)})
+    const f2 = alice.call('app',"blog", "get_test_properties", {}).then(r => {results.push(1)})
     await Promise.all([f1,f2])
 
     // prove that show_env returned before ping

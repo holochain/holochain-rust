8ec156317b9a4c74baf75b8301bd759617eabddb
Eric Harris-Braun <eric@harris-braun.com>
2020-05-05 11:58:38 -0400

    send back gossip list after hold_aspect request from sim2h to confirm holding

--- crates/core/src/dht/actions/hold_aspect.rs
+++ crates/core/src/dht/actions/hold_aspect.rs
@@ -5,6 +5,8 @@ use crate::{
 };
 use futures::{future::Future, task::Poll};
 use holochain_core_types::{error::HolochainError, network::entry_aspect::EntryAspect};
+use holochain_persistence_api::cas::content::AddressableContent;
+use lib3h_protocol::data_types::EntryListData;
 use snowflake::ProcessUniqueId;
 use std::{pin::Pin, sync::Arc};
 
@@ -13,13 +15,34 @@ pub async fn hold_aspect(aspect: EntryAspect, context: Arc<Context>) -> Result<(
     let action_wrapper = ActionWrapper::new(Action::HoldAspect((aspect.clone(), id)));
     dispatch_action(context.action_channel(), action_wrapper.clone());
     let r = HoldAspectFuture {
-        context,
+        context: context.clone(),
         //        aspect,
         id,
     }
     .await;
     if r.is_err() {
         panic!(r);
+    } else {
+        // send a gossip list with this aspect in it back to sim2h so it know we are holding it
+        let c = context.clone();
+        let closure = async move || {
+            let state = context
+                .state()
+                .expect("No state present when trying to respond with gossip list");
+
+            // TODO: send the whole holding map for now fix to the specific aspect later
+            let address_map = state.dht().get_holding_map().clone();
+
+            let action = Action::RespondGossipList(EntryListData {
+                space_address: state.network().dna_address.clone().unwrap().into(),
+                provider_agent_id: context.agent_id.address().into(), //get_list_data.provider_agent_id,
+                request_id: "".to_string(),
+                address_map: address_map.into(),
+            });
+            dispatch_action(context.action_channel(), ActionWrapper::new(action));
+        };
+        let future = closure();
+        c.spawn_task(future);
     }
     r
 }

a0f4c4c931baaf08e1170b455d2a2c7ee804af01
Eric Harris-Braun <eric@harris-braun.com>
2020-07-07 20:57:54 -0400

    better debug and slower sim2h gossip retry

--- crates/sim2h/src/lib.rs
+++ crates/sim2h/src/lib.rs
@@ -123,10 +123,10 @@ impl<T> SendExt<T> for crossbeam_channel::Sender<T> {
     }
 }
 
-//const RETRY_FETCH_MISSING_ASPECTS_INTERVAL_MS: u64 = 30000; // 30 seconds
+const RETRY_FETCH_MISSING_ASPECTS_INTERVAL_MS: u64 = 30000; // 30 seconds
 /// actual timing is handled by sim2h_im_state
 /// but it does cause a mutate, so we don't want to spam it too hard
-const RETRY_FETCH_MISSING_ASPECTS_INTERVAL_MS: u64 = 500; // half second
+//const RETRY_FETCH_MISSING_ASPECTS_INTERVAL_MS: u64 = 500; // half second
 
 fn open_lifecycle(desc: &str, uuid: &str, uri: &Lib3hUri) {
     debug!("connection event open_conns: {} for {}@{}", desc, uuid, uri);

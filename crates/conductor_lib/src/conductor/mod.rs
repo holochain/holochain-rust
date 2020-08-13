1fa8a770244fe86803b6c6e5a6442caa0b9db15f
Eric Harris-Braun <eric@harris-braun.com>
2020-08-13 11:51:57 -0400

    adds get_meta admin method

--- crates/conductor_lib/src/conductor/mod.rs
+++ crates/conductor_lib/src/conductor/mod.rs
@@ -8,7 +8,7 @@ pub mod ui_admin;
 
 pub use self::{
     admin::ConductorAdmin,
-    base::{mount_conductor_from_config, Conductor, CONDUCTOR},
+    base::{mount_conductor_from_config, Conductor, GetMetaOptions, GetMetaResponse, CONDUCTOR},
     debug::ConductorDebug,
     test_admin::ConductorTestAdmin,
     ui_admin::ConductorUiAdmin,
